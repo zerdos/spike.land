37cab2ff0e299e94cef070bb991d1ba54f485ca4
Zoltan Erdos <zoltan.erdos@me.com>
2020-04-04 20:27:15 +0100

    add emotion

--- packages/bcd/src/CounterButton.tsx
+++ packages/bcd/src/CounterButton.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
 
+import { css } from 'emotion'
 
-export const CounterButton: React.FC<{label: string}> = ({label}) => {
+const color = 'white'
 
 
+export const CounterButton: React.FC<{ label: string }> = ({ label }) => {
 
   const [counter, changecounter] = useState(0);
-  
-  return <>
-    <button onClick={()=>changecounter(counter+1)}> {label} {counter}</button>
-  </>;
+
+  return <div>
+    <div
+      className={css`
+    padding: 32px;
+    background-color: hotpink;
+    font-size: 24px;
+    border-radius: 4px;
+    &:hover {
+      color: ${color};
+    }
+  `}
+    >
+      Hover to change color.
+</div>
+
+    <button onClick={() => changecounter(counter + 1)}> {label} {counter}</button>
+      </div>;
 }
--- packages/bcd/src/PopupApp.tsx
+++ packages/bcd/src/PopupApp.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {CounterButton} from './CounterButton';
+import { CounterButton } from './CounterButton';
+
 
 
 ReactDOM.render(<CounterButton label={"Clicked in a row:"} />, document.getElementById('root'));
