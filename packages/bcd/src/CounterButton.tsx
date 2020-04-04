599d845d754d9ade2c448ac098a27c25ce2bbde9
Zoltan Erdos <zoltan.erdos@me.com>
2020-04-04 20:29:49 +0100

    yarn upgrade

--- packages/bcd/src/CounterButton.tsx
+++ packages/bcd/src/CounterButton.tsx
@@ -1,29 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { css } from 'emotion'
+import { css } from "emotion";
 
-const color = 'white'
-
+const color = "white";
 
 export const CounterButton: React.FC<{ label: string }> = ({ label }) => {
-
   const [counter, changecounter] = useState(0);
 
-  return <div>
-    <div
-      className={css`
-    padding: 32px;
-    background-color: hotpink;
-    font-size: 24px;
-    border-radius: 4px;
-    &:hover {
-      color: ${color};
-    }
-  `}
-    >
-      Hover to change color.
-</div>
+  return (
+    <div>
+      <div
+        className={css`
+          padding: 32px;
+          background-color: red;
+          font-size: 24px;
+          border-radius: 4px;
+          &:hover {
+            color: ${color};
+          }
+        `}
+      >
+        Hover to change color.
+      </div>
 
-    <button onClick={() => changecounter(counter + 1)}> {label} {counter}</button>
-      </div>;
-}
+      <button onClick={() => changecounter(counter + 1)}>
+        {" "}
+        {label} {counter}
+      </button>
+    </div>
+  );
+};
