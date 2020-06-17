31d54a365b1e548956882f2e2f987f5cd08bab3c
Zoltan Erdos <zolika84@gmail.com>
2020-06-17 10:12:13 +0100

    add one more deno app

---
+++ deno/my_app/index.tsx
@@ -0,0 +1,8 @@
+/// <reference lib="es2015.iterable" />
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {App} from "./App.tsx";
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
