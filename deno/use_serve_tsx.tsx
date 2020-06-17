9cc04075a2887e889e4b28f96c082b956ebd384b
Zoltan Erdos <zolika84@gmail.com>
2020-06-17 09:36:41 +0100

    deno stuff

--- deno/use_serve_tsx.tsx
+++ deno/use_serve_tsx.tsx
@@ -2,9 +2,28 @@
 
 import { createApp } from "app";
 import { serveJsx } from "serve_jsx";
+import React from "react";
+import ReactDOMServer from "react-dom/server"
 
 const app = createApp();
 // .jsx/.tsx files in ./pages directory will be dynamically imported
 // and rendered component served as html
+app.handle("/", async (req) => {
+    await req.respond({
+      status: 200,
+      headers: new Headers({
+        "content-type": "text/html; charset=UTF-8",
+      }),
+      body: ReactDOMServer.renderToString(
+        <html>
+          <head>
+            <meta charSet="utf-8" />
+            <title>servest</title>
+          </head>
+          <body>Hello Servest!</body>
+        </html>,
+      ),
+    });
+  });
 app.use(serveJsx("./pages", (f) => import(`./${f}`)));
 app.listen({ port: 8899 });
