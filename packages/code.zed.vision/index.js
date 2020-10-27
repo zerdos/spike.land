a25db5b4de26c7bd31f078d9acd46cac4568f390
Zoltan Erdos <zoltan.erdos@me.com>
2020-10-27 19:57:33 +0000

    just the index.js

--- packages/code.zed.vision/index.js
+++ packages/code.zed.vision/index.js
@@ -1,3 +1,3 @@
-import {startMonaco} from "https://unpkg.com/react-cdn-monaco-editor@2.5.2/lib/editor.js"
+import {startMonaco} from "https://unpkg.com/smart-cdn-monaco/lib/editor.js"
 import { run } from "/code.zed-vision.min.js"
 run(startMonaco) 
