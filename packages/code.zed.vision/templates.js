06f7d0c817e104fe97fbe5841ef9ceb624e11946
Zoltan Erdos <zoltan.erdos@me.com>
2020-10-27 19:31:20 +0000

    monaco up

--- packages/code.zed.vision/templates.js
+++ packages/code.zed.vision/templates.js
@@ -1,4 +1,24 @@
-<!DOCTYPE html>
+export const starter = `import * as React from "react";
+import ReactDOM from "react-dom";
+
+const Counter = (
+    { initial = 0 }
+) => {
+    const [clicks, setClicks] = React.useState(initial);
+
+    return <div>
+        <p>Clicks: <strong>{clicks}</strong></p>
+        <button onClick={() => setClicks(clicks + 1)}>+</button>
+        <button onClick={() => setClicks(clicks - 1)}>-</button>
+    </div>;
+};
+
+ReactDOM.render(
+    <Counter initial={0} />,
+    document.getElementById("root")
+);`
+
+export const html = `<!DOCTYPE html>
 <html>
 
 <head>
@@ -86,4 +106,4 @@
     run(startMonaco) 
   </script>
 </body>
-</html>
+</html>`
