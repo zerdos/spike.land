7b31307a5b2674986124ad8d7aecdf3b13fdeadc
Zoltan Erdos <zolika84@gmail.com>
2020-10-29 09:34:26 +0000

    clean up

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
-    "prettier",
   ],
   settings: {
     react: {
--- packages/smart-monaco-editor/lib/editor.d.ts
+++
@@ -1,8 +0,0 @@
-interface StartMonaco {
-    onChange: (code: string) => void;
-    code: string;
-    language: "html" | "javascript" | "typescript";
-}
-export declare const importSpecificVersion: (version: any) => Function;
-export declare const startMonaco: ({ onChange, code, language }: StartMonaco) => Promise<any>;
-export {};
