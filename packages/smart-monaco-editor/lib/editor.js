a8fa830651f451f67b7c94c5c74e5cc2791039ff
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-01 14:34:39 +0000

    build

--- packages/smart-monaco-editor/lib/editor.js
+++ packages/smart-monaco-editor/lib/editor.js
@@ -10,7 +10,9 @@ export const startMonaco = async ({ onChange , code , language  })=>{
         });
         await new Promise((resolve)=>req([
                 "vs/editor/editor.main"
-            ], resolve)
+            ], ()=>{
+                resolve(true);
+            })
         );
     } else {
         editor.onDidChangeModelContent(()=>onChange(editor.getValue())
@@ -18,8 +20,7 @@ export const startMonaco = async ({ onChange , code , language  })=>{
         editor.setValue(code);
         return editor;
     }
-    const monaco = window.monaco;
-    editor = monaco.editor.create(window.document.getElementById("container"), {
+    editor = window.monaco.editor.create(window.document.getElementById("container"), {
         cursorStyle: "block",
         formatOnType: true,
         scrollbar: {
@@ -49,12 +50,15 @@ export const startMonaco = async ({ onChange , code , language  })=>{
         autoSurround: "l  anguageDefined",
         trimAutoWhitespace: true,
         codeActionsOnSaveTimeout: 100,
-        model: monaco.editor.createModel(code, language, monaco.Uri.parse(language === "typescript" ? "file:///main.tsx" : "file:///main.html")),
+        model: window.monaco.editor.createModel(code, language, window.monaco.Uri.parse(language === "typescript" ? "file:///main.tsx" : "file:///main.html")),
         value: code,
         language: language,
         theme: "vs-dark"
     });
-    monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+    editor.onDidChangeModelContent(()=>onChange(editor.getValue())
+    );
+    editor.setValue(code);
+    window.monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
         noSuggestionDiagnostics: true,
         noSemanticValidation: true,
         noSyntaxValidation: true
@@ -92,30 +96,30 @@ export const startMonaco = async ({ onChange , code , language  })=>{
                 depend: []
             }
         ];
-        const dts = importHelper.map(({ name , url  })=>(async ()=>monaco.languages.typescript.typescriptDefaults.addExtraLib(await (await fetch(url)).text(), `file:///node_modules/@types/${name}/index.d.ts`)
+        const dts = importHelper.map(({ name , url  })=>(async ()=>window.monaco.languages.typescript.typescriptDefaults.addExtraLib(await (await fetch(url)).text(), `file:///node_modules/@types/${name}/index.d.ts`)
             )()
         );
-        monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-            target: monaco.languages.typescript.ScriptTarget.ESNext,
+        window.monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+            target: window.monaco.languages.typescript.ScriptTarget.ESNext,
             allowNonTsExtensions: true,
             allowUmdGlobalAccess: true,
             strict: true,
             allowJs: true,
             noEmitOnError: true,
             allowSyntheticDefaultImports: true,
-            moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
-            module: monaco.languages.typescript.ModuleKind.CommonJS,
+            moduleResolution: window.monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+            module: window.monaco.languages.typescript.ModuleKind.CommonJS,
             noEmit: true,
             typeRoots: [
                 "node_modules/@types"
             ],
-            jsx: monaco.languages.typescript.JsxEmit.React,
+            jsx: window.monaco.languages.typescript.JsxEmit.React,
             jsxFactory: "React.createElement",
             jsxFragmentFactory: "React.Fragment",
             esModuleInterop: true
         });
         await Promise.all(dts);
-        monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+        window.monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
             noSuggestionDiagnostics: false,
             noSemanticValidation: false,
             noSyntaxValidation: false
