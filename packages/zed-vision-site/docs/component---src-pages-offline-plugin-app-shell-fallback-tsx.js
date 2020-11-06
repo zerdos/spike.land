a4104146f0cdb902300620a39425e627ac2657f6
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-06 15:05:44 +0000

    update files

---
+++ packages/zed-vision-site/docs/component---src-pages-offline-plugin-app-shell-fallback-tsx.js
@@ -0,0 +1,30 @@
+(this["webpackJsonp"] = this["webpackJsonp"] || []).push([[12],{
+
+/***/ 124:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "pageQuery", function() { return pageQuery; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(0);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_layout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(18);
+/* harmony import */ var _components_seo__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(15);
+/* harmony import */ var _emotion_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(1);
+
+
+
+
+
+var NotFoundPage = function NotFoundPage() {
+  return Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__[/* jsx */ "b"])(_components_layout__WEBPACK_IMPORTED_MODULE_1__[/* Layout */ "a"], null, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__[/* jsx */ "b"])(_components_seo__WEBPACK_IMPORTED_MODULE_2__[/* SEO */ "a"], {
+    title: "... offline"
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__[/* jsx */ "b"])("h1", null, "Maybe you are offline"), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__[/* jsx */ "b"])("p", null, "Check your internet connection"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (NotFoundPage);
+var pageQuery = "3159585216";
+
+/***/ })
+
+}]);
