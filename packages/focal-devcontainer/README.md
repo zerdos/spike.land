a37659d5157b5d8e9bfae4e2d866cbd9331e93cd
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-08 21:25:18 +0000

    pre-release

--- packages/focal-devcontainer/README.md
+++ packages/focal-devcontainer/README.md
@@ -5,5 +5,6 @@
 - Yarn: 1.22.10
 - With noVNC - XFCE trought noVNC (http://localhost:6080)
 - with Google-chrome- docker preinstalled
+- Zsh + Oh my zsh!
 
 Git repository: https://github.com/zed-vision/monorepo
--- packages/smart-monaco-editor/index.js
+++
@@ -1,4 +0,0 @@
-const version="6.5.0";
-
-export const importSpecificVersion = (version) => Function(`return import("https://unpkg.com/smart-monaco-editor${version}/lib/editor.js")`);
-export const mod = importSpecificVersion(version);
