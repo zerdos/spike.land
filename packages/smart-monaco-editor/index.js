9880896202263ca0cc8e99bf5941f9bccdf831c0
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-08 20:15:43 +0000

    version file

--- packages/smart-monaco-editor/index.js
+++ packages/smart-monaco-editor/index.js
@@ -1,4 +1,4 @@
-const version="6.4.5";
+const version="6.4.6";
 
 export const importSpecificVersion = (version) => Function(`return import("https://unpkg.com/smart-monaco-editor${version}/lib/editor.js")`);
 export const mod = importSpecificVersion(version);
