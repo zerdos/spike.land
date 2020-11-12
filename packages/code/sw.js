0991ea202cbc7602b9221a8a7899b7f33e1c4ebb
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-12 12:02:23 +0000

    pre-release

--- packages/code/sw.js
+++ packages/code/sw.js
@@ -1,1 +1,30 @@
-console.log("hello2")
+const cacheName = 'code-7.0.0';
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                '/index.html'
+            ])
+                .then(() => self.skipWaiting());
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        (async () => {
+            const offlineCache = await caches.open(cacheName);
+            const cachedRespond = await offlineCache.match(event.request, { ignoreSearch: true })
+            const resp = cachedRespond || fetch(event.request);
+            if (resp !== cachedRespond) setTimeout(()=>offlineCache.add(event.request.url));
+
+            return resp;
+        })()
+    );
+
+});
