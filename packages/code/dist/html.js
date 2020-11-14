91d125981ee1969ee3eca0c10f886ece95c6e40d
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-14 20:12:58 +0000

    pre-release

--- packages/code/dist/html.js
+++ packages/code/dist/html.js
@@ -1,4 +1,4 @@
-export const version = `7.1.20`; export const html = `<!DOCTYPE html>
+export const version = `7.1.21`; export const html = `<!DOCTYPE html>
 <html>
 
 <head>
@@ -154,7 +154,7 @@ NodeList.prototype.on = NodeList.prototype.addEventListener =
   <script type="module">
 
     const runner = async () => {
-      const version = "7.1.20";
+      const version = "7.1.21";
       const cdnAddress = "https://unpkg.com/@zedvision/code@";
       const script = "/dist/_cBundle.js.min.js";
 
@@ -178,7 +178,7 @@ NodeList.prototype.on = NodeList.prototype.addEventListener =
 </html>`; export const sw = `importScripts("https://unpkg.com/comlink@4.3.0/dist/umd/comlink.min.js");
 importScripts("https://unpkg.com/idb@5.0.7/build/iife/with-async-ittr-min.js");
 importScripts(
-  "https://unpkg.com/@zedvision/code@7.1.20/dist/worker-script.js",
+  "https://unpkg.com/@zedvision/code@7.1.21/dist/worker-script.js",
 );
 
 const dbPromise = idb.openDB("localZedCodeStore", 1, {
@@ -205,7 +205,7 @@ const SHATEST = {
   },
 };
 
-var cacheKey = "7.1.20";
+var cacheKey = "7.1.21";
 
 this.addEventListener("install", function (e) {
   e.waitUntil(
