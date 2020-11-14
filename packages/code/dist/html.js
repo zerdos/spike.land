2674b374e125421942d4a06188ae7f2e4c1c82ca
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-14 15:38:27 +0000

    pre-release

--- packages/code/dist/html.js
+++ packages/code/dist/html.js
@@ -1,4 +1,4 @@
-export const version = `7.1.7`; export const html = `<!DOCTYPE html>
+export const version = `7.1.8`; export const html = `<!DOCTYPE html>
 <html>
 
 <head>
@@ -163,7 +163,7 @@ export const version = `7.1.7`; export const html = `<!DOCTYPE html>
   <script type="module">
 
     const runner = async () => {
-      const version = "7.1.7";
+      const version = "7.1.8";
       const cdnAddress = "https://unpkg.com/@zedvision/code@";
       const script = "/dist/_cBundle.js.min.js";
 
