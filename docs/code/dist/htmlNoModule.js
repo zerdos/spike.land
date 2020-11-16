ba0c54d1b5c47e80ce3b93db286f3bbd16b3fd5c
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-16 18:32:08 +0000

    pre-release

--- docs/code/dist/htmlNoModule.js
+++ docs/code/dist/htmlNoModule.js
@@ -1,4 +1,4 @@
-const version = `7.2.1`; const html = `<!DOCTYPE html>
+const version = `7.2.2`; const html = `<!DOCTYPE html>
 <html>
 
 <head>
@@ -161,7 +161,7 @@ const version = `7.2.1`; const html = `<!DOCTYPE html>
         run();
       } else {
         try{
-          const version = "@7.2.1";
+          const version = "@7.2.2";
           const { run } = await import(cdnAddress   +  version + script)
         run();
         } catch(e){
@@ -185,7 +185,7 @@ const version = `7.2.1`; const html = `<!DOCTYPE html>
 importScripts("https://unpkg.com/idb@5.0.7/build/iife/with-async-ittr-min.js");
 
 // importScripts(
-//   "https://unpkg.com/@zedvision/code@7.2.1/dist/htmlNoModule.js",
+//   "https://unpkg.com/@zedvision/code@7.2.2/dist/htmlNoModule.js",
 // );
 
 const dbPromise = idb.openDB("localZedCodeStore", 1, {
@@ -220,7 +220,7 @@ const SHATEST = {
   },
 };
 
-var cacheKey = "7.2.1-1";
+var cacheKey = "7.2.2-1";
 
 this.addEventListener("install", function (e) {
   e.waitUntil(
