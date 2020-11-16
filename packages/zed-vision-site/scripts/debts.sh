e9d56f3efb46965b419682b72de9fc5b9b373021
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-16 18:38:09 +0000

    pre-release

--- packages/zed-vision-site/scripts/debts.sh
+++ packages/zed-vision-site/scripts/debts.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 if ! [ -x "$(command -v deno)" ]; then
-    curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.5.2
+    curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.5.3
 fi
 
 if ! [ -x "$(command -v file_server)" ]; then
-    deno install --allow-net --allow-read -f https://deno.land/std@0.77.0/http/file_server.ts 
+    deno install --allow-net --allow-read -f https://deno.land/std@0.78.0/http/file_server.ts 
 fi
 
