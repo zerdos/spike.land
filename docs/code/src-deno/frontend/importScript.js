ad86746238aa5842a7240a62d851a0f259826124
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-17 21:12:22 +0000

    clean

--- docs/code/src-deno/frontend/importScript.js
+++
@@ -1,9 +0,0 @@
-export const importScript = async (src) =>
-  document.querySelector(`script[src="${src}"]`) ||
-  new Promise(function (resolve, reject) {
-    const s = window.document.createElement("script");
-    s.src = src;
-    s.onload = resolve;
-    s.onerror = reject;
-    window.document.head.appendChild(s);
-  });
--- docs/code/src/dec.ts
+++
@@ -1,6 +0,0 @@
-/// <reference lib="webworker" />
-
-export type {
-  FetchEvent,
-  KVNamespace,
-} from "../../../node_modules/@cloudflare/workers-types/index.d.ts";
