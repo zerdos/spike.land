07a37f259ff2f8d8b1726713445c4af6923fbf27
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-21 21:11:07 +0000

    pre-release

--- packages/code/dist/htmlNoModule.js
+++ packages/code/dist/htmlNoModule.js
@@ -1,4 +1,4 @@
-const version = `7.6.6`; const html = `  <!DOCTYPE html>
+const version = `7.6.7`; const html = `  <!DOCTYPE html>
 <html>
 
 <head>
@@ -167,7 +167,7 @@ const version = `7.6.6`; const html = `  <!DOCTYPE html>
         const { run } = await import("/code/dist/codeLoader.min.js");
         run();
       } catch (e) {
-        const { run } = await import( "https://unpkg.com/@zedvision/code@7.6.6/dist/codeLoader.min.js" );
+        const { run } = await import( "https://unpkg.com/@zedvision/code@7.6.7/dist/codeLoader.min.js" );
         run();
       }
   }
@@ -183,7 +183,7 @@ importScripts(
 );
 
 // importScripts(
-//   "https://unpkg.com/@zedvision/code@7.6.6/dist/htmlNoModule.js",
+//   "https://unpkg.com/@zedvision/code@7.6.7/dist/htmlNoModule.js",
 // );
 
 (({ Comlink, idb, location, caches, addEventListener }) => {
@@ -219,7 +219,7 @@ importScripts(
     },
   };
 
-  var cacheKey = "7.6.6-1";
+  var cacheKey = "7.6.7-1";
 
   addEventListener("install", function (e) {
     e.waitUntil(
