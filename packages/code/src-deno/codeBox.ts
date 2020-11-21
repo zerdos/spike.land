56f496d533bfddae88163c213f2cbedb0b142550
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-21 20:42:06 +0000

    pre-release

--- packages/code/src-deno/codeBox.ts
+++ packages/code/src-deno/codeBox.ts
@@ -297,7 +297,7 @@ export async function run() {
 
       let motionDep = "";
       let motionScript = "";
-      if (code.includes("framer-motion")) {
+      if (code.includes("Motion")) {
         motionDep =
           `<script crossorigin src="https://unpkg.com/framer-motion@2.9.4/dist/framer-motion.js"></script>`;
 
