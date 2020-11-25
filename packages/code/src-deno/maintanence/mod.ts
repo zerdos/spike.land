1a64e409c6be30ab15726af875cf723af5deb95c
Zoltan Erdos <zoltan.erdos@me.com>
2020-11-25 10:45:43 +0000

    pre-release

--- packages/code/src-deno/maintanence/mod.ts
+++
@@ -1,8 +0,0 @@
-import { config } from "https://deno.land/x/dotenv/mod.ts";
-
-import { getKeys } from "./maintenence.ts";
-
-const env = config();
-
-console.log(await getKeys(env.API_KEY, "a"));
-env.API_KEY;
