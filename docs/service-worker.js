165d7363bb81cb05a23a5e6a8b7252373127ba1f
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-13 12:18:33 +0000

    pre-release

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -81,7 +81,7 @@ if (!self.define) {
     });
   };
 }
-define("./service-worker.js",['./workbox-ac194f0a'], function (workbox) { 'use strict';
+define("./service-worker.js",['./workbox-3bf8039c'], function (workbox) { 'use strict';
 
   /**
   * Welcome to your Workbox-powered service worker!
@@ -107,14 +107,14 @@ define("./service-worker.js",['./workbox-ac194f0a'], function (workbox) { 'use s
    */
 
   workbox.precacheAndRoute([{
-    "url": "/60279455279f06c16ede.worker.js",
+    "url": "/16ea82d49bac0bf4dc29.worker.js",
     "revision": null
   }, {
-    "url": "/f27820059287a6ea6a49.worker.js",
+    "url": "/8fe932435a071f961917.worker.js",
     "revision": null
   }, {
     "url": "/render-page.js",
-    "revision": "e9ec551074b71e7df71cb1abbfe3ecba"
+    "revision": "7232cee3fad0a33f7386893099b7893b"
   }, {
     "url": "/static/forkMe-1a2bcb7dd2c870c3afe5d50466500c18.png",
     "revision": "1a2bcb7dd2c870c3afe5d50466500c18"
