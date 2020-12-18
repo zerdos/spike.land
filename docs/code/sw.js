6fecdc5d6907a2db6af25ae86b32d3072e12d472
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-18 10:32:38 +0000

    pre-release

--- docs/code/sw.js
+++ docs/code/sw.js
@@ -1,8 +1,10 @@
 importScripts("https://unpkg.com/workbox-sw@6.0.2/build/workbox-sw.js");
 const {strategies} = workbox;
 self.addEventListener("fetch", (event) => {
-  if (!event.request.url.endsWith("sw.js") && (event.request.url.endsWith(".js") && event.request.url.endsWith(".html") || event.request.url.endsWith(".woff") || event.request.url.endsWith(".png") || event.request.url.endsWith(".ts"))) {
+  const {request} = event;
+  const {url} = request;
+  if (!url.endsWith("sw.js") && (url.endsWith(".js") && url.endsWith(".html") || url.endsWith(".woff") || url.endsWith(".png") || url.endsWith(".ts"))) {
     const cacheFirst = new strategies.CacheFirst();
-    event.respondWith(cacheFirst.handle({request: event.request}));
+    event.respondWith(cacheFirst.handle({event, request}));
   }
 });
--- packages/code/src/sw.ts
+++ packages/code/src/sw.ts
@@ -5,9 +5,11 @@ importScripts(
 const {strategies} = workbox;
 
 self.addEventListener('fetch', (event) => {
-  if (!event.request.url.endsWith("sw.js") && (event.request.url.endsWith(".js") &&  event.request.url.endsWith(".html") || event.request.url.endsWith(".woff") ||event.request.url.endsWith(".png")  || event.request.url.endsWith(".ts"))) {
+  const {request} = event;
+  const {url} = request;
+  if (!url.endsWith("sw.js") && (url.endsWith(".js") &&  url.endsWith(".html") || url.endsWith(".woff") || url.endsWith(".png")  || url.endsWith(".ts"))) {
     // Using the previously-initialized strategies will work as expected.
     const cacheFirst = new strategies.CacheFirst();
-    event.respondWith(cacheFirst.handle({request: event.request}));
+    event.respondWith(cacheFirst.handle({event, request}));
   }
 });
