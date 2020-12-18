8b1ea8f865ebc16c38583c1291a1652104108ef0
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-18 10:22:20 +0000

    pre-release

--- docs/code/sw.js
+++ docs/code/sw.js
@@ -1,7 +1,7 @@
 importScripts("https://unpkg.com/workbox-sw@6.0.2/build/workbox-sw.js");
 const {strategies} = workbox;
 self.addEventListener("fetch", (event) => {
-  if (event.request.url.endsWith(".js")) {
+  if (!event.request.url.endsWith("sw.js") && (event.request.url.endsWith(".js") && event.request.url.endsWith(".html") || event.request.url.endsWith(".woff") || event.request.url.endsWith(".png") || event.request.url.endsWith(".ts"))) {
     const cacheFirst = new strategies.CacheFirst();
     event.respondWith(cacheFirst.handle({request: event.request}));
   }
--- packages/code/src/sw.ts
+++ packages/code/src/sw.ts
@@ -1,11 +1,11 @@
 importScripts(
   "https://unpkg.com/workbox-sw@6.0.2/build/workbox-sw.js",
 );
-
+// This will trigger the importScripts() for workbox.strategies and its dependencies:
 const {strategies} = workbox;
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.url.endsWith('.js')) {
+  if (!event.request.url.endsWith("sw.js") && (event.request.url.endsWith(".js") &&  event.request.url.endsWith(".html") || event.request.url.endsWith(".woff") ||event.request.url.endsWith(".png")  || event.request.url.endsWith(".ts"))) {
     // Using the previously-initialized strategies will work as expected.
     const cacheFirst = new strategies.CacheFirst();
     event.respondWith(cacheFirst.handle({request: event.request}));
