78371731af5b4a65f85041f535c6c19b4e0ac64e
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-23 18:17:39 +0000

    pre-release

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -114,7 +114,7 @@ define("./service-worker.js",['./workbox-ac194f0a'], function (workbox) { 'use s
     "revision": null
   }, {
     "url": "/render-page.js",
-    "revision": "f3391a4a3884fc77f0ae58a29e80fb6d"
+    "revision": "246aecbcf042a2430622a66e6da3c4b3"
   }, {
     "url": "/static/forkMe-1a2bcb7dd2c870c3afe5d50466500c18.png",
     "revision": "1a2bcb7dd2c870c3afe5d50466500c18"
