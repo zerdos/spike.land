752bc04d76b2d04d35cd2bf5cd6448b88402eead
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-24 17:52:42 +0000

    pre-release

--- packages/shadb/src/shaDB.js
+++ packages/shadb/src/shaDB.js
@@ -2,9 +2,9 @@ import { openDB } from "https://unpkg.com/idb@5.0.8/build/esm/index.js";
 import { getDbObj } from "./getDbObj.js";
 
 export function getDB() {
-  const dbPromise = openDB("localZedCodeStore", 1, {
+  const dbPromise = openDB("zed-vision-alpha", 2, {
     upgrade(db) {
-      db.createObjectStore("codeStore");
+      db.createObjectStore("sha-store");
     },
     blocked() {
     },
