740d5308b04eaa751eba7e87037692dd3b188678
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-25 20:09:25 +0000

    pre-release

--- packages/code/dist/db.js
+++ packages/code/dist/db.js
@@ -1,10 +1,11 @@
-import { getDB } from "https://unpkg.com/@zedvision/shadb@10.13.7/dist/shaDB.js";
 export const shaDB = {
     get: async (key, type) => {
+        const { getDB } = await import("https://unpkg.com/@zedvision/shadb@10.13.7/dist/shaDB.js");
         const db = await (await getDB("shaDB"))();
         return db.get(key, type);
     },
     put: async (key, value) => {
+        const { getDB } = await import("https://unpkg.com/@zedvision/shadb@10.13.7/dist/shaDB.js");
         const db = await (await getDB("shaDB"))();
         return db.put(key, value);
     },
