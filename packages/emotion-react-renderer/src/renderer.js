b41685f5295658c1fb0b3c5ff2e5f6c0b5a972aa
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-25 16:58:11 +0000

    save

---
+++ packages/emotion-react-renderer/src/renderer.js
@@ -0,0 +1,16 @@
+import * as Motion from "framer-motion";
+import React, { Fragment } from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { jsx, css, Global } from "@emotion/react";
+export { React };
+export { Fragment };
+export { Motion };
+export const { motion } = Motion;
+export { jsx };
+export { css };
+export { Global };
+export const renderEmotion = (el, container) => {
+    render(jsx(Fragment, { children: el }), container);
+    return () => unmountComponentAtNode(container);
+};
+export default React;
