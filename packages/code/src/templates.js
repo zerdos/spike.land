13a3ed20aa00ef611227d6e7c5f7a4168d1a9035
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-25 20:41:11 +0000

    pre-release

--- packages/code/src/templates.js
+++ packages/code/src/templates.js
@@ -22,6 +22,7 @@ export const getHtml = ({ HTML, css, link }) => {
 <html lang="en">
 <head profile="http://www.w3.org/2005/10/profile">
 <title>${title}</title>
+<link rel="preload" href="${link}" type="text/html" as="fetch" crossorigin>
 <link rel="icon" type="image/png" href="https://zed.vision/zed-icon-big.png" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -36,10 +37,16 @@ export const getHtml = ({ HTML, css, link }) => {
 </div>
 <script type="module">
 import { renderEmotion } from "https://unpkg.com/@zedvision/emotion-react-renderer@10.13.3/dist/bundle.js"
-import App from "${link}" 
-
-renderEmotion(App(), document.body.children[0]);
-
+fetch("${link}")
+.then(data=>data.text())
+.then(async (text) => {
+  const App = (
+    await import(URL.createObjectURL(
+      new Blob([text], { type: "application/javascript" }
+      ))
+    )).default;
+    renderEmotion(App(), document.body.children[0]);
+})
 </script>
 </body>
 </html>
