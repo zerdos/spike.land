1dc0e9b8d28966054c872046d4e28572c8ed29c1
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-25 22:03:21 +0000

    pre-release

--- packages/code/dist/workerdomtest.js
+++ packages/code/dist/workerdomtest.js
@@ -2,8 +2,7 @@
 fetch("https://ipfs.io/ipfs/QmNSW14V6QUWdVGTCehjcB3a6j3F7dRyxy4cTrTb7japmi")
     .then(data => data.text())
     .then(async (text) => {
-    const { renderEmotion } = await import("https://unpkg.com/@zedvision/emotion-react-renderer@10.13.3/dist/bundle.js");
     const App = (await import(URL.createObjectURL(new Blob([text], { type: "application/javascript" })))).default;
+    const { renderEmotion } = await import("https://unpkg.com/@zedvision/emotion-react-renderer@10.13.3/dist/bundle.js");
     renderEmotion(App(), document.getElementById("zbody"));
-    document.getElementById("zbody").innerHTML = "<h1>yay</h1>";
 });
