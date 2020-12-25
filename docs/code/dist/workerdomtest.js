803ab8ebd75e491eb0cf3fd23942318903a8f6b6
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-25 21:55:44 +0000

    pre-release

--- docs/code/dist/workerdomtest.js
+++ docs/code/dist/workerdomtest.js
@@ -1,1 +1,9 @@
 "use strict";
+fetch("https://ipfs.io/ipfs/QmNSW14V6QUWdVGTCehjcB3a6j3F7dRyxy4cTrTb7japmi")
+    .then(data => data.text())
+    .then(async (text) => {
+    const { renderEmotion } = await import("https://unpkg.com/@zedvision/emotion-react-renderer@10.13.3/dist/bundle.js");
+    const App = (await import(URL.createObjectURL(new Blob([text], { type: "application/javascript" })))).default;
+    renderEmotion(App(), document.getElementById("zbody"));
+    document.getElementById("zbody").innerHTML = "<h1>yay</h1>";
+});
