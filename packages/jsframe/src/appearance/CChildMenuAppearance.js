3b80635d228c9aa493991135e40b9e73afdeefd4
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-26 18:50:18 +0000

    pre-release

--- packages/jsframe/src/appearance/CChildMenuAppearance.js
+++
@@ -1,9 +0,0 @@
-function CChildMenuAppearance(model) {
-  this.childMenuHTML = "";
-  this.childMenuWidth = 0;
-  this.childMenuAlign = "LEFT"; // off set position from parent frameComponent "LEFT" "RIGHT" "CENTER"
-  this.yOffset = 0;
-  this.xOffset = 0;
-}
-
-module.exports = CChildMenuAppearance;
--- packages/jsframe/src/index.js
+++
@@ -1,4 +0,0 @@
-//export { default as JSFrame } from './JSFrame.js';
-module.exports = {
-  JSFrame: require("./JSFrame"),
-};
