5c3d46a08fb9377fdcde40b4a1c53d3223b551dc
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-27 22:17:14 +0000

    pre-release

--- packages/code/dist/src/versions.js
+++ packages/code/dist/src/versions.js
@@ -1,6 +1,6 @@
 export default function () {
     const v = {
-        ipfs: "QmQHaxgx5vf1Y9bCDALCp9Xbeycn4RQxd7Xinc5ALRrPQ",
+        ipfs: "QmYWeJgvE9Nxm2qk5SZy14kJC32rRZDv89dUGfy71HvDAY",
         emotionRenderer: "10.13.3",
         prettier: "2.2.1",
         editor: "10.13.14",
