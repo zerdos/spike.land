e9fd3fdf7430b05a3397ab05ff11820ebe8f4b39
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-27 16:22:22 +0000

    pre-release

---
+++ docs/code/dist/src/ipfsKV.d.ts
@@ -0,0 +1,4 @@
+export namespace ipfsKV {
+    function add(data: any, options: any): Promise<any>;
+    function addAll(files: any): Promise<any>;
+}
---
+++ docs/code/dist/src/ipfsKV.worker.js
@@ -0,0 +1,47 @@
+"use strict";
+importScripts("https://unpkg.com/comlink@4.3.0/dist/umd/comlink.js");
+importScripts("https://unpkg.com/ipfs@0.52.3/dist/index.min.js");
+// async function getIpfsiD() {
+//   const { shaDB } = await import("./db.js");
+//   const v4 =
+//     (await import("https://unpkg.com/uuid@8.3.2/dist/esm-browser/v4.js"))
+//       .default;
+//   let ipfsId = await shaDB.get("ipfs");
+//   if (!ipfsId) {
+//     ipfsId = v4();
+//     await shaDB.put("ipfs", ipfsId);
+//   }
+//   return ipfsId;
+// }
+let ipfsNode;
+try {
+    const ipfsKV = {
+        add: async (data, options) => {
+            ipfsNode = ipfsNode || await Ipfs.create();
+            const { cid } = await ipfsNode.add(data, options);
+            if (options && options.onlyHash) {
+                return (new Ipfs.CID(1, 112, cid.multihash)).toString();
+            }
+            return cid.string;
+        },
+        addAll: async (files) => {
+            try {
+                ipfsNode = ipfsNode || await Ipfs.create();
+                const res = [];
+                for await (const result of ipfsNode.addAll(files)) {
+                    const { path, cid } = result;
+                    const CID = cid.string;
+                    res.push({ path, CID });
+                }
+                return res;
+            }
+            catch (e) {
+                return ({ e });
+            }
+        }
+    };
+    Comlink.expose(ipfsKV);
+}
+catch (_a) {
+    //just noise reducing c:)
+}
