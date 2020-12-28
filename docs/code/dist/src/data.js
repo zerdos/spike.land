2a4d42e1602baa2bc65e4373b1085070a9b16357
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-28 23:10:32 +0000

    pre-release

--- docs/code/dist/src/data.js
+++ docs/code/dist/src/data.js
@@ -1,6 +1,22 @@
 import { shaDB } from "./db.js";
 import getVersions from "./versions.js";
 const v = getVersions();
+export const getProjects = async () => {
+    const uuid = await getUserId();
+    const projects = await shaDB.get(uuid, "json");
+    if (typeof projects === "string" || projects === null || !projects.list) {
+        const v4 = (await import(`https://unpkg.com/uuid@${v.uuid}/dist/esm-browser/v4.js`)).default;
+        const projectId = v4();
+        await shaDB.put(uuid, JSON.stringify({
+            list: [projectId],
+            [projectId]: {
+                lastOpen: Date.now(),
+            },
+        }));
+        return [projectId];
+    }
+    return projects.list;
+};
 export async function getZkey(hash) {
     const uuid = await getUserId();
     const { sha256 } = await import(`https://unpkg.com/@zedvision/sha256@${v.sha256}/sha256.js`);
@@ -35,7 +51,6 @@ export async function getCodeToLoad() {
     const keyToLoad = await shaDB.get(projectName);
     const projectDesc = await shaDB.get(keyToLoad, "json");
     if (projectDesc !== null) {
-        return ;
         const data = {
             code: await shaDB.get(projectDesc.code),
             transpiled: await shaDB.get(projectDesc.transpiled) || "",
@@ -51,55 +66,38 @@ export async function getCodeToLoad() {
             html: null,
             versions: null,
         };
-        return data;
     }
-    export const getProjects = async () => {
-        const uuid = await getUserId();
-        const projects = await shaDB.get(uuid, "json");
-        if (typeof projects === "string" || projects === null || !projects.list) {
-            const v4 = (await import(`https://unpkg.com/uuid@${v.uuid}/dist/esm-browser/v4.js`)).default;
-            const projectId = v4();
-            await shaDB.put(uuid, JSON.stringify({
-                list: [projectId],
-                [projectId]: {
-                    lastOpen: Date.now(),
-                },
-            }));
-            return [projectId];
-        }
-        // const search = new URLSearchParams(window.location.search);
-        // const keyToLoad = search.get("h") || await db.get(projectName);
-        // if(keyToLoad){
-        //   projects.map(p=>shaDB.get())
-        // }
-        return projects.list;
-    };
-    export const saveCode = async ({ code, html, transpiled, versions }) => {
-        const { sha256 } = await import(`https://unpkg.com/@zedvision/sha256@${v.sha256}/sha256.js`);
-        const projectName = await getActiveProject();
-        const prevHash = await shaDB.get(projectName);
-        const desc = {
-            code: await sha256(code),
-            html: await sha256(html || ""),
-            transpiled: await sha256(transpiled),
-            versions: await sha256(versions || "").
-                prevHash
-        };
-        const hash = await sha256(JSON.stringify(desc));
-        await shaDB.put(hash, JSON.stringify(desc));
-        const prevData = await shaDB.get(prevHash);
-        if (code)
-            shaDB.put(desc.code, code);
-        if (html)
-            shaDB.put(desc.html, html);
-        if (transpiled)
-            shaDB.put(desc.transpiled, transpiled);
-        if (versions)
-            shaDB.put(desc.versions, versions);
-        await shaDB.put(projectName, hash);
-        // setQueryStringParameter("h", hash);
-        //const response = fetch(request);
-        // lets not save now - we will save the diff only
-        // await response;
-    };
 }
+// const search = new URLSearchParams(window.location.search);
+// const keyToLoad = search.get("h") || await db.get(projectName);
+// if(keyToLoad){
+//   projects.map(p=>shaDB.get())
+// }
+export const saveCode = async ({ code, html, transpiled, versions }) => {
+    const { sha256 } = await import(`https://unpkg.com/@zedvision/sha256@${v.sha256}/sha256.js`);
+    const projectName = await getActiveProject();
+    const prevHash = await shaDB.get(projectName);
+    const desc = {
+        code: await sha256(code),
+        html: await sha256(html || ""),
+        transpiled: await sha256(transpiled),
+        versions: await sha256(versions || "")
+            .prevHash,
+    };
+    const hash = await sha256(JSON.stringify(desc));
+    await shaDB.put(hash, JSON.stringify(desc));
+    const prevData = await shaDB.get(prevHash);
+    if (code)
+        shaDB.put(desc.code, code);
+    if (html)
+        shaDB.put(desc.html, html);
+    if (transpiled)
+        shaDB.put(desc.transpiled, transpiled);
+    if (versions)
+        shaDB.put(desc.versions, versions);
+    await shaDB.put(projectName, hash);
+    // setQueryStringParameter("h", hash);
+    //const response = fetch(request);
+    // lets not save now - we will save the diff only
+    // await response;
+};
