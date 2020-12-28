9746ef95b8b1eae39e0471a61860fe50b9183fa8
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-28 11:41:00 +0000

    pre-release

---
+++ docs/code/dist/src/versions.d.ts
@@ -0,0 +1,10 @@
+export default function _default(): {
+    ipfs: string;
+    emotionRenderer: string;
+    prettier: string;
+    editor: string;
+    diff: string;
+    sha256: string;
+    uuid: string;
+    comlink: string;
+};
---
+++ packages/code/dist/src/versions.js
@@ -0,0 +1,13 @@
+export default function () {
+    const v = {
+        ipfs: "QmbreYdX9LckbfH3fXDGxYFo2bVxwfZ65Lo9QptxGhhEMs",
+        emotionRenderer: "10.13.3",
+        prettier: "2.2.1",
+        editor: "10.14.6",
+        diff: "10.13.14",
+        sha256: "10.12.14",
+        uuid: "8.3.2",
+        comlink: "4.3.0",
+    };
+    return v;
+}
