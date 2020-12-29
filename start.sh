a4f9052560482cfb19c6f5f1e9f12c72c940b68c
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-29 19:41:46 +0000

    pre-release

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 
 CID=$(docker-compose exec ipfs ipfs add -r /export/packages -Q)
+
 firefox https://ipfs.io/ipfs/$CID &
-echo $CID
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/ec8e903035c7b0fcd3e95f1e483ab68c/dns_records/7545e99c94fd6ff43cc0591bab13cbe1" \
+echo $DATA
+ curl -X PUT "https://api.cloudflare.com/client/v4/zones/ec8e903035c7b0fcd3e95f1e483ab68c/dns_records/7545e99c94fd6ff43cc0591bab13cbe1" \
      -H "Authorization: Bearer $BBTOKEN" \
      -H "Content-Type: application/json" \
-     --data '{"type":"TXT","name":"_dnslink.x","content":"dnslink=/ipfs/$CID","ttl":1,"proxied":false}' 
-
-
+     --data  $(CID=$CID node -pe 'JSON.stringify({"type":"TXT","name":"_dnslink.x","content": "dnslink=/ipfs/"+process.env["CID"],"ttl":1,"proxied":false})')
 
