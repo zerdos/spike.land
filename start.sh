779fe19eea2d81aacdd2169b4d88ee8bfd4ed54e
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-29 22:58:30 +0000

    pre-release

--- start.sh
+++ start.sh
@@ -1,14 +1,33 @@
 
 BCID=$(docker-compose exec ipfs ipfs add -r /export/packages/zed-vision-site/public -Q)
-echo $DATA
- (curl -X PUT "https://api.cloudflare.com/client/v4/zones/ec8e903035c7b0fcd3e95f1e483ab68c/dns_records/7545e99c94fd6ff43cc0591bab13cbe1" \
+
+#  curl -X GET "https://api.cloudflare.com/client/v4/zones/ec8e903035c7b0fcd3e95f1e483ab68c/dns_records/?type=TXT" \
+#      -H "Authorization: Bearer $BBTOKEN" \
+#      -H "Content-Type: application/json" 
+#      --data  $(CID=$BCID node -pe 'JSON.stringify({"type":"TXT","name":"_dnslink","content": "dnslink=/ipfs/"+process.env["CID"],"ttl":1,"proxied":false})') &)
+
+
+
+echo "-------------------------------------------------------------" 
+echo $BCID
+echo "-------------------------------------------------------------" 
+
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/ec8e903035c7b0fcd3e95f1e483ab68c/dns_records/82dce0cb0b0d63ed28bfc429e829c4e9" \
      -H "Authorization: Bearer $BBTOKEN" \
      -H "Content-Type: application/json" \
-     --data  $(CID=$BCID node -pe 'JSON.stringify({"type":"TXT","name":"_dnslink","content": "dnslink=/ipfs/"+process.env["CID"],"ttl":1,"proxied":false})') &)
+     --data  $(BCID=$BCID node -pe 'JSON.stringify({"type":"TXT","name":"_dnslink","content": "dnslink=/ipfs/"+process.env["BCID"],"ttl":1,"proxied":false})')
 
 
-CID=$(docker-compose exec ipfs ipfs add -r /export/packages -Q) && firefox https://ipfs.io/ipfs/$CID &
-echo $DATA &&
+echo "-------------------------------------------------------------" 
+
+
+CID=$(docker-compose exec ipfs ipfs add -r /export/packages -Q) 
+
+echo "-------------------------------------------------------------" 
+
+firefox https://ipfs.io/ipfs/$CID 
+
+echo "-------------------------------------------------------------" 
  curl -X PUT "https://api.cloudflare.com/client/v4/zones/ec8e903035c7b0fcd3e95f1e483ab68c/dns_records/7545e99c94fd6ff43cc0591bab13cbe1" \
      -H "Authorization: Bearer $BBTOKEN" \
      -H "Content-Type: application/json" \
