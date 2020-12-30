7f4685d3e533fe2feeb8dbc5a53c95850607a2b7
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-30 11:18:10 +0000

    pre-release

--- packages/generator/index.js
+++ packages/generator/index.js
@@ -1,4 +1,3 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var devcontainerGenerator_1 = require("./lib/src/devcontainerGenerator");
-exports.DevcontainerGenerator = devcontainerGenerator_1.DevcontainerGenerator;
+const {DevcontainerGenerator} = require ("./lib/src/devcontainerGenerator.js")
+module.exports =  {DevcontainerGenerator};
