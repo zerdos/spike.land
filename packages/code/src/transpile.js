c21e9d6e22e2039b2dd04da46e0e95975b5de206
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-31 19:55:45 +0000

    pre-release

--- packages/code/src/transpile.js
+++ packages/code/src/transpile.js
@@ -1,9 +1,4 @@
-import * as Comlink from "https://unpkg.com/comlink@4.3.0/dist/esm/comlink.mjs";
 import versions from "./versions.js";
-import { shaDB } from "./db.js";
-import v4 from "https://unpkg.com/uuid@8.3.2/dist/esm-browser/v4.js";
-
-// import * as Comlink from "../../../dist/esm/comlink.mjs";
 
 /** @type {(arg0: string, arg1: boolean) => any} */
 let transform;
@@ -19,41 +14,19 @@ export async function transpileCode(code, hasToReport) {
   return await transform(code, hasToReport);
 }
 
-/** @type {null} */
-let node = null;
-
-export async function getIpfsiD() {
-  if (node) return node;
-
-  let ipfsId = await shaDB.get("ipfs", "string");
-  if (!ipfsId) {
-    ipfsId = v4();
-
-    await shaDB.put("ipfs", ipfsId);
-  }
-
-  return ipfsId;
-}
-
-/** @type {any} */
-let ipfsNode;
-/**
- * 
- * @param {string} code 
- * @param {boolean} hasToReport 
- */
-export async function ipfsAdd(code, hasToReport) {
-  ///@ts-ignore
-  ipfsNode = ipfsNode || await Ipfs.create({ repo: await getIpfsiD() });
-
-  return await transform(code, hasToReport);
-}
-
-function init() {
+async function init() {
   const v = versions();
 
-  const worker = new Worker(
+  const res = await fetch(
     `https://unpkg.com/@zedvision/code@${v.code}/src/transpile.worker.js`,
+  );
+  const workerSource = await res.text();
+  const worker = new Worker(
+    URL.createObjectURL(new Blob([workerSource.replace("$$ipfs$$", v.ipfs)])),
+  );
+
+  const Comlink = await import(
+    `https://unpkg.com/comlink@${v.comlink}/dist/esm/comlink.mjs`
   );
   // WebWorkers use `postMessage` and therefore work with Comlink.
   transform = Comlink.wrap(worker);
