170a164783320d1844440977aa7ea6e3a996ba55
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-31 11:55:38 +0000

    pre-release

--- packages/code/src/starter.js
+++ packages/code/src/starter.js
@@ -1,41 +1,25 @@
 export const starter = `import { useState } from "react";
 /** @jsx jsx */
-import { motion } from "framer-motion";
 import { css, jsx, Global } from "@emotion/react";;
 
-const Slider = () => {
-  const [sliderValue, setSlider] = useState(64);
+const Hello = () => {
+  const [name, setName] = useState(0);
 
-  return <>
-  <Global styles={css\`
-      body{
-          margin: 0;
-          background: rgb(\${sliderValue},\${255-sliderValue},255);
-          overflow: overlay;
-        }  
-    \`}/>
-    <input max="128"
-      css={\`
-        appearance: none;
-        width: 100%;
-        height: 40px; 
-        background: rgb(\${sliderValue*2},\${255-2*sliderValue},0); 
-        outline: none; 
-    \`} type="range"
-      value={sliderValue} step="1"
-      onChangeCapture={(e) => setSlider(Number(e.currentTarget.value))}>
-    </input>
-    <motion.p
-      animate={{ fontSize: sliderValue + \`px\` }}>
-      Example when the text gets bigger...
-    </motion.p>
-      <motion.p animate={{fontSize:128-sliderValue+"px"}}>
-        ...or smaller
-    </motion.p>
-  </>
+  return <div>
+    <h1>
+      Hello {name}! 
+    </h1>
+    <input value={name} onChange={(e)=>setName(e.target.value)} /> 
+  </div>
 }
 
 export default () => <>
+  <Global styles={css\`
+      body{
+          margin: 0;
+          overflow: overlay;
+        }  
+    \`}/>
   <Slider />
 </>
 `;
