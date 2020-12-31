9f8fc4acda6b382f73a6e8f6b17f6bb6c1acbf4a
Zoltan Erdos <zoltan.erdos@me.com>
2020-12-31 08:20:06 +0000

    pre-release

--- packages/cf-worker/src/handler.ts
+++ packages/cf-worker/src/handler.ts
@@ -196,13 +196,13 @@ export async function handleCloudRequest(request: Request): Promise<Response> {
       }
     }
 
-//    const resp= await fetch("https://unpkg.com/@zedvision/code@11.0.2/ipfs.html");
+    //    const resp= await fetch("https://unpkg.com/@zedvision/code@11.0.2/ipfs.html");
 
     // return text("ello")
 
     // if (request.url==="")
-    
-  return Response.redirect("https://blog.zed.vision", 301);
+
+    return Response.redirect("https://blog.zed.vision", 301);
   } else if (request.method === "POST") {
     // if (pathname==="/add"){
 
