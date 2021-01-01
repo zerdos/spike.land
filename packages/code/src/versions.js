037241fda62f181311049a111aba5bfd630e0686
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-01 15:55:57 +0000

    pre-release

--- packages/code/src/versions.js
+++ packages/code/src/versions.js
@@ -2,8 +2,8 @@ export default function () {
   const v = {
     ipfs: "QmWYS3GcnXAgPqAFHewUM4mZPNctioD1inV2e9vJgUPNec",
     babel: "7.12.12",
-    code: "11.0.15",
-    emotionRenderer: "10.13.3",
+    code: "11.1.0",
+    emotionRenderer: "11.0.20",
     shadb: "10.13.18",
     prettier: "2.2.1",
     editor: "10.14.6",
