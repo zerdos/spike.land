a04cc37c16fed99ed0c84de8b01bf217ac26618d
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-04 10:14:31 +0000

    pre-release

--- packages/cf-code-zed-vision/index.js
+++ packages/cf-code-zed-vision/index.js
@@ -1,27 +1,39 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  const url = new URL(request.url);
-  const { searchParams, pathname } = url;
+(() => {
+  // ../code/src/versions.js
+  function versions_default() {
+    const v = {
+      ipfs: "0.52.4-rc.4",
+      babel: "7.12.12",
+      code: "11.2.2",
+      emotionRenderer: "11.2.0",
+      shadb: "11.2.0",
+      prettier: "2.2.1",
+      editor: "11.2.0",
+      diff: "11.2.0",
+      sha256: "11.0.5",
+      uuid: "8.3.2",
+      comlink: "4.3.0"
+    };
+    return v;
+  }
 
-
+  // src/index.js
+  addEventListener("fetch", (event) => {
+    event.respondWith(handleRequest(event.request));
+  });
+  async function handleRequest(request) {
+    const url = new URL(request.url);
+    const {searchParams, pathname} = url;
+    const v = versions_default();
     const cache = caches.default;
- //  await cache.delete(request)
-    let response = await cache.match(request)
-  
-    if (!response || response.url!=="https://unpkg.com/@zedvision/code@11.2.2/ipfs.html") {
-      response = await fetch(`https://unpkg.com/@zedvision/code@11.2.2/ipfs.html`)
-      await cache.put(request, response.clone())
+    let response = await cache.match(request);
+    if (!response || response.url !== `https://unpkg.com/@zedvision/code@${v.code}/ipfs.html`) {
+      response = await fetch(`https://unpkg.com/@zedvision/code@${v.code}/ipfs.html`);
+      await cache.put(request, response.clone());
     }
     if (response.status > 399) {
-      response = new Response(response.statusText, { status: response.status })
+      response = new Response(response.statusText, {status: response.status});
     }
-    return response
-
-}
-
+    return response;
+  }
+})();
---
+++ packages/cf-code-zed-vision/src/index.js
@@ -0,0 +1,31 @@
+import versions from "@zedvision/code/src/versions.js"
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  const { searchParams, pathname } = url;
+
+
+  const v = versions();
+
+    const cache = caches.default;
+ //  await cache.delete(request)
+    let response = await cache.match(request)
+  
+    if (!response || response.url!==`https://unpkg.com/@zedvision/code@${v.code}/ipfs.html`) {
+      response = await fetch(`https://unpkg.com/@zedvision/code@${v.code}/ipfs.html`)
+      await cache.put(request, response.clone())
+    }
+    if (response.status > 399) {
+      response = new Response(response.statusText, { status: response.status })
+    }
+    return response
+
+}
+
