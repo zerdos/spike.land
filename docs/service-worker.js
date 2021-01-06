381a92070db47a18ef9c7d0f0c186ad6fc89b588
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-06 12:26:07 +0000

    pre-release

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -114,7 +114,7 @@ define("./service-worker.js",['./workbox-ac194f0a'], function (workbox) { 'use s
     "revision": null
   }, {
     "url": "/render-page.js",
-    "revision": "434c7d04134f142d39ae02e44c7a25a7"
+    "revision": "cffde0e318747a62abc09c069203a468"
   }, {
     "url": "/static/forkMe-1a2bcb7dd2c870c3afe5d50466500c18.png",
     "revision": "1a2bcb7dd2c870c3afe5d50466500c18"
