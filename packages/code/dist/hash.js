e99e8bb9347fae4cf56a15da0bab8499ee224448
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-06 19:28:27 +0000

    pre-release

--- packages/code/dist/hash.js
+++ packages/code/dist/hash.js
@@ -4,7 +4,10 @@ const v = versions();
  *
  * @param {string} cid
  */
-const feedTheCache = (cid) => fetch(`https://code.zed.vision/ipfs/${cid}`).then((resp) => resp.text()).then(console.log);
+const feedTheCache = (cid) => console.log(cid);
+// fetch(`https://code.zed.vision/ipfs/${cid}`).then((resp) => resp.text()).then(
+//   console.log,
+// );
 /**
  * @param {string | any[]} data
  */
