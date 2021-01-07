5ced61f38134c860c4a4850ecbd0b03065f7e5a4
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-07 10:38:27 +0000

    pre-release

--- docs/f45620896e764e562bc09d27e8604e16dc4e935a.js
+++ docs/f45620896e764e562bc09d27e8604e16dc4e935a.js
@@ -1524,7 +1524,7 @@ tags.forEach(function (tagName) {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony default export */ __webpack_exports__["a"] = (function(){var code="11.4.19";var shadb="11.4.16";var editor="11.3.0";var emotionRenderer="11.4.7";var v={code:code,shadb:shadb,ipfs:"0.52.3",workbox:"6.0.2",babel:"7.12.12",prettier:"2.2.1",uuid:"8.3.2",comlink:"4.3.0",editor:"https://unpkg.com/@zedvision/smart-monaco-editor@"+editor+"/dist/editor.js",DraggableWindow:"https://blog.zed.vision/ipfs/QmPszD6UC3kYc3WRCttGFKqnqvSUvrSxvVbAouNRSQTsHF/app.js",emotionRenderer:"https://unpkg.com/@zedvision/emotion-react-renderer@"+emotionRenderer+"/dist/bundle.js"};return v;});
+/* harmony default export */ __webpack_exports__["a"] = (function(){var code="11.5.1";var shadb="11.4.16";var editor="11.3.0";var emotionRenderer="11.4.7";var v={code:code,shadb:shadb,ipfs:"0.52.3",workbox:"6.0.2",babel:"7.12.12",prettier:"2.2.1",uuid:"8.3.2",comlink:"4.3.0",editor:"https://unpkg.com/@zedvision/smart-monaco-editor@"+editor+"/dist/editor.js",DraggableWindow:"https://blog.zed.vision/ipfs/QmPszD6UC3kYc3WRCttGFKqnqvSUvrSxvVbAouNRSQTsHF/app.js",emotionRenderer:"https://unpkg.com/@zedvision/emotion-react-renderer@"+emotionRenderer+"/dist/bundle.js"};return v;});
 
 /***/ }),
 
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -114,7 +114,7 @@ define("./service-worker.js",['./workbox-ac194f0a'], function (workbox) { 'use s
     "revision": null
   }, {
     "url": "/render-page.js",
-    "revision": "f8f9300e24cdbabdd60cedf48ec82861"
+    "revision": "c3a97436a698b3cf934b857b518d8261"
   }, {
     "url": "/static/forkMe-1a2bcb7dd2c870c3afe5d50466500c18.png",
     "revision": "1a2bcb7dd2c870c3afe5d50466500c18"
