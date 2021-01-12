bd94b68c2299c7935a2a1ece92a0dca0ac276ee3
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-12 20:01:30 +0000

    pre-release

--- docs/16-40f3e483eaead5aa6d88.js
+++ docs/16-40f3e483eaead5aa6d88.js
@@ -1,6 +1,6 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[16],{
 
-/***/ "zE1M":
+/***/ "yTfI":
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -36,11 +36,11 @@ function(x){return Object.assign(ret,window[x]);});resolve(ret);};s.onerror=reje
  * @param {string} src
  * @param {string} cssId
  */var importCss=function importCss(src,cssId){if(!window.document.getElementById(cssId)){var head=window.document.getElementsByTagName("head")[0];var link=window.document.createElement("link");link.id=cssId;link.rel="stylesheet";link.type="text/css";link.href=src;link.media="all";head.appendChild(link);}return true;};
-// CONCATENATED MODULE: ../code/dist/ipfs.client.js
+// CONCATENATED MODULE: ../code/dist/ipfsClient.js
 //@ts-ignore
-var ipfs;var getIpfs=/*#__PURE__*/function(){var _ref=Object(asyncToGenerator["a" /* default */])(/*#__PURE__*/regenerator_default.a.mark(function _callee(){var _yield$importScript,IpfsMessagePortClient,workerSrc,worker,_worker;return regenerator_default.a.wrap(function _callee$(_context){while(1){switch(_context.prev=_context.next){case 0:if(!ipfs){_context.next=2;break;}return _context.abrupt("return",ipfs);case 2:_context.next=4;return importScript("https://unpkg.com/ipfs-message-port-client@0.4.3/dist/index.min.js");case 4:_yield$importScript=_context.sent;IpfsMessagePortClient=_yield$importScript.IpfsMessagePortClient;workerSrc=window.location.hostname==="blog.zed.vision"?"https://blog.zed.vision/code/src/ipfs.shared.worker.js":window.location.hostname==="[::1]"?location.href+"src/ipfs.shared.worker.js":location.origin+"/src/ipfs.shared.worker.js";console.log(workerSrc);if(typeof SharedWorker!=="undefined"){console.log("FROM WORKER YEAAH");worker=new SharedWorker(workerSrc);ipfs=IpfsMessagePortClient.from(worker.port);}else{_worker=new Worker(workerSrc);ipfs=IpfsMessagePortClient.from(_worker);}return _context.abrupt("return",ipfs);case 10:case"end":return _context.stop();}}},_callee);}));return function getIpfs(){return _ref.apply(this,arguments);};}();
+var ipfs;var getIpfs=/*#__PURE__*/function(){var _ref=Object(asyncToGenerator["a" /* default */])(/*#__PURE__*/regenerator_default.a.mark(function _callee(){var _yield$importScript,IpfsMessagePortClient,workerSrc,worker,_worker,_MessageChannel,port1,port2;return regenerator_default.a.wrap(function _callee$(_context){while(1){switch(_context.prev=_context.next){case 0:if(!ipfs){_context.next=2;break;}return _context.abrupt("return",ipfs);case 2:_context.next=4;return importScript("https://unpkg.com/ipfs-message-port-client@0.4.3/dist/index.min.js");case 4:_yield$importScript=_context.sent;IpfsMessagePortClient=_yield$importScript.IpfsMessagePortClient;workerSrc=window.location.hostname==="blog.zed.vision"?"https://blog.zed.vision/code/src/ipfsWorker.js":window.location.hostname==="[::1]"?location.href+"src/ipfsWorker.js":location.origin+"/src/ipfsWorker.js";if(typeof SharedWorker!=="undefined"){worker=new SharedWorker(workerSrc);ipfs=IpfsMessagePortClient.from(worker.port);}else{_worker=new Worker(workerSrc);_MessageChannel=new MessageChannel(),port1=_MessageChannel.port1,port2=_MessageChannel.port2;port1.onmessage=function(e){_worker.postMessage(e.data,[e.data]);};_worker.onmessage=function(e){port1.postMessage(e.data,[e.data]);};ipfs=IpfsMessagePortClient.from(port2);}return _context.abrupt("return",ipfs);case 9:case"end":return _context.stop();}}},_callee);}));return function getIpfs(){return _ref.apply(this,arguments);};}();
 
 /***/ })
 
 }]);
-//# sourceMappingURL=16-a8491617b0a2da20f11e.js.map
+//# sourceMappingURL=16-40f3e483eaead5aa6d88.js.map
