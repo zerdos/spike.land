99b609ca06178325395d8ed59836ba2d7e214a5b
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-15 18:38:00 +0000

    pre-release

--- packages/ipfs/src/ipfs.ts
+++ packages/ipfs/src/ipfs.ts
@@ -1,9 +1,11 @@
 
 
-import Ipfs from "ipfs/dist/index.min.js"
+import * as IPFS from "ipfs/dist/index.min.js"
 import IpfsClient from "ipfs-message-port-client"
 import { IPFSService, Server } from "ipfs-message-port-server"
 import CID from "cids"
+
+const Ipfs = {create: (opts)=>IPFS.create(opts)}
 
 // export const Ipfs = {Ipfs: {create: IPFS.create}}
 export {IpfsClient}
