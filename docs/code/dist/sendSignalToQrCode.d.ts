3be3117001128a4bd0b1ae11c799fe00dd889e5e
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-16 09:17:34 +0000

    pre-release

--- docs/code/dist/sendSignalToQrCode.d.ts
+++ docs/code/dist/sendSignalToQrCode.d.ts
@@ -5,4 +5,4 @@ export function sendSignalToQrCode(rootUrl: string): Promise<void>;
 /**
  * @param {string} hash
  */
-export function getZkey(hash: string): Promise<string>;
+export function getZkey(hash: string): Promise<`${string}${any}${any}${any}`>;
