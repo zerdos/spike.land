ee9a6ff995ee1d29d3ccf35a027fd7176ec78d6c
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-20 11:35:26 +0000

    pre-release

--- cloudflare/cf-code-zed-vision/src/index.ts
+++ cloudflare/cf-code-zed-vision/src/index.ts
@@ -143,6 +143,7 @@ async function alterHeaders(response: Response, pathname: string) {
       "text/css;charset=UTF-8",
     );
   } else if (pathname.endsWith(".jpg")) {
+    resp.headers.delete("content-type");
     resp.headers.set("content-type", "image/jpeg");
   } else if (pathname.indexOf(".") === -1) {
     resp.headers.delete("content-type"),
