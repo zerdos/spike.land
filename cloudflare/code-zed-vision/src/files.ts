cbe5e7169557b8b5163b815027a7e0c068119c88
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-21 17:18:45 +0000

    pre-release

--- cloudflare/code-zed-vision/src/files.ts
+++ cloudflare/code-zed-vision/src/files.ts
@@ -617,6 +617,7 @@ export const files = {
   "src/templates.js": "QmbbHgU51zh5JuuAPLsxHngGsyEmjYX3o8LA8dtDph18UJ",
   "src/transpile.js": "QmcqWzx4L1c3dfG7jrwQqy23wrx9mn6jgjoEAxmGyfEsPp",
   "src/workBox.js": "QmUNb95Uy4M1qwMACE8oBvhEP2SYyUCc5gHaAYBs5YpV68",
+  "src/workers/files.umd.js": "QmUSsWjoGoBygMHNLTourwZxnmPMLPj7mGBasRNCaUtPvH",
   "src/workers/ipfs/README.md":
     "QmbdTTJXo4rWcSfDFooCBXWGYtCnbixPpSJh9hjAQ6KMFw",
   "src/workers/ipfs/dist/gateways.d.ts":
@@ -944,8 +945,8 @@ export const files = {
     "QmSxTFf8YACtJvwT4M2s7sJ4yv4PFB4LYViYLt7vMyMtHJ",
   "src/workers/ipfs/src": "QmUqoKmHckW1HgxPr5nprzQiwHLgsuryoGeuHFjJiYCeo1",
   "src/workers/ipfs": "QmSKnk75qLtX4DtfB6R3xtuUG3jYtnwPkpLa1zCjFe6fmF",
-  "src/workers": "QmVF23hYaaK4bpMrg3WZLgXtPUQS6oAHWKP4xfpg2VXPRU",
-  "src": "QmUQGE88acdHceu74uyUBvQMYeLNMqWGvRzHngxaDQfjw4",
-  "": "QmVmnAMAqL1LQrjJbK6V66rBWUCQGBLMCAqhTr9W1sxmqV",
+  "src/workers": "QmbRTZ8LDhmmEbn6vXcB3NzSSNMPWoiC4GxsGv3G1Cxrcf",
+  "src": "Qme5Hqdc7vUPLUSM9Ca1rsmmtcQmntMsJSYDVsH5BXwWsC",
+  "": "QmPduw5Zbqb1kGMhyT5KTSuWbvoey1XzhsEbcpyRNCnfP4",
   "foo": "bar",
 };
