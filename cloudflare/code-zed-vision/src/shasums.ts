259ed660a08c596b39aa9424b3db3464f8c057b3
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-22 09:10:51 +0000

    pre-release

--- cloudflare/code-zed-vision/src/shasums.ts
+++ cloudflare/code-zed-vision/src/shasums.ts
@@ -30,7 +30,7 @@ export const shasums = {
   ".gitignore":
     "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
   ".tsBuildInfo":
-    "083b33565766de6d0a7dda0b50b4ab8badbaaa55ae90848b302300af428f07f4",
+    "8ec6815e5a5542cbc4a720a9eca421c4af68d0c1ce65ce0f7331cc7e88d991c1",
   ".ignore": "bcae0416621b3db43d8cba1e4454a792c7af9e4b7287e51056efc5a215d95839",
   "manifest.json":
     "474c4e928619e360478a2f959a69c26d5028933dd5e3d2b2814182548a769591",
@@ -566,10 +566,10 @@ export const shasums = {
     "8e609bb71c20b858c77f0e9f90bb1319db8477b13f9f965f1a1e18524bf50881",
   "src/smart-monaco-editor/dist/monaco.js":
     "5e0258ef425bc23888eb413c71963f531dbb51f021f9ffc820576dab13c7a9e0",
-  "src/smart-monaco-editor/dist/editor.d.ts":
-    "ccde8810403d283347f8039e8e3804c6229cb9c3c9725c6b87d6602b4cd62e9f",
   "src/smart-monaco-editor/dist/monaco.d.ts":
     "d3513cac4bd5a2e18e2effa2da4e87cca4bdd071b5432993bf67a3391fd9c85f",
+  "src/smart-monaco-editor/dist/editor.d.ts":
+    "ccde8810403d283347f8039e8e3804c6229cb9c3c9725c6b87d6602b4cd62e9f",
   "src/smart-monaco-editor/dist/editor.js":
     "4247f235d2bc76739e2014d7d5d7642ade88d8d087508d45ec542df6fb7157aa",
   "src/workBox.js":
@@ -588,10 +588,10 @@ export const shasums = {
     "a65e9f5e35f77fb41f2ff08d40c7e706570fd61660cf8861b44f35bc0038f0d9",
   "src/refs.d.ts":
     "b012039d5e023b31d5999c01ed91164137d92285087565387581f32708db5d2d",
-  "src/emotion-react-renderer/dist/renderer.js":
-    "1d9c84bd8ad165eaf51c33a428e9207c05533eee90698f3394185b7ffeb99b30",
   "src/emotion-react-renderer/dist/DraggableWindow.js":
     "2af4d70af0183fd00b01a74525373bfddce6f49ad23d3c6e8b0bf16caebd0f1b",
+  "src/emotion-react-renderer/dist/renderer.js":
+    "1d9c84bd8ad165eaf51c33a428e9207c05533eee90698f3394185b7ffeb99b30",
   "src/emotion-react-renderer/package.json":
     "0b9908dcfb5f94afd9e80119d3735a7d9d1f57b4d9449d423a8d1e2cc097a2d0",
   "src/emotion-react-renderer/tsconfig.json":
@@ -616,18 +616,18 @@ export const shasums = {
     "d1062891648dcc1c8000285deb9f4375ebb910c51da72a700198866811ebbc02",
   "src/workers/ipfs/.gitignore":
     "dba27c31aad935787bb275c3e5e4e957708f15386de599eff1db476022cd7e4c",
-  "src/workers/ipfs/dist/ipfs.client.js":
-    "6981ffd610fd63cbbe09630f5116b3d3cb7275acc4a4d1cbc3e668ef06bdea71",
-  "src/workers/ipfs/dist/ipfs.server.js":
-    "c3906575a0da7e93fcab6707e64cefaa04601097d39121e93559aebdb4e0247b",
-  "src/workers/ipfs/dist/gateways.d.ts":
-    "cf362fc8a73de409069bd6b64ffdb58b272525eec768534dda2b8bee65932faa",
   "src/workers/ipfs/dist/gateways.js":
     "908a3ed7447294f9d7e3de059995f60aeb832e30f06af2118fc292482829b3e6",
+  "src/workers/ipfs/dist/ipfs.client.js":
+    "6981ffd610fd63cbbe09630f5116b3d3cb7275acc4a4d1cbc3e668ef06bdea71",
   "src/workers/ipfs/dist/ipfs.server.d.ts":
     "8e609bb71c20b858c77f0e9f90bb1319db8477b13f9f965f1a1e18524bf50881",
+  "src/workers/ipfs/dist/gateways.d.ts":
+    "cf362fc8a73de409069bd6b64ffdb58b272525eec768534dda2b8bee65932faa",
   "src/workers/ipfs/dist/ipfs.client.d.ts":
     "6be375caa009446112051476fe3a57e6457d4182d78f4d186aed5dc9434fc0f5",
+  "src/workers/ipfs/dist/ipfs.server.js":
+    "c3906575a0da7e93fcab6707e64cefaa04601097d39121e93559aebdb4e0247b",
   "src/workers/ipfs/node_legacy/ipfs.min.js":
     "2d608ef59d1e8323dd3e4233f63cd755416ee4c16f968d5aed9bb1b7b1d1000a",
   "src/workers/ipfs/node_legacy/server.min.js":
@@ -720,36 +720,36 @@ export const shasums = {
     "19dfa82f69b84860a9c249e791e3f445cd9cbe13abe9af02459c8499d3a5786e",
   "src/shadb/dist/getDbObj.d.ts":
     "ef26ee980b42b1068059a8da73b1df08cb679839566d9d99f09bd4c90d1712f8",
-  "src/shadb/dist/diff.d.ts":
-    "24433731e7c49c8dc3fda87f228fb35117d7d9ec2f63356bb79c4d5d1c8673d6",
-  "src/shadb/dist/sha256.d.ts":
-    "ff7ef3c93a6e9ee5043735b245c64d7926fe934a803a2b3f38258f2725958ebf",
-  "src/shadb/dist/diffChars.d.ts":
-    "6d21fa304e45c032ddf353f27ab96535975792fd58886c1b5c64dcbf41b989f5",
   "src/shadb/dist/diffChars.js":
     "ae92a69d0b35ff74fc7760f1afeb88bd16ae7a251b7dc80d071aa894c9a66a78",
-  "src/shadb/dist/vendor/idb.js":
-    "2aa52ffdf9a5ec1ee952b41dc54bd13822b3ddc67f531f4743154c75fc33b6a6",
-  "src/shadb/dist/vendor/diffMin.d.ts":
-    "49467f7acb2d82b4308b34ea63b4c329af2586256c77a6b4042b0c886809bdcd",
-  "src/shadb/dist/vendor/diffChars.js":
-    "7d8f62c0ddf5f2abc2ddeae42f8a460853065d7da33005bbae97c504de3437ee",
-  "src/shadb/dist/vendor/idb.d.ts":
-    "bb8fefeb16e745a0d62bf8eb29c6dc16d44f9c6d5c4c1ae6062e03dff605f125",
-  "src/shadb/dist/vendor/diffChars.d.ts":
-    "a63f34437d24b296a89baefc9ee6b00e507ffa2c3064bb45a9057055505f2f2a",
-  "src/shadb/dist/vendor/diffMin.js":
-    "43d1f239704c26af851b802d257b637dd7d9a8719ddb0f317ec3641e8801c88d",
-  "src/shadb/dist/shaDB.js":
-    "4f91f4553d0d322a41ed224fdc0ee2307cf7468d3ab04481f1985a7d8fde9a0c",
   "src/shadb/dist/sha256.js":
     "9282f9b890811885468f6551e5f8477efa93d6c92948c65bf6fd789427049802",
+  "src/shadb/dist/shaDB.js":
+    "4f91f4553d0d322a41ed224fdc0ee2307cf7468d3ab04481f1985a7d8fde9a0c",
+  "src/shadb/dist/vendor/diffChars.d.ts":
+    "a63f34437d24b296a89baefc9ee6b00e507ffa2c3064bb45a9057055505f2f2a",
+  "src/shadb/dist/vendor/idb.js":
+    "2aa52ffdf9a5ec1ee952b41dc54bd13822b3ddc67f531f4743154c75fc33b6a6",
+  "src/shadb/dist/vendor/diffMin.js":
+    "43d1f239704c26af851b802d257b637dd7d9a8719ddb0f317ec3641e8801c88d",
+  "src/shadb/dist/vendor/diffChars.js":
+    "7d8f62c0ddf5f2abc2ddeae42f8a460853065d7da33005bbae97c504de3437ee",
+  "src/shadb/dist/vendor/diffMin.d.ts":
+    "49467f7acb2d82b4308b34ea63b4c329af2586256c77a6b4042b0c886809bdcd",
+  "src/shadb/dist/vendor/idb.d.ts":
+    "bb8fefeb16e745a0d62bf8eb29c6dc16d44f9c6d5c4c1ae6062e03dff605f125",
   "src/shadb/dist/getDbObj.js":
     "83ec716216cc06cc082b065b23cba6509fb26ae1f5fa18ca36a773abad861b8b",
   "src/shadb/dist/diff.js":
     "6294b2f64cf78c4c6f2bad84192f639807ed27b1a5b0e51aff8d791df4c7a04a",
+  "src/shadb/dist/diffChars.d.ts":
+    "6d21fa304e45c032ddf353f27ab96535975792fd58886c1b5c64dcbf41b989f5",
+  "src/shadb/dist/diff.d.ts":
+    "24433731e7c49c8dc3fda87f228fb35117d7d9ec2f63356bb79c4d5d1c8673d6",
   "src/shadb/dist/shaDB.d.ts":
     "59fc351c34125b205dbd5f2763c961901a7fc719f274500b6a8557986be5fbf7",
+  "src/shadb/dist/sha256.d.ts":
+    "ff7ef3c93a6e9ee5043735b245c64d7926fe934a803a2b3f38258f2725958ebf",
   "src/shadb/package.json":
     "2c43aad174139e658d4e97f2ddd5a1936968b2eebcfbb8575f46568d77679353",
   "tsconfig.json":
