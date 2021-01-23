210407a5a93bc2039d30d9b3c7d80b6bcefc1dad
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-23 20:57:51 +0000

    pre-release

--- cloudflare/code-zed-vision/src/shasums.ts
+++ cloudflare/code-zed-vision/src/shasums.ts
@@ -30,7 +30,7 @@ export const shasums = {
   ".gitignore":
     "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
   ".tsBuildInfo":
-    "6451cc29dccc1c293abd8240a86b95dea3818a07266dc30202a4e3c5ae6ccd4e",
+    "ac09915a7af4090b16bae3e3d5ffdffc9609842b71ff54fcb73f3bacd038cc10",
   "index2.html":
     "a4e7e11765c8212f6426d77ca1f222064839be9637e59ce85cad9431be906c97",
   ".ignore": "bcae0416621b3db43d8cba1e4454a792c7af9e4b7287e51056efc5a215d95839",
