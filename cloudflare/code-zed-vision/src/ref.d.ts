d26ec29d3dc90480d3e7eaf199a9fd8b8c8a5f63
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-25 13:00:59 +0000

    pre-release

--- cloudflare/code-zed-vision/src/ref.d.ts
+++ cloudflare/code-zed-vision/src/ref.d.ts
@@ -2,5 +2,4 @@ declare var SHAKV: KVNamespace;
 declare var USERKEYS: KVNamespace;
 declare var USERS: KVNamespace;
 declare var LOGS: KVNamespace;
-declare var IPFSKV: KVNamespace;
 declare var IPFS: KVNamespace;
