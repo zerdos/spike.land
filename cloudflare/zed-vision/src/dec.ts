1a5ecc6c0aa2ee1b1ac80d78fc65da1ab748eb60
zed <zolika84@gmail.com>
2021-03-17 16:21:38 +0000

    pre-release

--- cloudflare/zed-vision/src/dec.ts
+++ cloudflare/zed-vision/src/dec.ts
@@ -1,9 +1,6 @@
 // deno-lint-ignore-file
 import gen from "https://unpkg.com/uuid@8.3.2/dist/esm-browser/v4.js";
 
-export type {
-  FetchEvent,
-  KVNamespace,
-} from "../../../node_modules/@cloudflare/workers-types/index.d.ts";
+export type { FetchEvent, KVNamespace } from "@cloudflare/workers-types";
 
 export const v4 = () => gen() as unknown as string;
