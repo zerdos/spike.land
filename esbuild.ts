18ef4e92030eb747c4350c77e0da719e70d40583
Zoltan Erdos <zolika84@gmail.com>
2021-04-15 11:19:27 +0100

    pre-release

---
+++ esbuild.ts
@@ -0,0 +1,5 @@
+import * as esbuild from 'https://deno.land/x/esbuild@v0.11.11/mod.js'
+const ts = 'let hasProcess: boolean = typeof process != "null"'
+const result = await esbuild.transform(ts, { loader: 'ts', logLevel: 'warning' })
+console.log('result:', result)
+esbuild.stop()
