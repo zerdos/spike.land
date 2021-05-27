9c1aecd4d5563a84215570a5e571c40fa0f5e7c2
Zed <zolika84@gmail.com>
2021-05-27 14:03:30 +0100

    pre-release

--- packages/code/modules/vendor/idb.js
+++ packages/code/modules/vendor/idb.js
@@ -25,8 +25,8 @@ const reverseTransformCache = new WeakMap();
 function promisifyRequest(request) {
     const promise = new Promise((resolve, reject) => {
         const unlisten = () => {
-            request.removeEventListener("success", success);
-            request.removeEventListener("error", error);
+            request.removeEventListener('success', success);
+            request.removeEventListener('error', error);
         };
         const success = () => {
             resolve(wrap1(request.result));
@@ -36,8 +36,8 @@ function promisifyRequest(request) {
             reject(request.error);
             unlisten();
         };
-        request.addEventListener("success", success);
-        request.addEventListener("error", error);
+        request.addEventListener('success', success);
+        request.addEventListener('error', error);
     });
     promise.then((value) => {
         if (value instanceof IDBCursor) {
@@ -53,36 +53,34 @@ function cacheDonePromiseForTransaction(tx) {
         return;
     const done = new Promise((resolve, reject) => {
         const unlisten = () => {
-            tx.removeEventListener("complete", complete);
-            tx.removeEventListener("error", error);
-            tx.removeEventListener("abort", error);
+            tx.removeEventListener('complete', complete);
+            tx.removeEventListener('error', error);
+            tx.removeEventListener('abort', error);
         };
         const complete = () => {
             resolve();
             unlisten();
         };
         const error = () => {
-            reject(tx.error || new DOMException("AbortError", "AbortError"));
+            reject(tx.error || new DOMException('AbortError', 'AbortError'));
             unlisten();
         };
-        tx.addEventListener("complete", complete);
-        tx.addEventListener("error", error);
-        tx.addEventListener("abort", error);
+        tx.addEventListener('complete', complete);
+        tx.addEventListener('error', error);
+        tx.addEventListener('abort', error);
     });
     transactionDoneMap.set(tx, done);
 }
 let idbProxyTraps = {
     get(target, prop, receiver) {
         if (target instanceof IDBTransaction) {
-            if (prop === "done")
+            if (prop === 'done')
                 return transactionDoneMap.get(target);
-            if (prop === "objectStoreNames") {
+            if (prop === 'objectStoreNames') {
                 return target.objectStoreNames || transactionStoreNamesMap.get(target);
             }
-            if (prop === "store") {
-                return receiver.objectStoreNames[1]
-                    ? undefined
-                    : receiver.objectStore(receiver.objectStoreNames[0]);
+            if (prop === 'store') {
+                return receiver.objectStoreNames[1] ? undefined : receiver.objectStore(receiver.objectStoreNames[0]);
             }
         }
         return wrap1(target[prop]);
@@ -92,22 +90,21 @@ let idbProxyTraps = {
         return true;
     },
     has(target, prop) {
-        if (target instanceof IDBTransaction && (prop === "done" || prop === "store")) {
+        if (target instanceof IDBTransaction && (prop === 'done' || prop === 'store')) {
             return true;
         }
         return prop in target;
-    },
+    }
 };
 function replaceTraps(callback) {
     idbProxyTraps = callback(idbProxyTraps);
 }
 function wrapFunction(func) {
-    if (func === IDBDatabase.prototype.transaction &&
-        !("objectStoreNames" in IDBTransaction.prototype)) {
+    if (func === IDBDatabase.prototype.transaction && !('objectStoreNames' in IDBTransaction.prototype)) {
         return function (storeNames, ...args) {
             const tx = func.call(unwrap1(this), storeNames, ...args);
             transactionStoreNamesMap.set(tx, storeNames.sort ? storeNames.sort() : [
-                storeNames,
+                storeNames
             ]);
             return wrap1(tx);
         };
@@ -123,13 +120,12 @@ function wrapFunction(func) {
     };
 }
 function transformCachableValue(value) {
-    if (typeof value === "function")
+    if (typeof value === 'function')
         return wrapFunction(value);
     if (value instanceof IDBTransaction)
         cacheDonePromiseForTransaction(value);
-    if (instanceOfAny(value, getIdbProxyableTypes())) {
+    if (instanceOfAny(value, getIdbProxyableTypes()))
         return new Proxy(value, idbProxyTraps);
-    }
     return value;
 }
 function wrap1(value) {
@@ -150,17 +146,17 @@ function openDB1(name, version, { blocked, upgrade, blocking, terminated } = {})
     const request = indexedDB.open(name, version);
     const openPromise = wrap1(request);
     if (upgrade) {
-        request.addEventListener("upgradeneeded", (event) => {
+        request.addEventListener('upgradeneeded', (event) => {
             upgrade(wrap1(request.result), event.oldVersion, event.newVersion, wrap1(request.transaction));
         });
     }
     if (blocked)
-        request.addEventListener("blocked", () => blocked());
+        request.addEventListener('blocked', () => blocked());
     openPromise.then((db) => {
         if (terminated)
-            db.addEventListener("close", () => terminated());
+            db.addEventListener('close', () => terminated());
         if (blocking)
-            db.addEventListener("versionchange", () => blocking());
+            db.addEventListener('versionchange', () => blocking());
     }).catch(() => {
     });
     return openPromise;
@@ -168,39 +164,37 @@ function openDB1(name, version, { blocked, upgrade, blocking, terminated } = {})
 function deleteDB1(name, { blocked } = {}) {
     const request = indexedDB.deleteDatabase(name);
     if (blocked)
-        request.addEventListener("blocked", () => blocked());
+        request.addEventListener('blocked', () => blocked());
     return wrap1(request).then(() => undefined);
 }
 const readMethods = [
-    "get",
-    "getKey",
-    "getAll",
-    "getAllKeys",
-    "count",
+    'get',
+    'getKey',
+    'getAll',
+    'getAllKeys',
+    'count'
 ];
 const writeMethods = [
-    "put",
-    "add",
-    "delete",
-    "clear",
+    'put',
+    'add',
+    'delete',
+    'clear'
 ];
 const cachedMethods = new Map();
 function getMethod(target, prop) {
-    if (!(target instanceof IDBDatabase && !(prop in target) &&
-        typeof prop === "string")) {
+    if (!(target instanceof IDBDatabase && !(prop in target) && typeof prop === 'string')) {
         return;
     }
     if (cachedMethods.get(prop))
         return cachedMethods.get(prop);
-    const targetFuncName = prop.replace(/FromIndex$/, "");
+    const targetFuncName = prop.replace(/FromIndex$/, '');
     const useIndex = prop !== targetFuncName;
     const isWrite = writeMethods.includes(targetFuncName);
-    if (!(targetFuncName in (useIndex ? IDBIndex : IDBObjectStore).prototype) ||
-        !(isWrite || readMethods.includes(targetFuncName))) {
+    if (!(targetFuncName in (useIndex ? IDBIndex : IDBObjectStore).prototype) || !(isWrite || readMethods.includes(targetFuncName))) {
         return;
     }
     const method = async function (storeName, ...args) {
-        const tx = this.transaction(storeName, isWrite ? "readwrite" : "readonly");
+        const tx = this.transaction(storeName, isWrite ? 'readwrite' : 'readonly');
         let target1 = tx.store;
         if (useIndex)
             target1 = target1.index(args.shift());
@@ -215,6 +209,6 @@ function getMethod(target, prop) {
 replaceTraps((oldTraps) => ({
     ...oldTraps,
     get: (target, prop, receiver) => getMethod(target, prop) || oldTraps.get(target, prop, receiver),
-    has: (target, prop) => !!getMethod(target, prop) || oldTraps.has(target, prop),
+    has: (target, prop) => !!getMethod(target, prop) || oldTraps.has(target, prop)
 }));
 export { deleteDB1 as deleteDB, openDB1 as openDB };
