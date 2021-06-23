9e1852eb56cc0f4f3bc3bc1588863c24f652278b
Zoltan Erdos <zolika84@gmail.com>
2021-06-23 09:25:03 +0100

    pre-release

--- worker/src/spike.land/logs.ts
+++ worker/src/spike.land/logs.ts
@@ -1,5 +1,8 @@
 import { json } from "@responds";
 export async function logs() {
   const list = await LOGS.list();
-  return json(list);
+  const dates = await Promise.all(
+    list.keys.map((x) => LOGS.get(x.name, "json")),
+  );
+  return json(dates);
 }
