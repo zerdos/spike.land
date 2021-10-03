6ef4c490e66a53a5ff97c9f861be136c94a009c9
Zoltan Erdos <zolika84@gmail.com>
2021-10-03 10:55:25 +0100

    pre-release

--- counter/rollup.config.js
+++ counter/rollup.config.js
@@ -1,17 +1,17 @@
-import { terser } from 'rollup-plugin-terser'
+import { terser } from "rollup-plugin-terser";
 // plugin-node-resolve and plugin-commonjs are required for a rollup bundled project
 // to resolve dependencies from node_modules. See the documentation for these plugins
 // for more details.
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: 'src/index.mjs',
+  input: "src/index.mjs",
   output: {
-    exports: 'named',
-    format: 'es',
-    file: 'dist/index.mjs',
+    exports: "named",
+    format: "es",
+    file: "dist/index.mjs",
     sourcemap: true,
   },
   plugins: [commonjs(), nodeResolve({ browser: true }), terser()],
-}
+};
