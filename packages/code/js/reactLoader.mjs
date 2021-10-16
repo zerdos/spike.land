2f61753c9a31fd4b105e0cf558858169561a346e
Zoltan Erdos <zolika84@gmail.com>
2021-10-16 10:52:45 +0100

    window process

--- packages/code/js/reactLoader.mjs
+++ packages/code/js/reactLoader.mjs
@@ -1,20 +1,23 @@
+import { importScript } from "./importScript.mjs";
 
-import {importScript} from "./importScript.mjs"
+export const run = async (mode = "window", code = "") => {
+  if (!window.React) {
+    await (Promise.all([
+      importScript(
+        "https://unpkg.com/react@17.0.2/umd/react.production.min.js",
+      ),
+      importScript(
+        "https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js",
+      ),
+      importScript(
+        "https://unpkg.com/react-is@17.0.2/umd/react-is.production.min.js",
+      ),
+    ]));
+  }
 
-export const run = async (mode = "window", code = "") =>{
+  window.process = { env: { NODE_ENV: "production" } };
 
-if (!window.React) {
-    await(Promise.all([
-      importScript("https://unpkg.com/react@17.0.2/umd/react.production.min.js"),
-      importScript("https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js"),
-      importScript("https://unpkg.com/react-is@17.0.2/umd/react-is.production.min.js"),
-      
-  
-  ]));
-}
-
-
-    const {run: runCode } = await import("../dev.mjs");
+  const { run: runCode } = await import("../dev.mjs");
 
   return runCode(mode, code);
-}
+};
