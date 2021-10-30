2f03c030f3935e66a01e43ba7a807cb1ef8aef06
Zoltan Erdos <zolika84@gmail.com>
2021-10-30 16:29:44 +0100

    new build

--- packages/blog/scripts/debts.sh
+++ packages/blog/scripts/debts.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 if ! [ -x "$(command -v deno)" ]; then
-    curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.15.1
+    curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.15.3
 fi
 
 if ! [ -x "$(command -v file_server)" ]; then
-    deno install --allow-net --allow-read -f https://deno.land/std@0.111.0/http/file_server.ts 
+    deno install --allow-net --allow-read -f https://deno.land/std@0.113.0/http/file_server.ts 
 fi
 
