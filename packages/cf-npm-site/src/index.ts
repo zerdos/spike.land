d11d6e2c4c2c96663abab3c7e4abdf8a5c12633a
Zoltan Erdos <zolika84@gmail.com>
2021-11-01 20:32:47 +0000

    cf-npm-site

---
+++ packages/cf-npm-site/src/index.ts
@@ -0,0 +1,38 @@
+export default function (packageName: string, version: string, serveDir: string = "") {
+  return async function(request: Request, env: EventInit)  {
+    try {
+
+      const url = new URL(request.url);
+      const { pathname } = url;
+
+
+      const uri = (pathname.startsWith("/@")
+      ? pathname.substring(1)
+      : `@${version}${serveDir?`/${serveDir}`:``}${pathname}`);
+
+    let myCache = await caches.open(`blog-npm:${version}-${serveDir}`);
+    const cachedResp = await myCache.match(request, {});
+
+    if (cachedResp) {
+      return cachedResp;
+    } 
+
+    let targetPath = uri;
+
+    if (uri.endsWith("/")) {
+      targetPath = `${uri}index.html`;
+    } else if (pathname.indexOf(".")===-1) {
+      targetPath = `${uri}/index.html`;
+    }
+
+    const resp = fetch(`https://unpkg.com/${packageName}${targetPath}`);
+
+    myCache.put(request, (await resp).clone());
+
+    return resp;
+
+    } catch (Error) {
+      return new Response(`Yayy... ${Object.prototype.toString.call(Error)}`);
+    }
+  }
+}
