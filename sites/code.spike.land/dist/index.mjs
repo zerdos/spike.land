6dc7c4a3efc4fcce27c7346dccb1189758bb35a7
Zoltan Erdos <zolika84@gmail.com>
2021-11-03 13:57:21 +0000

    up

--- sites/code.spike.land/dist/index.mjs
+++ sites/code.spike.land/dist/index.mjs
@@ -37,7 +37,7 @@ var require_wait = __commonJS({
 });
 
 // ../../packages/code/package.json
-var version = "0.0.40";
+var version = "0.0.41";
 
 // ../../packages/cf-npm-site/dist/index.mjs
 function src_default(packageName, version2, serveDir = "") {
