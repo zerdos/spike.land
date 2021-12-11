d4e3ebb4d38bc14d6618ed4fafb4e04f28de711d
Zoltan Erdos <zolika84@gmail.com>
2021-12-11 10:26:46 +0000

    pre-release

--- packages/code/js/workers/babel.worker.js
+++ packages/code/js/workers/babel.worker.js
@@ -1,26 +1,35 @@
-importScripts("https://unpkg.com/comlink@4.3.1/dist/umd/comlink.min.js");
-importScripts(
+self.importScripts("https://unpkg.com/comlink@4.3.1/dist/umd/comlink.min.js");
+self.importScripts(
   "https://unpkg.com/@babel/standalone@7.16.4/babel.min.js",
+);
+
+
+const { Comlink, Babel } = self;
+const { transform } = Babel;
+const { expose } = Comlink;
+
+
+const babelTransform = (code) =>
+transform(
+  code,
+  {
+    compact: false,
+    comments: false,
+    presets: [
+      "react",
+      "es2017",
+      ["typescript", { isTSX: true, allExtensions: true }],
+    ],
+  },
+).code
+
+addEventListener(
+  "connect",
+  (e) => expose(babelTransform, e.ports[0]),
 );
 
 addEventListener("message", (event) => {
   if (event.data.comlinkInit) {
-    const { Comlink, Babel } = self;
-    const { transform } = Babel;
-    const { expose } = Comlink;
-
-    expose((code) =>
-      transform(
-        code,
-        {
-          compact: false,
-          comments: false,
-          presets: [
-            "react",
-            "es2017",
-            ["typescript", { isTSX: true, allExtensions: true }],
-          ],
-        },
-      ).code, event.data.port);
+    expose(babelTransform, event.data.port);
   }
 });
