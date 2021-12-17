4e556a9bc2ba346b7f57428c2e2b3fa062181808
Zoltan Erdos <zolika84@gmail.com>
2021-12-17 19:48:14 +0000

    pre-release

--- packages/code/scripts/importmapGenerator.mjs
+++ packages/code/scripts/importmapGenerator.mjs
@@ -14,7 +14,26 @@ const exceptions = {
   "react-transition-group": "https://esm.sh/react-transition-group",
   "hoist-non-react-statics": "https://esm.sh/hoist-non-react-statics",
   "prop-types": "https://esm.sh/prop-types",
-  "@babel/runtime/": "https://cdn.skypack.dev/@babel/runtime/",
+  "@babel/runtime/helpers/esm/extends": "https://ga.jspm.io/npm:@babel/runtime@7.16.5/helpers/esm/extends.js",
+  "@babel/runtime/helpers/extends": "https://ga.jspm.io/npm:@babel/runtime@7.16.5/helpers/esm/extends.js",
+  "@emotion/react":
+  {
+    "@babel/runtime/helpers/esm/extends": "https://ga.jspm.io/npm:@babel/runtime@7.16.5/helpers/esm/extends.js",
+    "@babel/runtime/helpers/extends": "https://ga.jspm.io/npm:@babel/runtime@7.16.5/helpers/esm/extends.js",
+    "@emotion/cache": "https://ga.jspm.io/npm:@emotion/cache@11.7.1/dist/emotion-cache.browser.esm.js",
+    "@emotion/hash": "https://ga.jspm.io/npm:@emotion/hash@0.8.0/dist/hash.browser.esm.js",
+    "@emotion/memoize": "https://ga.jspm.io/npm:@emotion/memoize@0.7.5/dist/emotion-memoize.browser.esm.js",
+    "@emotion/serialize": "https://ga.jspm.io/npm:@emotion/serialize@1.0.2/dist/emotion-serialize.browser.esm.js",
+    "@emotion/sheet": "https://ga.jspm.io/npm:@emotion/sheet@1.1.0/dist/emotion-sheet.browser.esm.js",
+    "@emotion/unitless": "https://ga.jspm.io/npm:@emotion/unitless@0.7.5/dist/unitless.browser.esm.js",
+    "@emotion/utils": "https://ga.jspm.io/npm:@emotion/utils@1.0.0/dist/emotion-utils.browser.esm.js",
+    "@emotion/weak-memoize": "https://ga.jspm.io/npm:@emotion/weak-memoize@0.2.5/dist/weak-memoize.browser.esm.js",
+    "hoist-non-react-statics": "https://ga.jspm.io/npm:hoist-non-react-statics@3.3.2/dist/hoist-non-react-statics.cjs.js",
+    "object-assign": "https://ga.jspm.io/npm:object-assign@4.1.1/index.js",
+    "react": "https://ga.jspm.io/npm:react@17.0.2/index.js",
+    "react-is": "https://ga.jspm.io/npm:react-is@16.13.1/index.js",
+    "stylis": "https://ga.jspm.io/npm:stylis@4.0.13/index.js",
+    "@emotion/react": "https://ga.jspm.io/npm:@emotion/react@11.7.1/dist/emotion-react.browser.esm.js"},
   // "@mui/material": `https://unpkg.com/@mui/material@${muiMateriaVersion}/modern/index.js`,
   // "@mui/material/": `https://unpkg.com/@mui/material@${muiMateriaVersion}/modern/`,
   // "@mui/core": `https://unpkg.com/@mui/core@${muiCoreVersion}/modern/index.js`,
@@ -39,8 +58,7 @@ const exceptions = {
     `https://unpkg.com/@spike.land/esm@${zedEsmLatestVersion}/dist/textdiff-create.mjs`,
   "textdiff-patch":
     `https://unpkg.com/@spike.land/esm@${zedEsmLatestVersion}/dist/textdiff-patch.mjs`,
-  "uuid": `https://unpkg.com/uuid@${uidVersion}/dist/esm-browser/index.js`,
-};
+    "uuid": `https://ga.jspm.io/npm:uuid@${uidVersion}/dist/esm-browser/index.js`};
 
 const getUnpkgLink = async (packageName) => {
   if (exceptions[packageName]) {
@@ -143,18 +161,20 @@ const getUnpkgLink = async (packageName) => {
 };
 
 const list = [
-  "@babel/runtime/",
-  "@emotion/cache",
-  "@emotion/hash",
-  "@emotion/is-prop-valid",
-  "@emotion/memoize",
+  // "@babel/runtime/",
+  "@babel/runtime/helpers/esm/extends",
+  "@babel/runtime/helpers/extends",
+  // "@emotion/cache",
+  // "@emotion/hash",
+  // "@emotion/is-prop-valid",
+  // "@emotion/memoize",
   "@emotion/react",
-  "@emotion/serialize",
-  "@emotion/sheet",
-  "@emotion/styled",
-  "@emotion/utils",
-  "@emotion/unitless",
-  "@emotion/weak-memoize",
+  // "@emotion/serialize",
+  // "@emotion/sheet",
+  // "@emotion/styled",
+  // "@emotion/utils",
+  // "@emotion/unitless",
+  // "@emotion/weak-memoize",
   // "@mui/material",
   // "@mui/core",
   // "@mui/system",
@@ -198,9 +218,14 @@ const bigMap = await Promise.all(
   list.map((packageName) => getUnpkgLink(packageName)),
 );
 
+
+
 const importMap = { "imports": {} };
 const flat = bigMap.flatMap((v) => v);
 flat.map((i) => Object.assign(importMap.imports, i));
+
+
+Object.keys(importMap.imports).map(key=> typeof importMap.imports[key] !=="string" && Object.keys(importMap.imports[key]).map(x=>importMap.imports[x]=importMap.imports[key][x]) );
 
 importMap.imports.dev = "./js/starter.mjs";
 console.log(JSON.stringify(importMap, undefined, 2));
