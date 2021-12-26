98b198c7688b6929637d4e5d0524969ccf10a8a1
Zoltan Erdos <zolika84@gmail.com>
2021-12-26 02:34:03 +0000

    pre-release

--- packages/code/generate.mjs
+++ packages/code/generate.mjs
@@ -58,25 +58,25 @@ await generator.install("react-transition-group");
 const importMap = { ...generator.getMap() };
 importMap.imports.dev = "./js/starter.mjs";
 importMap.imports["framer-motion"] =
-  "https://unpkg.com/@spike.land/esm@0.3.70/dist/framer-motion.mjs";
+  "https://unpkg.com/@spike.land/esm@0.3.73/dist/framer-motion.mjs";
 
 importMap.imports["textdiff-create"] =
-  "https://unpkg.com/@spike.land/esm@0.3.70/dist/textdiff-create.mjs";
+  "https://unpkg.com/@spike.land/esm@0.3.73/dist/textdiff-create.mjs";
 
 importMap.imports["textdiff-patch"] =
-  "https://unpkg.com/@spike.land/esm@0.3.70/dist/textdiff-patch.mjs";
+  "https://unpkg.com/@spike.land/esm@0.3.73/dist/textdiff-patch.mjs";
 
 importMap.imports["ipfs-only-hash"] =
-  "https://unpkg.com/@spike.land/esm@0.3.70/dist/ipfs-only-hash.mjs";
+  "https://unpkg.com/@spike.land/esm@0.3.73/dist/ipfs-only-hash.mjs";
 importMap.imports["react"] =
-  "https://unpkg.com/@spike.land/esm@0.3.70/dist/react.mjs";
+  "https://unpkg.com/@spike.land/esm@0.3.73/dist/react.mjs";
 importMap.imports["react-dom"] =
-  "https://unpkg.com/@spike.land/esm@0.3.70/dist/react-dom.mjs";
+  "https://unpkg.com/@spike.land/esm@0.3.73/dist/react-dom.mjs";
 importMap.imports["react-is"] =
-  "https://unpkg.com/@spike.land/esm@0.3.70/dist/react-is.mjs";
+  "https://unpkg.com/@spike.land/esm@0.3.73/dist/react-is.mjs";
 
 importMap.imports["@emotion/react"] =
-  "https://unpkg.com/@spike.land/esm@0.3.70/dist/emotion-react.mjs";
+  "https://unpkg.com/@spike.land/esm@0.3.73/dist/emotion-react.mjs";
 
 // importMap.imports.tslib =
 //   "https://cdnjs.cloudflare.com/ajax/libs/tslib/2.3.1/tslib.es6.min.js";
