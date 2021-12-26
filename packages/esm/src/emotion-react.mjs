e2aef1be8a293cdf54fb1e233ff4eb75a70c7327
Zoltan Erdos <zolika84@gmail.com>
2021-12-26 02:37:10 +0000

    pre-release

--- packages/esm/src/emotion-react.mjs
+++ packages/esm/src/emotion-react.mjs
@@ -7,17 +7,10 @@ export const {ThemeContext} = emotionReact;
 export const {ThemeProvider} = emotionReact;
 export const {__unsafe_useEmotionCache} = emotionReact;
 export const {createElement} = emotionReact;
-
-
 export const {css} = emotionReact;
-
 export const {jsx} = emotionReact;
-
-
 export const {keyframes} = emotionReact;
 export const {useTheme} = emotionReact;
-
-
 export const {withEmotionCache} = emotionReact;
 export const {withTheme} = emotionReact;
 
