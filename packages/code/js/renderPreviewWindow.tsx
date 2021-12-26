8161783c81081ae8f9b1f1cb7792931b70cd192d
Zoltan Erdos <zolika84@gmail.com>
2021-12-26 08:55:27 +0000

    pre-release

--- packages/code/js/renderPreviewWindow.tsx
+++ packages/code/js/renderPreviewWindow.tsx
@@ -1,22 +1,27 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
+import { Suspense } from "react";
 import {} from "react-dom/next";
 import { DraggableWindow } from "./DraggableWindow";
 // import { getHtmlAndCss
 //  } from "./renderToString";
 
 export const renderPreviewWindow = async (session) => {
-  const target = document.getElementById("root")!;
+  const target = document.createElement("div");
+  const editor = document.getElementById("shadowEditor")!;
+  target.style.opacity = "0";
+  editor.style.opacity = "0";
+
   const { room } = session;
 
-  const Element = () => (
-    <DraggableWindow
-      onShare={() => open(`https://code.spike.land/api/room/${room}/public`)}
-      onRestore={() => {}}
-      position={session.mode === "window" ? "fixed" : "absolute"}
-      session={session}
-    />
-  );
+  // const Element = () => (
+  //   <DraggableWindow
+  //     onShare={() => open(`https://code.spike.land/api/room/${room}/public`)}
+  //     onRestore={() => {}}
+  //     position={session.mode === "window" ? "fixed" : "absolute"}
+  //     session={session}
+  //   />
+  // );
 
   // const {html} = getHtmlAndCss( <Element></Element> as any );
 
@@ -26,8 +31,8 @@ export const renderPreviewWindow = async (session) => {
 
   // target  .innerHTML = html;
 
-  const root = createRoot(target);
-  root.render(<Element></Element> as any);
+  const root = createRoot(target, {});
+  // root.render(<Element></Element> as any);
 
   root.render(
     <DraggableWindow
@@ -40,6 +45,12 @@ export const renderPreviewWindow = async (session) => {
       session={session}
     />,
   );
-
   document.body.appendChild(target);
+
+  setTimeout(() => {
+    target.style.opacity = "1";
+    editor.style.opacity = "1";
+
+    document.getElementById("root")?.remove();
+  }, 400);
 };
