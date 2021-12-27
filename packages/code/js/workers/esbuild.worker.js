42d481b894d480d9d3e637182dee2c264f499d0a
Zoltan Erdos <zolika84@gmail.com>
2021-12-27 10:10:35 +0000

    pre-release

--- packages/code/js/workers/esbuild.worker.js
+++ packages/code/js/workers/esbuild.worker.js
@@ -3,26 +3,27 @@ self.importScripts(
   "https://unpkg.com/esbuild-wasm@0.14.8/lib/browser.min.js",
 );
 
-const { Comlink } = self;
+const { Comlink, esbuild } = self;
 // const { transform } = Babel;
 const { expose } = Comlink;
 
 let initializedEsbuild = null;
 
-const init = esbuild.initialize({
-  wasmURL: "https://unpkg.com/esbuild-wasm@0.14.8/esbuild.wasm",
-});
+const init = () =>
+  esbuild.initialize({
+    wasmURL: "https://unpkg.com/esbuild-wasm@0.14.8/esbuild.wasm",
+  });
 
 const transform = (code) => esbuild.transform(code, { loader: "tsx" });
 
 addEventListener(
   "connect",
-  async (e) => await init && expose(transform, e.ports[0]),
+  async (e) => await init() && expose(transform, e.ports[0]),
 );
 
 addEventListener("message", async (event) => {
   if (event.data.comlinkInit) {
-    await init;
+    await init();
     expose(transform, event.data.port);
   }
 });
