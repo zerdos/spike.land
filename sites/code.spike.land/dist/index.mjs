e3185ee44b5c36d1adf07d24edb56b5546c638b0
Zoltan Erdos <zolika84@gmail.com>
2021-12-31 00:49:31 +0000

    pre-release

--- sites/code.spike.land/dist/index.mjs
+++ sites/code.spike.land/dist/index.mjs
@@ -581,7 +581,7 @@ var require_textdiff_create = __commonJS({
 });
 
 // ../../packages/code/package.json
-var version = "0.4.105";
+var version = "0.4.107";
 
 // ../../packages/cf-npm-site/dist/index.mjs
 function src_default(packageName, version2, serveDir = "") {
