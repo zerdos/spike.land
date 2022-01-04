c1d70ba64a7d91d34f5ea045dcd8a4a0a21b76c2
Zoltan Erdos <zolika84gmail.com>
2022-01-04 16:33:10 +0000

    pre-release

--- monorepo/packages/qrious/dist/qriousType.d.ts
+++ monorepo/packages/qrious/dist/qriousType.d.ts
@@ -1,17 +1,17 @@
 interface IQROptions {
-  element: HTMLCanvasElement;
-  size: number;
-  foregroundAlpha: number;
-  foreground: string;
-  padding: number;
-  backgroundAlpha: number;
-  background: string;
-  value: string;
+    element: HTMLCanvasElement;
+    size: number;
+    foregroundAlpha: number;
+    foreground: string;
+    padding: number;
+    backgroundAlpha: number;
+    background: string;
+    value: string;
 }
 export interface IQRious {
-  constructor: (opts: IQROptions) => {};
-  use: (BrowserElementService: any) => {};
-  value: string;
-  get: () => IQROptions;
+    constructor: (opts: IQROptions) => {};
+    use: (BrowserElementService: any) => {};
+    value: string;
+    get: () => IQROptions;
 }
 export {};
