b7f2c3cf1c8fcc00ce5f82552603822d14603c42
Zoltan Erdos <zolika84@gmail.com>
2022-01-05 08:23:57 +0000

    pre-release

--- monorepo/packages/qwik-app/.rollup.cache/Users/z/z/monorepo/packages/qwik-app/src/index.server.js
+++ monorepo/packages/qwik-app/.rollup.cache/Users/z/z/monorepo/packages/qwik-app/src/index.server.js
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
  */
 import { h } from "@builder.io/qwik";
-import { QwikLoader, renderToString } from "@builder.io/qwik/server";
+import { QwikLoader, renderToString, } from "@builder.io/qwik/server";
 import { ToDoApp } from "./components";
 /**
  * Entry point for server-side pre-rendering.
@@ -14,33 +14,21 @@ import { ToDoApp } from "./components";
  * @returns a promise when all of the rendering is completed.
  */
 export function renderApp(opts) {
-  const todos = {
-    filter: "all",
-    items: [
-      { completed: false, title: "Read Qwik docs" },
-      { completed: false, title: "Build HelloWorld" },
-      { completed: false, title: "Profit" },
-    ],
-  };
-  return renderToString(
-    h(
-      "html",
-      null,
-      h(
-        "head",
-        null,
-        h("title", null, "Qwik Demo: ToDo"),
-        h("link", { rel: "stylesheet", href: "/base.css" }),
-        h("link", { rel: "stylesheet", href: "/index.css" }),
-      ),
-      h(
-        "body",
-        { "q:base": "/build/" },
-        h(ToDoApp, { todos: todos }),
-        h(QwikLoader, { debug: opts.debug }),
-      ),
-    ),
-    opts,
-  );
+    const todos = {
+        filter: "all",
+        items: [
+            { completed: false, title: "Read Qwik docs" },
+            { completed: false, title: "Build HelloWorld" },
+            { completed: false, title: "Profit" },
+        ],
+    };
+    return renderToString(h("html", null,
+        h("head", null,
+            h("title", null, "Qwik Demo: ToDo"),
+            h("link", { rel: "stylesheet", href: "/base.css" }),
+            h("link", { rel: "stylesheet", href: "/index.css" })),
+        h("body", { "q:base": "/build/" },
+            h(ToDoApp, { todos: todos }),
+            h(QwikLoader, { debug: opts.debug }))), opts);
 }
 //# sourceMappingURL=index.server.js.map
