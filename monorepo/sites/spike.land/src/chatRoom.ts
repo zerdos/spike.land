b427b7ef60c52c38041e3fa517c9c9b3ea480e53
Zoltan Erdos <zolika84@gmail.com>
2022-01-07 20:10:40 +0000

    pre-release

--- monorepo/sites/spike.land/src/chatRoom.ts
+++ monorepo/sites/spike.land/src/chatRoom.ts
@@ -227,11 +227,13 @@ export class Code {
 
 
           const html = HYDRATED.replace(
-            `<div id="root"></div>`,
+            `<script type="importmap-shim" src="https://unpkg.com/@spike.land/code@{VERSION}/js/importmap.json"></script>`,
+            `<script type="importmap">${JSON.stringify(imap)}}</script>`)
+            .replace(`<div id="root"></div>`,
             `<div id="root"><style>${css}</style><div id="zbody">${htmlContent}</div></div>`,
           ).replaceAll(vReg, version).replace(
             `<script type="importmap-shim" src="./importmap.json"></script>`,
-            `<script type="importmap-shim">${JSON.stringify(imap)}</script>`,
+            `<script type="importmap">${JSON.stringify(imap)}</script>`,
           );
           return new Response(html, {
             status: 200,
