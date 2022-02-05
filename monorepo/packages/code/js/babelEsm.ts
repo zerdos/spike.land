5019d5efdd93bee498160e6ed0921ea65abd193e
Zoltan Erdos <zolika84gmail.com>
2022-02-05 19:51:19 +0000

    pre-release

--- monorepo/packages/code/js/babelEsm.ts
+++ monorepo/packages/code/js/babelEsm.ts
@@ -2,7 +2,9 @@ import { transform } from "@babel/standalone";
 
 export const babelTransform = (code: string) =>
   transform(
-    code,
+    `/** @jsx jsX */
+    import { jsx as jsX } from "@emotion/react";
+    ` + code,
     {
       compact: false,
       comments: false,
