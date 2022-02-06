afaa7fa732380130558fb66ebb21774dba619915
Zoltan <z@Zoltans-MacBook-Pro.local>
2022-02-06 13:18:19 +0000

    pre-release

--- monorepo/packages/code/generate.mjs
+++ monorepo/packages/code/generate.mjs
@@ -41,7 +41,7 @@ await generator.install("preact/compat");
 
 // await generator.install("@emotion/server");
 await generator.install("@emotion/react");
-//await generator.install("@emotion/cache");
+await generator.install("@emotion/cache");
 // await generator.install("lodash/throttle");
 // await generator.install("simple-text-compress");
 // await generator.install("@emotion/styled");
