5aa89e7197dd12bf7b8a233be6199ade0b406844
Zoltan Erdos <zolika84@gmail.com>
2022-02-07 18:28:16 +0000

    pre-release

--- monorepo/packages/code/js/appStarter.ts
+++ monorepo/packages/code/js/appStarter.ts
@@ -1,67 +1,82 @@
-import "es-module-shims";
+import { dependencies } from "../package.json";
+import { WorkboxEvent, WorkboxLifecycleEvent } from "workbox-window";
 
 import importMap from "./importmap.json";
+
+// const esbuild = import("./esbuildEsm.ts");
+
+window.esmsInitOptions = {
+  shimMode: true,
+  revokeBlobURLs: true,
+  fetch: myFetch,
+
+  resolve: (id: string, parentUrl: string) => {
+    return parentUrl + id;
+  },
+  polyfillEnable: ["css-modules", "json-modules"],
+  onerror: (error) => console.log(error), // defaults to `((e) => { throw e; })`
+  noLoadEventRetriggers: true,
+  skip: /^https?:\/\/(cdn\.skypack\.dev|jspm\.dev)\//,
+};
+
+async function myFetch(input: RequestInfo, init?: RequestInit) {
+  const url = input.toString();
+
+  const urlEnd = url.substring(-3);
+  if (
+    url.indexOf("monaco") === -1 && ["tsx", ".ts"].indexOf(urlEnd) !== -1
+  ) {
+    console.log(url);
+    const res = await fetch(url, init);
+    if (!res.ok) return res;
+
+    const source = await res.text();
+    return new Response(source, init);
+
+    //https://localhost:8000/monorepo/packages/code/js/editor.ts
+    // const { transform } = await importShim("./dist/esbuildEsm.mjs");
+
+    //const transformed = await transform(source);
+
+    // return new Response(
+    //   new Blob([transformed], { type: "application/javascript" }),
+    // );
+  }
+  return fetch(url, init);
+}
 
 document.body.appendChild(Object.assign(document.createElement("script"), {
   type: "importmap-shim",
   innerHTML: JSON.stringify(importMap),
 }));
 
-(async (injectedRoom = "") => {
-  // const esbuild = import("./esbuildEsm.ts");
+document.body.appendChild(Object.assign(document.createElement("script"), {
+  async: true,
+  src: `https://unpkg.com/es-module-shims@${
+    dependencies["es-module-shims"]
+  }/dist/es-module-shims.wasm.js`,
+}));
 
-  window.esmsInitOptions = {
-    shimMode: true,
-    revokeBlobURLs: true,
-    fetch: myFetch,
+const script = `import {run} from "./dist/starter.mjs";
 
-    resolve: (id: string, parentUrl: string) => {
-      return parentUrl + id;
-    },
-    polyfillEnable: ["css-modules", "json-modules"],
-    onerror: (error) => console.log(error), // defaults to `((e) => { throw e; })`
-    noLoadEventRetriggers: true,
-    skip: /^https?:\/\/(cdn\.skypack\.dev|jspm\.dev)\//,
-  };
+run();
 
-  async function myFetch(input: RequestInfo, init?: RequestInit) {
-    const url = input.toString();
+(async()=>{
+const { Workbox } = await import("workbox-window");
 
-    const urlEnd = url.substring(-3);
-    if (
-      url.indexOf("monaco") === -1 && ["tsx", ".ts"].indexOf(urlEnd) !== -1
-    ) {
-      console.log(url);
-      const res = await fetch(url, init);
-      if (!res.ok) return res;
+const wb = new Workbox("./sw.js");
 
-      const source = await res.text();
-      return new Response(source, init);
-
-      //https://localhost:8000/monorepo/packages/code/js/editor.ts
-      // const { transform } = await importShim("./dist/esbuildEsm.mjs");
-
-      //const transformed = await transform(source);
-
-      // return new Response(
-      //   new Blob([transformed], { type: "application/javascript" }),
-      // );
-    }
-    return fetch(url, init);
+wb.addEventListener("activated", async (event: WorkboxLifecycleEvent) => {
+  if (!event.isUpdate) {
+    console.log("Service worker activated for the first time!");
   }
+});
 
-  // window.process = { env: { NODE_ENV: "production" } };
+wb.register();
+})();`;
 
-  const { run } = await importShim("./dist/starter.mjs");
-  run(injectedRoom);
-
-  const { Workbox } = await importShim("workbox-window");
-
-  const wb = new Workbox("./sw.js");
-  wb.addEventListener("activated", async (event) => {
-    if (!event.isUpdate) {
-      console.log("Service worker activated for the first time!");
-    }
-  });
-  wb.register();
-})();
+document.body.appendChild(Object.assign(document.createElement("script"), {
+  async: true,
+  type: "module-shim",
+  innerHTML: script,
+}));
