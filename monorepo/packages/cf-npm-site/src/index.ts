6ce03adc4507ee89e88a726067d13b125b20415a
Zoltan Erdos <zolika84@gmail.com>
2022-02-07 23:32:31 +0000

    pre-release

--- monorepo/packages/cf-npm-site/src/index.ts
+++ monorepo/packages/cf-npm-site/src/index.ts
@@ -4,117 +4,116 @@ export default function (
   serveDir: string = "",
 ) {
   return async function (request: Request, env: EventInit) {
-    
-      return await tryToResp(request, env, 4);
-  
- 
-async function tryToResp(request: Request, env: EventInit, retry: number):Promise< Response>{
-  try {
-  const url = new URL(request.url);
-  const { pathname } = url;
 
-const isChunk = pathname.indexOf("/chunks") !== -1;
+    return await tryToResp(request, env, 4);
 
 
+    async function tryToResp(request: Request, env: EventInit, retry: number): Promise<Response> {
+      try {
+        const url = new URL(request.url);
+        const { pathname } = url;
 
-  const cacheKey = isChunk? new Request(url.host+pathname.substring( pathname.indexOf("/chunks"+7)) ): new Request(url.toString());
-  const cache = caches.default;
+        const isChunk = pathname.indexOf("/chunks") !== -1;
 
-  const cachedResp = await cache.match(cacheKey);
 
-  if (cachedResp) {
-    return cachedResp;
-  }
 
-  const uri = (pathname.startsWith("/@")
-    ? pathname.substring(1)
-    : `@${version}${
-      serveDir
-        ? `/${serveDir}`
-        : ``
-    }${pathname}`);
+        const cacheKey = isChunk ? new Request(url.host + pathname.substring(pathname.indexOf("/chunks" + 7))) : new Request(url.toString());
+        const cache = caches.default;
 
-  let targetPath = uri;
+        const cachedResp = await cache.match(cacheKey);
 
-  if (uri.endsWith("/")) {
-    targetPath = `${uri}index.html`;
-  } else if (pathname.indexOf(".") === -1) {
-    targetPath = `${uri}/index.html`;
-  }
+        if (cachedResp) {
+          return cachedResp;
+        }
 
-  const reqCloned = request.clone();
+        const uri = (pathname.startsWith("/@")
+          ? pathname.substring(1)
+          : `@${version}${serveDir
+            ? `/${serveDir}`
+            : ``
+          }${pathname}`);
 
-  const newReq = new Request(
-    `https://unpkg.com/${packageName}${targetPath}`,
-    {
-      headers: {
-        ...reqCloned.headers,
-      },
-    },
-  );
+        let targetPath = uri;
 
-  const origResp = await fetch(newReq);
+        if (uri.endsWith("/")) {
+          targetPath = `${uri}index.html`;
+        } else if (pathname.indexOf(".") === -1) {
+          targetPath = `${uri}/index.html`;
+        }
 
-  if (!origResp.ok) throw new Error("not ok");
+        const reqCloned = request.clone();
 
-  const cloned = origResp.clone();
+        const newReq = new Request(
+          `https://unpkg.com/${packageName}${targetPath}`,
+          {
+            headers: {
+              ...reqCloned.headers,
+            },
+          },
+        );
 
-  const resp = new Response(cloned.body, {
-    headers: {
-      ...cloned.headers,
-      "Cache-Control": isChunk
-        ? "public, max-age=604800, immutable"
-        : "no-cache",
-    },
-  });
+        const origResp = await fetch(newReq);
 
-  // ich
+        if (!origResp.ok) throw new Error("not ok");
 
-  if (
-    pathname.endsWith(".mjs") || pathname.endsWith(".js") ||
-    pathname.endsWith(".ts") || pathname.endsWith(".tsx")
-  ) {
-    resp.headers.delete("content-type");
-    resp.headers.set(
-      "content-type",
-      "text/javascript;charset=UTF-8",
-    );
-    resp.headers.set("Access-Control-Allow-Origin","*");
-  } else if (pathname.endsWith(".css")) {
-    resp.headers.delete("content-type");
-    resp.headers.set(
-      "content-type",
-      "text/css;charset=UTF-8",
-    );
-  } else if (pathname.endsWith(".json")) {
-    resp.headers.delete("content-type");
-    resp.headers.set(
-      "content-type",
-      "application/json;charset=UTF-8",
-    );
-  } else if (pathname.endsWith(".jpg")) {
-    resp.headers.delete("content-type");
-    resp.headers.set("content-type", "image/jpeg");
-  } else if (pathname.indexOf(".") === -1 || pathname.endsWith(".html")) {
-    resp.headers.delete("content-type"),
-      resp.headers.set(
-        "content-type",
-        "text/html;charset=UTF-8",
-      );
-  }
+        const cloned = origResp.clone();
 
-  if (origResp.status === 200) await cache.put(cacheKey, resp.clone());
+        const resp = new Response(cloned.body, {
+          headers: {
+            ...cloned.headers,
+            "Cache-Control": isChunk
+              ? "public, max-age=604800, immutable"
+              : "no-cache",
+          },
+        });
 
-  return resp;
-} catch (Error) {
-  if (retry>4) {
-   await wait(5000-retry*1000);
-   return tryToResp (request, env, retry-1)
-  }
-  return new Response(`No... ${Object.prototype.toString.call(Error)}`);
-}
-}
-};
+        // ich
+
+        if (
+          pathname.endsWith(".mjs") || pathname.endsWith(".js") ||
+          pathname.endsWith(".ts") || pathname.endsWith(".tsx")
+        ) {
+          resp.headers.delete("content-type");
+          resp.headers.set(
+            "content-type",
+            "text/javascript;charset=UTF-8",
+          );
+          resp.headers.set("Access-Control-Allow-Origin", "*");
+        } else if (pathname.endsWith(".css")) {
+          resp.headers.delete("content-type");
+          resp.headers.set(
+            "content-type",
+            "text/css;charset=UTF-8",
+          );
+        } else if (pathname.endsWith(".json")) {
+          resp.headers.delete("content-type");
+          resp.headers.set(
+            "content-type",
+            "application/json;charset=UTF-8",
+          );
+        } else if (pathname.endsWith(".jpg")) {
+          resp.headers.delete("content-type");
+          resp.headers.set("content-type", "image/jpeg");
+        } else if (pathname.indexOf(".") === -1 || pathname.endsWith(".html")) {
+          resp.headers.delete("content-type"),
+            resp.headers.set(
+              "content-type",
+              "text/html;charset=UTF-8",
+            );
+        }
+
+        if (origResp.status === 200) await cache.put(cacheKey, resp.clone());
+
+        return resp;
+      } catch (Error) {
+        if (retry > 4) {
+          await wait(5000 - retry * 1000);
+          return tryToResp(request, env, retry - 1)
+        }
+        return new Response(`No... ${Object.prototype.toString.call({ Error })}`);
+      }
+    }
+  };
 }
 
 async function wait(delay: number): Promise<void> {
