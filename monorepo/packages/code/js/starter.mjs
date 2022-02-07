13dc44c3e98cfa37b4c52bebda7d06ba65662f07
Zoltan Erdos <zolika84@gmail.com>
2022-02-07 07:43:35 +0000

    pre-release

--- monorepo/packages/code/js/starter.mjs
+++ monorepo/packages/code/js/starter.mjs
@@ -33,64 +33,7 @@ export const run = async (injectedRoom) => {
     window.aniStart = Date.now();
     const root = hydrateRoot(container, jsx(App));
 
-    // const outers = [];
-    // const deltas = [];
-
-    // let first = document.getElementById("zbody").innerHTML;
-    // const html = first;
-
-    // const interV = setInterval(() => {
-    // const el = container.innerHTML;
-    // deltas.push(createDelta(first, el));
-
-    // first = el;
-    //      deltas.push()
-    // outers.push(el);
-    // }, 1000 / 60);
-
-    // window.deltas = deltas;
-
-    // const compressed = compress(JSON.stringify(deltas));
-
-    // setTimeout(async () => {
-    //   clearInterval(interV);
-
-    //   // const deltaStr = JSON.stringify(deltas);
-    //   const user = ((self && self.crypto && self.crypto.randomUUID &&
-    //     self.crypto.randomUUID()) || (await import("./uidV4.mjs")).default())
-    //     .slice(
-    //       0,
-    //       8,
-    //     );
-
-    join(room, user, deltas);
-    return;
-    //   //s
-
-    //   // console.log(deltaStr);
-    //   // root.unmount();
-    //   // // const compressed = compress(JSON.stringify(deltas));
-    //   // console.log(
-    //   //   JSON.stringify(deltas).length,
-    //   //   // JSON.stringify(compressed).length,
-    //   // );
-
-    //   // let i = 0;
-    //   // const deltasLength = deltas.length;
-    //   // let last = html;
-
-    //   // setInterval(() => {
-    //   //   const index = i % deltasLength;
-    //   //   if (index === 0) last = html;
-
-    //   //   i++;
-    //   //   const delta = deltas[index];
-    //   //   if (!delta) return;
-    //   //   const next = applyDelta(last, delta);
-    //   //   last = next;
-    //   //   container.innerHTML = next;
-    //   // }, 1000 / 60);
-    // }, 2000);
+    join(room, user);
 
     return;
   } else {
@@ -98,23 +41,6 @@ export const run = async (injectedRoom) => {
       const App = (await import(
         `https://spike.land/api/room/${room}/js`
       )).default;
-
-      // const applyDelta = (await import(
-      //   "https://unpkg.com/@spike.land/esm@0.4.33/dist/textdiff-patch.mjs"
-      // )).default;
-
-      // const resp = await fetch(
-      //   `https://spike.land/api/room/${room}/delta`,
-      // );
-      // const deltas = await resp.json();
-
-      //   const { jsx } = emotionReact;
-
-      //   const { ReactDOM } = window;
-
-      //  const container = document.createElement("div");
-      //  container.id = "upgrade-me";
-      // document.body.appendChild(container);
 
       let container = document.getElementById("zbody");
       render(jsx(App), container);
@@ -128,62 +54,6 @@ export const run = async (injectedRoom) => {
         container.innerHTML =
           `<style>${session.css}</style><div id="zbody">${session.html}</div>`;
         container = document.getElementById("zbody");
-      }
-
-      if (deltas && deltas.length) {
-        //   const st = document.createElement("style");
-        //   st.innerHTML = session.css;
-
-        const html = container.innerHTML;
-
-        //   const applied = applyDelta(html, deltas[0]);
-        //   container.innerHTML = session.html;
-        // `<style>${session.css}</style><div>${session.html}</div>`;
-
-        //  const root = ReactDOM.createRoot(target);
-
-        //   root.render(jsx(App));
-
-        //   document.body.appendChild(st);
-
-        //  document.body.appendChild(container);
-
-        //   const zBody = container;
-
-        let i = 0;
-        let last = html;
-        const deltasLength = deltas.length;
-        const animationLength = (2000 - (Date.now() - window.aniStart)) /
-          deltas.length;
-        console.log({ animationLength });
-        const clInt = setInterval(() => {
-          if (i >= deltas.length) {
-            clearInterval(clInt);
-            return;
-          }
-          const index = i % deltasLength;
-          if (index === 0) last = html;
-          //
-          i++;
-          const delta = deltas[index];
-          if (!delta) return;
-          const next = applyDelta(last, delta);
-          last = next;
-
-          // const newDiv = document.createElement("div");
-          // newDiv.id = "zbodyw";
-          // newDiv.setAttribute("id", "zbodyw");
-
-          // newDiv.innerHTML = `<div>${next}</div>`;
-
-          container.innerHTML = next;
-
-          // console.log(next);
-          //    document.removeChild(container);
-          // console.log(next);
-          // zbod
-        }, animationLength);
-        //  document.appendChild(container);
       } else {
         const App = (await import(
           `https://spike.land/api/room/${room}/js`
@@ -212,31 +82,10 @@ export const run = async (injectedRoom) => {
 
         const { hydrateRoot } = React;
 
-        hydrateRoot(container, App);
+        hydrateRoot(container, jsx(App));
       }
     })();
   }
 
-  /// For local dev
-  // if (document.getElementById("root").innerHTML.length === 0) {
-  //   const cacheKey = `state-${room}`;
-  //   const savedStateStr = localStorage.getItem(cacheKey);
-  //   if (!savedStateStr) {
-  //     const savedState = JSON.parse(savedStateStr);
-  //     const preRenderContainer = document.getElementById("root");
-  //     if (
-  //       preRenderContainer && savedState &&
-  //       preRenderContainer.innerHTML.length === 0
-  //     ) {
-  //       preRenderContainer.innerHTML = `<style>${savedState.css}</style>
-  //         <div id="zbody">${savedState.html}</div>`;
-  //       console.log(`<style>${savedState.css}</style>
-  //       ${savedState.html}
-  //       `);
-  //     }
-  //   }
-  // }
-
   join(room, user);
-  // Console.log({ r
 };
