f8c328d7d849ce33e9f2ac4293d423c7e10701be
Zoltan Erdos <zolika84@gmail.com>
2022-02-11 15:45:20 +0000

    pre-release

--- monorepo/packages/code/js/appStarter.ts
+++ monorepo/packages/code/js/appStarter.ts
@@ -6,7 +6,6 @@ import "es-module-shims";
   window.esmsInitOptions = {
     shimMode: true,
     revokeBlobURLs: true,
-    fetch: myFetch,
 
     resolve: (id: string, parentUrl: string) => {
       return parentUrl + id;
@@ -17,35 +16,34 @@ import "es-module-shims";
     skip: /^https?:\/\/(cdn\.skypack\.dev|jspm\.dev)\//,
   };
 
-  async function myFetch(input: RequestInfo, init?: RequestInit) {
-    const url = input.toString();
+  // async function myFetch(input: RequestInfo, init?: RequestInit) {
+  //   const url = input.toString();
 
-    const urlEnd = url.substring(-3);
-    if (
-      url.indexOf("monaco") === -1 && ["tsx", ".ts"].indexOf(urlEnd) !== -1
-    ) {
-      console.log(url);
-      const res = await fetch(url, init);
-      if (!res.ok) return res;
+  //   const urlEnd = url.substring(-3);
+  //   if (
+  //     url.indexOf("monaco") === -1 && ["tsx", ".ts"].indexOf(urlEnd) !== -1
+  //   ) {
+  //     console.log(url);
+  //     const res = await fetch(url, init);
+  //     if (!res.ok) return res;
 
-      const source = await res.text();
-      return new Response(source, init);
+  //     const source = await res.text();
+  //     return new Response(source, init);
 
-      //https://localhost:8000/monorepo/packages/code/js/editor.ts
-      // const { transform } = await importShim("./dist/esbuildEsm.mjs");
+  //     //https://localhost:8000/monorepo/packages/code/js/editor.ts
+  //     // const { transform } = await importShim("./dist/esbuildEsm.mjs");
 
-      //const transformed = await transform(source);
+  //     //const transformed = await transform(source);
 
-      // return new Response(
-      //   new Blob([transformed], { type: "application/javascript" }),
-      // );
-    }
-    return fetch(url, init);
-  }
+  //     // return new Response(
+  //     //   new Blob([transformed], { type: "application/javascript" }),
+  //     // );
+  //   }
+  //   return fetch(url, init);
+  // }
 
   const { run } = await importShim(
     "https://spike.land/dist/starter.mjs",
-    "https://spike.land",
   );
   run();
 })();
