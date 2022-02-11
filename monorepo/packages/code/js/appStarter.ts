9ed93497fe082d2759f5cf81c4b52a862e94db33
Zoltan Erdos <zolika84@gmail.com>
2022-02-11 15:31:04 +0000

    pre-release

--- monorepo/packages/code/js/appStarter.ts
+++ monorepo/packages/code/js/appStarter.ts
@@ -43,6 +43,6 @@ import "es-module-shims";
     return fetch(url, init);
   }
 
-  const { run } = await importShim("https://spike.land/dist/starter.mjs");
+  const { run } = await importShim("../../dist/starter.mjs");
   run();
 })();
