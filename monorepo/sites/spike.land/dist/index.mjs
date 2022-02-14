5ff524d01c6bb33d0635b6540c392b02f8ab0b2b
Zoltan Erdos <zolika84@gmail.com>
2022-02-14 10:48:07 +0000

    pre-release

--- monorepo/sites/spike.land/dist/index.mjs
+++ monorepo/sites/spike.land/dist/index.mjs
@@ -691,7 +691,7 @@ var require_textdiff_create = __commonJS({
 });
 
 // ../../packages/code/package.json
-var version = "0.7.80";
+var version = "0.7.82";
 
 // src/chat.ts
 var import_cookie = __toESM(require_cookie(), 1);
