484a82eb9936e3dd66734048ef0dcb862936d72c
Zoltan Erdos <zolika84@gmail.com>
2022-02-15 21:15:02 +0000

    pre-release

--- monorepo/packages/code/js/dist/chunks/renderToString-NN3J2EDG.mjs
+++
@@ -1,1 +0,0 @@
-import"./chunk-AH6KKUH7.mjs";import{CacheProvider as c,jsx as t}from"https://spike.land/dist/emotion.mjs";import{createCache as m}from"https://spike.land/dist/emotion.mjs";import{renderToString as n}from"https://spike.land/dist/react.mjs";var d=o=>{let e=m({key:"css"}),r="";return e.sheet.insert=s=>{r+=s},{html:n(t(c,{value:e},t(o,null))),css:r}};export{d as getHtmlAndCss};
