2cae385b86b5931b4e4b34d5ad153dd245c26947
Zoltan Erdos <zolika84@gmail.com>
2022-02-15 22:07:34 +0000

    pre-release

--- monorepo/sites/spike.land/dist/index.mjs
+++ monorepo/sites/spike.land/dist/index.mjs
@@ -581,7 +581,7 @@ var require_textdiff_create = __commonJS({
 });
 
 // ../../packages/code/package.json
-var version = "0.7.215";
+var version = "0.7.222";
 
 // ../../packages/cf-npm-site/dist/index.mjs
 function src_default(packageName, version2, serveDir = "") {
@@ -644,7 +644,7 @@ function src_default(packageName, version2, serveDir = "") {
           resp.headers.set("content-type", "application/json;charset=UTF-8");
         } else if (pathname.endsWith(".ttf")) {
           resp.headers.delete("content-type");
-          resp.headers.set("content-type", "font/truetype;charset=UTF-8;");
+          resp.headers.set("content-type", "font/ttf");
         } else if (pathname.endsWith(".jpg")) {
           resp.headers.delete("content-type");
           resp.headers.set("content-type", "image/jpeg");
