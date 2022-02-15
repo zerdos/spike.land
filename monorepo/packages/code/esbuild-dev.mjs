f4aac155da9ce56fd8273350d613da23960a2900
Zoltan Erdos <zolika84@gmail.com>
2022-02-15 20:30:42 +0000

    pre-release

--- monorepo/packages/code/esbuild-dev.mjs
+++ monorepo/packages/code/esbuild-dev.mjs
@@ -1,5 +1,4 @@
 import https from "https";
-import http from "http";
 
 import esbuild from "esbuild";
 import * as importMap from "esbuild-plugin-import-map";
@@ -21,57 +20,6 @@ const isDevelopment = environment === "development";
 importMap.load(jsonData);
 const importMapPlugin = importMap.plugin();
 
-let httpPlugin = {
-  name: "http",
-  setup(build) {
-    // Intercept import paths starting with "http:" and "https:" so
-    // esbuild doesn't attempt to map them to a file system location.
-    // Tag them with the "http-url" namespace to associate them with
-    // this plugin.
-    build.onResolve({ filter: /^https?:\/\// }, (args) => ({
-      path: args.path,
-      namespace: "http-url",
-    }));
-
-    // We also want to intercept all import paths inside downloaded
-    // files and resolve them against the original URL. All of these
-    // files will be in the "http-url" namespace. Make sure to keep
-    // the newly resolved URL in the "http-url" namespace so imports
-    // inside it will also be resolved as URLs recursively.
-    build.onResolve({ filter: /.*/, namespace: "http-url" }, (args) => ({
-      path: new URL(args.path, args.importer).toString(),
-      namespace: "http-url",
-    }));
-
-    // When a URL is loaded, we want to actually download the content
-    // from the internet. This has just enough logic to be able to
-    // handle the example import from unpkg.com but in reality this
-    // would probably need to be more complex.
-    build.onLoad({ filter: /.*/, namespace: "http-url" }, async (args) => {
-      let contents = await new Promise((resolve, reject) => {
-        function fetch(url) {
-          console.log(`Downloading: ${url}`);
-          let lib = url.startsWith("https") ? https : http;
-          let req = lib.get(url, (res) => {
-            if ([301, 302, 307].includes(res.statusCode)) {
-              fetch(new URL(res.headers.location, url).toString());
-              req.abort();
-            } else if (res.statusCode === 200) {
-              let chunks = [];
-              res.on("data", (chunk) => chunks.push(chunk));
-              res.on("end", () => resolve(Buffer.concat(chunks)));
-            } else {
-              reject(new Error(`GET ${url} failed: status ${res.statusCode}`));
-            }
-          }).on("error", reject);
-        }
-        fetch(args.path);
-      });
-      return { contents };
-    });
-  },
-};
-
 console.log(`
 -------------------------------------------------
 --------------${environment}---------------------
@@ -91,24 +39,24 @@ await esbuild.build({
     // "./js/workers/prettier.worker.js",
   ],
   bundle: true,
-  target: "es2017",
-  sourcemap: isDevelopment,
   minify: !isDevelopment,
   minifyWhitespace: !isDevelopment,
   minifyIdentifiers: !isDevelopment,
   minifySyntax: !isDevelopment,
   legalComments: "external",
-  treeShaking: !isDevelopment,
+  ignoreAnnotations: true,
+  treeShaking: true,
   outExtension: {".js": ".workerJs"},
-  plugins: [importMapPlugin],
   platform: "browser",
   define: {
     "process.env.NODE_ENV": `"${environment}"`,
   },
-
   format: "iife",
+  loader: { 
+    
+    '.ttf': 'file'
+  },
 
-  // loader: {   ".worker.js": "dataurl"},
   outdir: "js/dist/workers/",
 });
 
