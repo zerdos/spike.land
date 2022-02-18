30cc23ae39ddd19cc2385d0c69d63a4d26d2377c
Zoltan Erdos <zolika84@gmail.com>
2022-02-18 09:48:12 +0000

    pre-release

--- fixtures/Qr.tsx
+++ fixtures/Qr.tsx
@@ -1,5 +1,4 @@
-/** @jsx jsx */
-import { css, jsx } from "@emotion/react";
+import { css} from "@emotion/react";
 
 import { useEffect, useRef, useState } from "react";
 import { dynamicImport } from "../../dynamicImport";
