4d0957a65849c7840ac8d03a2dbc5922cea38fe9
Zoltan Erdos <zolika84@gmail.com>
2022-02-18 09:58:41 +0000

    pre-release

--- monorepo/packages/code/esbuild-dev.mjs
+++ monorepo/packages/code/esbuild-dev.mjs
@@ -84,7 +84,7 @@ const build = (entryPoints) =>
     sourcemap: isDevelopment,
     allowOverwrite: true,
     treeShaking: !isDevelopment,
-     tsconfig: "./tsconfig.json",
+    //  tsconfig: "./tsconfig.json",
     platform: "browser",
     ignoreAnnotations: true,
     chunkNames: "chunks/[name]-[hash]",
