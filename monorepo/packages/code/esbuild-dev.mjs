79847fe70729b332318270d399155094cd8e3887
Zoltan Erdos <zolika84gmail.com>
2022-02-19 18:13:49 +0000

    pre-release

--- monorepo/packages/code/esbuild-dev.mjs
+++ monorepo/packages/code/esbuild-dev.mjs
@@ -112,7 +112,7 @@ const build = (entryPoints) =>
       ".ts:": "tsx",
       ".css": "css",
       ".ts": "ts",
-      ".d.ts": "dataurl",
+      ".d.ts?text": "text",
       ".workerJS": "file",
     ".wasm": "file",
     },
