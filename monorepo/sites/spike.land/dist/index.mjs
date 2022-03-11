f0b06de22089b88e3350a9b45307a6edd547ea82
Zoltan Erdos <zolika84gmail.com>
2022-03-11 18:24:43 +0000

    pre-release

--- monorepo/sites/spike.land/dist/index.mjs
+++ monorepo/sites/spike.land/dist/index.mjs
@@ -581,7 +581,7 @@ var require_textdiff_create = __commonJS({
 });
 
 // ../../packages/code/package.json
-var version = "0.7.432";
+var version = "0.7.434";
 
 // ../../packages/cf-npm-site/dist/index.mjs
 function src_default(packageName, version2, serveDir = "") {
