88156ba62ef7d62e230b77ce24913cb38359ba65
Zoltan Erdos <zolika84@gmail.com>
2022-04-03 17:10:34 +0100

    pre-release

--- monorepo/packages/code/js/appStarter.ts
+++ monorepo/packages/code/js/appStarter.ts
@@ -1,56 +1,53 @@
 // import { Workbox } from "workbox-window";
 import "es-module-shims";
-import imap from "./mockedMap.json";
+import imap from "./mockedMap.json" assert { type: "json" };
 
-(async () => {
-  // const esbuild = import("./esbuildEsm.ts");
+// const esbuild = import("./esbuildEsm.ts");
 
-  window.esmsInitOptions = {
-    shimMode: false,
-    revokeBlobURLs: true,
-    fetch: fetch,
-    resolve: (id: string, parentUrl: string) => {
-      return parentUrl + id;
-    },
-    polyfillEnable: ["css-modules", "json-modules"],
-    onerror: (error) => console.log(error), // defaults to `((e) => { throw e; })`
-    noLoadEventRetriggers: true,
-    skip: /^https?:\/\/(cdn\.skypack\.dev|jspm\.dev)\//,
-  };
+window.esmsInitOptions = {
+  shimMode: false,
+  revokeBlobURLs: true,
+  fetch: fetch,
+  resolve: (id: string, parentUrl: string) => {
+    return parentUrl + id;
+  },
+  polyfillEnable: ["css-modules", "json-modules"],
+  onerror: (error) => console.log(error), // defaults to `((e) => { throw e; })`
+  noLoadEventRetriggers: true,
+  skip: /^https?:\/\/(cdn\.skypack\.dev|jspm\.dev)\//,
+};
 
-  // async function myFetch(input: RequestInfo, init?: RequestInit) {
-  //   const url = input.toString();
+// async function myFetch(input: RequestInfo, init?: RequestInit) {
+//   const url = input.toString();
 
-  //   const urlEnd = url.substring(-3);
-  //   if (
-  //     url.indexOf("monaco") === -1 && ["tsx", ".ts"].indexOf(urlEnd) !== -1
-  //   ) {
-  //     console.log(url);
-  //     const res = await fetch(url, init);
-  //     if (!res.ok) return res;
+//   const urlEnd = url.substring(-3);
+//   if (
+//     url.indexOf("monaco") === -1 && ["tsx", ".ts"].indexOf(urlEnd) !== -1
+//   ) {
+//     console.log(url);
+//     const res = await fetch(url, init);
+//     if (!res.ok) return res;
 
-  //     const source = await res.text();
-  //     return new Response(source, init);
+//     const source = await res.text();
+//     return new Response(source, init);
 
-  //     //https://localhost:8000/monorepo/packages/code/js/editor.ts
-  //     // const { transform } = await importShim("./dist/esbuildEsm.mjs");
+//     //https://localhost:8000/monorepo/packages/code/js/editor.ts
+//     // const { transform } = await importShim("./dist/esbuildEsm.mjs");
 
-  //     //const transformed = await transform(source);
+//     //const transformed = await transform(source);
 
-  //     // return new Response(
-  //     //   new Blob([transformed], { type: "application/javascript" }),
-  //     // );
-  //   }
-  //   return fetch(url, init);
-  // }
+//     // return new Response(
+//     //   new Blob([transformed], { type: "application/javascript" }),
+//     // );
+//   }
+//   return fetch(url, init);
+// }
 
-  document.body.appendChild(Object.assign(document.createElement("script"), {
-    type: "importmap-shim",
-    innerHTML: JSON.stringify(imap),
-  }));
+document.body.appendChild(Object.assign(document.createElement("script"), {
+  type: "importmap-shim",
+  innerHTML: JSON.stringify(imap),
+}));
 
-  const { run } = await importShim(
-    "/dist/starter.mjs",
-  );
-  run();
-})();
+importShim<null, { run: () => void }>(
+  "/dist/starter.mjs",
+).then(({ run }) => run());
