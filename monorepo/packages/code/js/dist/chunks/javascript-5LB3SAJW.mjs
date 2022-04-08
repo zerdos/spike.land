7d897702a14d0da8c9e370bd492ff9794acd243b
Zoltan Erdos <zolika84@gmail.com>
2022-04-08 06:17:25 +0100

    pre-release

--- monorepo/packages/code/js/dist/chunks/javascript-5LB3SAJW.mjs
+++
@@ -1,7 +0,0 @@
-import{a as t,b as e}from"./chunk-TBYSQXVZ.mjs";import"./chunk-X36YSYFW.mjs";import"./chunk-6XRSRXUF.mjs";var i=t,o={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{i as conf,o as language};
-/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.34.0-dev.20220407(e57709f4d91206411789844130d7d28230f8bb8c)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/
--- monorepo/packages/code/js/dist/chunks/typescript-E56IQJLC.mjs
+++
@@ -1,1 +0,0 @@
-import{a,b}from"./chunk-TBYSQXVZ.mjs";import"./chunk-X36YSYFW.mjs";import"./chunk-6XRSRXUF.mjs";export{a as conf,b as language};
