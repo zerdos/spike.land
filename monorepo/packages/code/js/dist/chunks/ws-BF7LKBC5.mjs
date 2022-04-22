b4341c016ed4b5323845db700db7792f51f599a3
Zoltan Erdos <zolika84@gmail.com>
2022-04-22 07:39:50 +0100

    pre-release

--- monorepo/packages/code/js/dist/chunks/ws-BF7LKBC5.mjs
+++ monorepo/packages/code/js/dist/chunks/ws-BF7LKBC5.mjs
@@ -216,7 +216,7 @@ async function join(App) {
       errorText: ""
     };
     const stayFullscreen = location.pathname.endsWith("public");
-    const { quickStart } = await import("./quickStart-ZGB6T7NI.mjs");
+    const { quickStart } = await import("./quickStart-GG2KXYRZ.mjs");
     quickStart(session, roomName, stayFullscreen);
   }
   wsConnection.addEventListener("message", (message) => processWsMessage(message, "ws"));
