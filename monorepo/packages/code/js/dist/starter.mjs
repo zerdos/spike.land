f22d46671f0f20fc5812a1227dc63a1fdec6b73e
Zoltan Erdos <zolika84@gmail.com>
2022-04-28 08:18:14 +0100

    pre-release

--- monorepo/packages/code/js/dist/starter.mjs
+++ monorepo/packages/code/js/dist/starter.mjs
@@ -379,10 +379,10 @@ var require_shared = __commonJS({
     (module.exports = function(key, value) {
       return store[key] || (store[key] = value !== void 0 ? value : {});
     })("versions", []).push({
-      version: "3.22.2",
+      version: "3.22.3",
       mode: IS_PURE ? "pure" : "global",
       copyright: "\xA9 2014-2022 Denis Pushkarev (zloirock.ru)",
-      license: "https://github.com/zloirock/core-js/blob/v3.22.2/LICENSE",
+      license: "https://github.com/zloirock/core-js/blob/v3.22.3/LICENSE",
       source: "https://github.com/zloirock/core-js"
     });
   }
