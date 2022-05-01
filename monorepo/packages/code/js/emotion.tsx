8424ddb37faa6da00d5c1a7d41dd4de4cf5e21e0
Zoltan Erdos <zolika84gmail.com>
2022-05-01 10:04:25 +0100

    pre-release

--- monorepo/packages/code/js/emotion.tsx
+++ monorepo/packages/code/js/emotion.tsx
@@ -1,13 +1,13 @@
 import * as emotionReact from "@emotion/react";
 import createCache from "@emotion/cache";
-export { Fragment, jsx } from "@emotion/react/jsx-runtime";
+export { Fragment, jsx, jsxs } from "@emotion/react/jsx-runtime";
 
 export const { CacheProvider } = emotionReact;
 export const { ClassNames } = emotionReact;
 export const { Global } = emotionReact;
 export const { ThemeContext } = emotionReact;
 export const { ThemeProvider } = emotionReact;
-// export const { __unsafe_useEmotionCache } = emotionReact;
+export const { __unsafe_useEmotionCache } = emotionReact;
 
 export const { keyframes } = emotionReact;
 export const { useTheme } = emotionReact;
