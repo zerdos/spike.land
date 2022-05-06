4d4090bf26456aa1007b2daf822b9bffabd9f348
Zoltan Erdos <zolika84gmail.com>
2022-05-06 06:25:09 +0100

    pre-release

--- monorepo/packages/code/js/start.ts
+++ monorepo/packages/code/js/start.ts
@@ -1,4 +1,6 @@
 import { run } from "./starter";
-import StarterApp from "https://spike.land/api/room/code-main/js"
+import StarterApp from "https://spike.land/api/room/code-main/js";
 
-fetch("https://spike.land/api/room/code-main/session").then(resp=>resp.json()).then((session)=> run(session, StarterApp));
+fetch("https://spike.land/api/room/code-main/session").then((resp) =>
+  resp.json()
+).then((session) => run(session, StarterApp));
