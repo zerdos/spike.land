e8071047d8e147d31d44992530372fa1ecda7c7c
Zoltan Erdos <zolika84@gmail.com>
2022-05-20 09:26:35 +0100

    pre-release

--- monorepo/packages/code/main.mjs
+++ monorepo/packages/code/main.mjs
@@ -1,39 +1,9 @@
-export const ipfsSw = async () => {
-  // We start a shared worker where IPFS node is loaded.
-  const worker = createIPFSWorker();
-  // @ts-ignore - Store worker in the window so that it's available in console.
-  window.worker = worker;
+import OrbitDb from "orbit-db/dist/orbitdb"
+import { IPFSClient } from '../../node_modules/ipfs-message-port-client/'
+// URL to the script containing ipfs-message-port-server.
+const IPFS_SERVER_URL = './worker.js'
 
-  // Service workers do not have access to the `SharedWorker` API
-  // (see https://github.com/w3c/ServiceWorker/issues/678)
-  // To overcome that limitation the page will listen for the service worker message
-  // and provide it with a message port to the shared worker, which will enable
-  // it to use our (shared) IPFS node.
-  navigator.serviceWorker.onmessage = onServiceWorkerMessage;
 
-  // @ts-ignore - register expects string but weback requires this URL hack.
-  await navigator.serviceWorker.register(new URL("./sw.js", import.meta.url), {
-    scope: "/",
-  });
-
-  await navigator.serviceWorker.ready;
-
-  // This is just for testing, lets us know when SW is ready.
-
-  console.log("*******ready******");
-  // URLs like `localhost:3000/ipfs/Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD`
-  // are loaded from service worker. However it could be that such a URL is loaded
-  // before the service worker was registered in which case our server just loads a blank
-  // page (that doesn't have data-viewer attribute). If that is the case we load
-  // the actual IPFS content after the SW is ready.
-  if (document.documentElement.dataset.viewer == null) {
-    load(location.pathname);
-  }
-};
-
-/**
- * @param {string} path
- */
 const load = async (path) => {
   const [, protocol] = path.split("/");
   switch (protocol) {
@@ -45,12 +15,44 @@ const load = async (path) => {
   }
 };
 
-/**
- * Handles ipfs message port request from service worker and
- * responds to it with it.
- *
- * @param {MessageEvent} event
- */
+export const ipfsSw = async () => {
+  const worker = new SharedWorker(  
+     new URL(IPFS_SERVER_URL, import.meta.url),
+  { name: "IPFS" })
+  ;
+  window.worker = worker;
+  const ipfs = IPFSClient.from(worker.port);
+  window.ipfs = ipfs
+  window.OrbitDb =OrbitDb;
+
+console.log({OrbitDb})
+
+navigator.serviceWorker.onmessage = onServiceWorkerMessage;
+
+    // @ts-ignore - register expects string but weback requires this URL hack.
+    await navigator.serviceWorker.register(new URL("./sw.js", import.meta.url), {
+      scope: "/",
+    });
+  
+    await navigator.serviceWorker.ready;
+  
+    // This is just for testing, lets us know when SW is ready.
+  
+    console.log("*******ready******");
+    // URLs like `localhost:3000/ipfs/Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD`
+    // are loaded from service worker. However it could be that such a URL is loaded
+    // before the service worker was registered in which case our server just loads a blank
+    if (document.documentElement.dataset.viewer == null) {
+      load(location.pathname);
+    }
+  
+  const data = ipfs.cat('/ipfs/ ')
+
+
+  for await (const chunk of data) {
+    console.log(chunk) 
+  }
+}
 const onServiceWorkerMessage = (event) => {
   /** @type {null|ServiceWorker} */
   const serviceWorker = (event.source);
@@ -71,14 +73,3 @@ const onServiceWorkerMessage = (event) => {
     }
   }
 };
-
-/**
- * Creates a shared worker instance that exposes JS-IPFS node over MessagePort.
- * @returns {SharedWorker}
- */
-const createIPFSWorker = () =>
-  new SharedWorker(
-    // @ts-ignore - Constructor takes string but webpack needs URL
-    new URL("./worker.js", import.meta.url),
-    { name: "IPFS" },
-  );
--- monorepo/packages/code/worker.tsx
+++ monorepo/packages/code/worker.tsx
@@ -13,27 +13,24 @@ const main = async () => {
     // contexts that run `new SharedWorker(...)`
     // Note: It is important to start listening before we do any async work to
     //  ensure that connections aren't missed while awaiting
-    const connections = listen(self, "connect");
 
-    // Start an IPFS node & create server that will expose its API to all clients
-    // over message channel
-    // const {create } = Ipfs;
+    const connections = []
+    // queue connections that occur while node was starting.
+    self.onconnect = ({ports}) => connections.push(...ports)
 
     const ipfs = await create({ isNode: false, isWebWorker: true });
     // And add hello world for tests
     await ipfs.add({ content: "hello world" });
 
-    // @ts-ignore
-    // const { IPFSService, Server } = IpfsMessagePortServer;
-
     const service = new IPFSService(ipfs);
     const server = new Server(service);
     self.server = server;
     self.ipfs = ipfs;
-    // const orbit = await OrbitDB.createInstance(ipfs)
-    // self.orbit = orbit;
 
-    // db.events.on("replicated", address => {
+
+
+    
+    
     //   console.log(db.iterator({ limit: -1 }).collect())
     // })
 
@@ -42,46 +39,15 @@ const main = async () => {
     // await db.load()
 
     // connect every queued and future connection to the server
-    for await (const event of connections) {
-      const port = event.ports[0];
-      if (port) {
-        server.connect(port);
-      }
+    self.onconnect = ({ports}) => server.connect(ports[0])
+    for (const port of connections.splice(0)) {
+      server.connect(port)
     }
     // const result = db.iterator({ limit: -1 }).collect()
     // console.log(JSON.stringify(result, null, 2))
   } catch (err) {
     console.error(err);
   }
-};
-
-const listen = function (target, type, options) {
-  const events = [];
-  let resume;
-  let ready = new Promise((resolve) => (resume = resolve));
-
-  const write = (event) => {
-    events.push(event);
-    resume();
-  };
-  const read = async () => {
-    await ready;
-    ready = new Promise((resolve) => (resume = resolve));
-    return events.splice(0);
-  };
-
-  const reader = async function* () {
-    try {
-      while (true) {
-        yield* await read();
-      }
-    } finally {
-      target.removeEventListener(type, write, options);
-    }
-  };
-
-  target.addEventListener(type, write, options);
-  return reader();
 };
 
 main();
