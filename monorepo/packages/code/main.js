08c435b40f87e5344d8474131748030c3dad1cf7
Zoltan Erdos <z@zoltans-mbp.lan>
2022-06-03 17:20:19 +0100

    pre-release

--- monorepo/packages/code/main.js
+++ monorepo/packages/code/main.js
@@ -1,8 +1,13 @@
 import { IPFSClient } from "ipfs-message-port-client";
-import "es-module-shims";
+
+navigator.serviceWorker.onmessage = onServiceWorkerMessage
+
+  // @ts-ignore - register expects string but webPack requires this URL hack.
+  navigator.serviceWorker.register("/sw.js", {
+    scope: "/",
+  });
 
 // URL to the script containing ipfs-message-port-server.
-const IPFS_SERVER_URL = new URL("worker.js", location.host);
 const load = async (path) => {
   const paths = path && path.split("/") || [];
   const protocol = path.length || "";
@@ -15,17 +20,7 @@ const load = async (path) => {
   }
 };
 
-const getIpfsPort = () =>(new SharedWorker(IPFS_SERVER_URL, { name: "IPFS" })).port;
-const ipfs = IPFSClient.from(getIpfsPort());
 const ipfsSw = async () => {
-  
-  navigator.serviceWorker.onmessage = (e) => onServiceWorkerMessage(e);
-
-  // @ts-ignore - register expects string but webPack requires this URL hack.
-  await navigator.serviceWorker.register("/sw.js", {
-    scope: "/",
-  });
-  
   await navigator.serviceWorker.ready;
 
   // This is just for testing, lets us know when SW is ready.
@@ -34,14 +29,12 @@ const ipfsSw = async () => {
   // are loaded from service worker. However it could be that such a URL is loaded
   // before the service worker was registered in which case our server just loads a blank
   if (document.documentElement.dataset.viewer == null) {
-  return  load(location.pathname);
+    return load(location.pathname);
   }
-await  loadApp()
-  
+  await loadApp();
 };
 ipfsSw();
-
-
+const getIpfsPort = () =>IPFSClient.from((new SharedWorker(new URL("./worker.js", location.origin), { name: "IPFS" })).port);
 function onServiceWorkerMessage(event) {
   /** @type {null|ServiceWorker} */
   const serviceWorker = (event.source);
@@ -62,20 +55,19 @@ function onServiceWorkerMessage(event) {
   }
 }
 
-
-const loadApp =async () => {
-
-const {run} = await importShim("./ws.mjs", import.meta.url)
-if (window.startedWithNativeEsmModules) return;
-await run();
-
-}
+const loadApp = async () => {
+  await import("es-module-shims");
+  
+  const { run } = await importShim("./ws.mjs", import.meta.url);
+  if (window.startedWithNativeEsmModules) return;
+  await run();
+};
 
 async function syncCachesLater() {
   const registration = await navigator.serviceWorker.ready;
   try {
-    await registration.çƒ.register('sync-cache');
+    await registration.çƒ.register("sync-cache");
   } catch {
-    console.log('Background Sync could not be registered!');
+    console.log("Background Sync could not be registered!");
   }
 }
