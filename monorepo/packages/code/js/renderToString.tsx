f2793141a991681683f1d15729c588d2b6eec858
Zoltan Erdos <z@zoltans-mbp.lan>
2022-06-03 19:56:47 +0100

    pre-release

--- monorepo/packages/code/js/renderToString.tsx
+++ monorepo/packages/code/js/renderToString.tsx
@@ -8,6 +8,44 @@ import { renderToString } from "react-dom/server";
 import { appFactory, createJsBlob } from "./starter";
 import { prettierCss, prettierHtml } from "./prettierEsm";
 
+import { CacheProvider } from '@emotion/react'
+import { renderToString } from 'react-dom/server'
+import createEmotionServer from '@emotion/server/create-instance'
+import createCache from '@emotion/cache'
+
+
+export const getIframe = (App: FC) => {
+const key = 'custom'
+const cache = createCache({ key })
+const { extractCriticalToChunks, constructStyleTagsFromChunks } = createEmotionServer(cache)
+
+const html = renderToString(
+  <CacheProvider value={cache}>
+    <App />
+  </CacheProvider>
+)
+
+const chunks = extractCriticalToChunks(html)
+const styles = constructStyleTagsFromChunks(chunks)
+
+return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My site</title>
+    ${styles}
+</head>
+<body>
+    <div id="root">${html}</div>
+
+    <script src="./bundle.js"></script>
+</body>
+</html>`;
+}
+
+
 export const renderFromString = async (transpiled: string) => {
   console.log("render to string");
   const App = await getApp(transpiled);
