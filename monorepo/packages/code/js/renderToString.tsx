d98e58b3347515f0166091032a5e996f79284493
Zoltan Erdos <z@zoltans-mbp.lan>
2022-06-06 14:19:02 +0100

    pre-release

--- monorepo/packages/code/js/renderToString.tsx
+++ monorepo/packages/code/js/renderToString.tsx
@@ -3,50 +3,11 @@
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import type { FC } from "react";
-import { renderToString } from "react-dom/server";
 
 import { appFactory, createJsBlob } from "./starter";
 import { prettierCss, prettierHtml } from "./prettierEsm";
 
-import { CacheProvider } from "@emotion/react";
 import { renderToString } from "react-dom/server";
-
-export const getIframe = async (App: FC) => {
-  var Buffer = require("buffer/").Buffer;
-
-  const createEmotionServer =
-    (await import("@emotion/server/create-instance")).default;
-
-  const key = "custom";
-  const cache = createCache({ key });
-  const { extractCriticalToChunks, constructStyleTagsFromChunks } =
-    createEmotionServer(cache);
-
-  const html = renderToString(
-    <CacheProvider value={cache}>
-      <App />
-    </CacheProvider>,
-  );
-
-  const chunks = extractCriticalToChunks(html);
-  const styles = constructStyleTagsFromChunks(chunks);
-
-  return `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My site</title>
-    ${styles}
-</head>
-<body>
-    <div id="root">${html}</div>
-
-    <script src="./bundle.js"></script>
-</body>
-</html>`;
-};
 
 export const renderFromString = async (transpiled: string) => {
   console.log("render to string");
