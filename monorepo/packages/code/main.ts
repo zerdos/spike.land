9856072859847db34cc34167f23a61250c45dee9
Zoltan Erdos <z@zoltans-mbp.lan>
2022-06-10 15:57:45 +0100

    pre-release

--- monorepo/packages/code/main.ts
+++ monorepo/packages/code/main.ts
@@ -1,9 +1,7 @@
 const loadApp = async () => {
-  console.log("es-module-shims import and start the app!");
   if (window.startedWithNativeEsmModules) return;
-
+  console.log("... with ES-module-shims!");
   await import("es-module-shims");
 };
 
 setTimeout(loadApp, 200);
-WSH
---
+++ monorepo/sites/esbuild-worker/README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-fvw3nv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-fvw3nv)
