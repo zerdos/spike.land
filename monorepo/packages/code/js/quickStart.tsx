0ed02ea775ab6513e04e2a7f5aca17d92f950fb4
Zoltan Erdos <z@Zoltans-MacBook-Pro.local>
2022-06-10 08:02:48 +0100

    pre-release

--- monorepo/packages/code/js/quickStart.tsx
+++ monorepo/packages/code/js/quickStart.tsx
@@ -1,43 +1,44 @@
 /** @jsxImportSource @emotion/react */
-import shiki from 'shiki';
+// import * as shiki from 'shiki';
 import { isMobile } from "./isMobile.mjs";
 import { AceEditor } from "./AceEditor";
 import { MonacoEditor } from "./MonacoEditor";
-impoer {useEffect, useState} from "react";
+import {useEffect} from "react";
 import { renderPreviewWindow } from "./renderPreviewWindow";
-import { useEffect, useInsertionEffect } from "react";
-import { useSpring } from 'types/framer.js';
 import { mST } from 'session.js';
 import { useState } from 'preact/hooks';
 
 
-const  Shiki = () => {
-  const [code, setCode] = useState(mST().code);
+// const  Shiki = () => {
+//   // const [code, setCode] = useState(mmST().code);
 
-    const [innerHtml, setInnerHtml] = useState('');
-  useEffect(()=>{
+//     const [innerHtml, setInnerHtml] = useState('');
+//   useEffect(()=>{
 
-    const run = async()=>{
-      const highliter = await shiki.getHighlighter({
-    theme: 'nord'
-  });
-     setInnerHtml(highliter.codeToHtml(`console.log('shiki');`, { lang: 'js' }))
-  }
-  run();
-}, [code])
+//     const run = async()=>{
+//       const highliter = await shiki.getHighlighter({
+//     theme: 'nord'
+//   });
+//      setInnerHtml(highliter.codeToHtml(mST().code, { lang: 'ts' }))
+//   }
+//   run();
+// }, [code])
   
-  return <div dangerouslySetInnerHTML={{__html: innerHtml}} />
-}
+//   return <div dangerouslySetInnerHTML={{__html: innerHtml}} />
+// }
 
 globalThis.editable=false;
 
 export async function quickStart() {
-  if (!globalThis.editable) return  renderPreviewWindow(Shiki);
+  // if (!globalThis.editable) return  renderPreviewWindow(Shiki);
+
+
+if (isMobile()) {
+  return renderPreviewWindow(AceEditor);
 }
+return renderPreviewWindow(MonacoEditor);
 
 
-  
 
-  return renderPreviewWindow(MonacoEditor);
 
 }
