df3a57acc5ef7320774a25ef8271ab3a025ca303
Zoltan Erdos <z@zoltans-mbp.lan>
2022-06-22 07:53:54 +0100

    push

--- packages/devcontainer-generator/index.mjs
+++ packages/devcontainer-generator/index.mjs
@@ -8,7 +8,7 @@ var node = {
 };
 var deno = "1.23.0";
 var git = "2.36.1";
-var cypress = "10.1.0";
+var cypress = "10.2.0";
 var yarn = "1.22.19";
 var dotnet3 = "3.1.417";
 var dotnet5 = "5.0.406";
