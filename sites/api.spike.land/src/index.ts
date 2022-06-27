0f2c643d1e13fb3ecee3c6ff46a6fab19c12da27
Zoltan Erdos <z@zoltans-mbp.lan>
2022-06-27 06:37:29 +0100

    berry

--- sites/api.spike.land/src/index.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request: Request, env: EventInit) {
-    try {
-      return new Response(`Hello. ${request.url.toString()}`);
-    } catch (Error) {
-      return new Response(`Yayy... ${Object.prototype.toString.call(Error)}`);
-    }
-  },
-};
