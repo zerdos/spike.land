e1e4a24a9328fbd5da3f28846227986d41ab5da7
Zoltan Erdos <z@zoltans-mbp.lan>
2022-07-01 08:08:31 +0100

    push

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'rzdnec',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
