818ca764d85d13279143613a77ba8715c91dd9a9
Zoltan Erdos <z@zoltans-mbp.lan>
2022-07-20 20:43:40 +0100

    push

--- packages/code/js/startAce.ts
+++ packages/code/js/startAce.ts
@@ -1,20 +1,20 @@
-export async function startAce(code: string) {
-  const ace = (await import("ace-builds/src/ace")).default;
+import {edit, createEditSession} from "ace-builds"
 
+import "ace-builds/src-noconflict/theme-monokai";
+
+import "ace-builds/src-noconflict/mode-typescript";
+
+export async function startAce(code: string) {
   // const {ace} = window;
-  var editor = ace.edit("editor");
-  var js = ace.createEditSession(code);
+  const editor = edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.session.setMode("ace/mode/typescript",
+   ()=> ({ jsx: true }),
+  );
+  const mode = editor.session.getMode();
+  const js = createEditSession(code, mode);
   editor.setSession(js);
 
-  await import("ace-builds/src/theme-monokai");
-
-  editor.setTheme("ace/theme/monokai");
-  await import("ace-builds/src/mode-typescript");
-  // await import("ace-builds/src/mode-typescript-highlight-rules");
-  editor.session.setMode(
-    "ace/mode/typescript",
-    () => ({ jsx: true }),
-  );
 
   return editor;
 }
