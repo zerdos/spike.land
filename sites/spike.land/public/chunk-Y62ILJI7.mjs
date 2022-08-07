df985f82e46348edc87acefde24625d6d175c609
Zoltan Erdos <z@zoltans-mbp.lan>
2022-08-07 12:26:54 +0100

    push

---
+++ sites/spike.land/public/chunk-Y62ILJI7.mjs
@@ -0,0 +1,1 @@
+import{i as e}from"./chunk-PWAMAVFK.mjs";e();async function r(i){return new Promise(n=>{setTimeout(()=>{n()},i)})}export{r as a};
---
+++ sites/spike.land/public/typescript-CPXMTFOD.mjs
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-ZCIP3R2F.mjs";import"./chunk-TRFEAFE6.mjs";import"./chunk-PWAMAVFK.mjs";export{a as conf,b as language};
