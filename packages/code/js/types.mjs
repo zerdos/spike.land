5b87fcad1d7b0e9d4895286da687cb658ff51301
Zoltan Erdos <z@zoltans-mbp.lan>
2022-08-14 10:01:05 +0100

    push

--- packages/code/js/types.mjs
+++ packages/code/js/types.mjs
@@ -1,17 +1,21 @@
+import emotionReactDts from "@emotion/react/types/index.d.ts"
+import emotionReactHelperDts from "@emotion/react/types/helper.d.ts";
+import reactDts from "@types/react/index.d.ts";
+import cssTypeDts from "csstype/index.d.ts";
+
+
 // const reactDts = "https://unpkg.com/../../../node_modules/@types/react/index.d.ts?text";
 // const jsxRuntimeDts = "https://unpkg.com/../../../node_modules/@types/react/jsx-runtime.d.ts?text";
 // const reactExpDts = "https://unpkg.com/@types/react/experimental.d.ts?text";
 // const globalDts = "https://unpkg.com/@types/react/global.d.ts?text";
 // const propTypesDts = "https://unpkg.com/@types/prop-types/index.d.ts?text";
-const cssTypeDts = "https://unpkg.com/csstype/index.d.ts?text";
 const framerDts =
   "https://raw.githubusercontent.com/zerdos/spike.land/main/packages/code/js/types/framer.d.ts";
 const emotionStyleBase =
   "https://unpkg.com/@emotion/styled/types/base.d.ts?text";
 const emotionStyled = "https://unpkg.com/@emotion/styled/types/index.d.ts?text";
 const emotionCache = "https://unpkg.com/@emotion/cache/types/index.d.ts?text";
-const emotionReactDts =
-  "https://unpkg.com/@emotion/react/types/index.d.ts?text";
+
 
 const emotionJSXDts =
   "https://unpkg.com/@emotion/react/types/jsx-runtime.d.ts?text";
@@ -21,8 +25,6 @@ const emotionJSXNameSpaceDTS =
   "https://unpkg.com/@emotion/react/types/jsx-namespace.d.ts?text";
 const emotionReactCssPropDts =
   "https://unpkg.com/@emotion/react/types/css-prop.d.ts?text";
-const emotionReactHelperDts =
-  "https://unpkg.com/@emotion/react/types/helper.d.ts?text";
 const emotionThemingDts =
   "https://unpkg.com/@emotion/react/types/theming.d.ts?text";
 const emotionSerializeDts =
@@ -33,7 +35,7 @@ const emotionUtilsDts =
 // const popmotionDts = "https://unpkg.com/popmotion/lib/index.d.ts?text";
 
 export const dtsFiles = {
-  reactDts: "https://unpkg.com/@types/react/index.d.ts",
+  reactDts,
   jsxRuntimeDts: "https://unpkg.com/@types/react/jsx-runtime.d.ts",
   // reactExpDts,
   // globalDts,
