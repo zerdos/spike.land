480b4e7f95ee981d4dca409e9d72c08592293258
Zoltan Erdos <z@zoltans-mbp.lan>
2022-08-30 01:20:36 +0100

    push

---
+++ packages/code/dist/chunk-types-JKDN7LPE.mjs
@@ -0,0 +1,41 @@
+import {
+  init_define_process
+} from "./chunk-chunk-JAPAFYDL.mjs";
+
+// js/types.mjs
+init_define_process();
+var cssTypeDts = "https://unpkg.com/csstype/index.d.ts";
+var framerDts = "https://raw.githubusercontent.com/zerdos/spike.land/main/packages/code/js/types/framer.d.ts";
+var emotionStyleBase = "https://unpkg.com/@emotion/styled/types/base.d.ts";
+var emotionStyled = "https://unpkg.com/@emotion/styled/types/index.d.ts";
+var emotionCache = "https://unpkg.com/@emotion/cache/types/index.d.ts";
+var emotionReactDts = "https://unpkg.com/@emotion/react/types/index.d.ts";
+var emotionJSXDts = "https://unpkg.com/@emotion/react/types/jsx-runtime.d.ts";
+var emotionJSXRuntimeDTS = "https://unpkg.com/@emotion/react/types/jsx-runtime.d.ts";
+var emotionJSXNameSpaceDTS = "https://unpkg.com/@emotion/react/types/jsx-namespace.d.ts";
+var emotionReactCssPropDts = "https://unpkg.com/@emotion/react/types/css-prop.d.ts";
+var emotionReactHelperDts = "https://unpkg.com/@emotion/react/types/helper.d.ts";
+var emotionThemingDts = "https://unpkg.com/@emotion/react/types/theming.d.ts";
+var emotionSerializeDts = "https://unpkg.com/@emotion/serialize/types/index.d.ts";
+var dtsFiles = {
+  reactDts: "https://unpkg.com/@types/react/index.d.ts",
+  jsxRuntimeDts: "https://unpkg.com/@types/react/jsx-runtime.d.ts",
+  propTypesDts: "https://unpkg.com/@types/prop-types/index.d.ts",
+  cssTypeDts,
+  framerDts,
+  emotionStyleBase,
+  emotionStyled,
+  emotionCache,
+  emotionJSXDts,
+  emotionJSXNameSpaceDTS,
+  emotionJSXRuntimeDTS,
+  emotionReactDts,
+  emotionReactCssPropDts,
+  emotionReactHelperDts,
+  emotionThemingDts,
+  emotionSerializeDts,
+  emotionUtilsDts: "https://unpkg.com/@emotion/utils/types/index.d.ts"
+};
+export {
+  dtsFiles
+};
