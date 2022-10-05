70a158f20ce3314dfc80847a7d10597a891fd2bc
Zoltan Erdos <zoltan.erdos@me.com>
2022-10-05 09:09:20 +0100

    push

---
+++ packages/spike.land/dist/users.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Users = void 0;
+class Users {
+    async fetch(_request, _env) {
+        return new Response("OKi");
+    }
+}
+exports.Users = Users;
+//# sourceMappingURL=users.js.map
