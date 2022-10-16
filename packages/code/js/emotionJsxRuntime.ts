6debf4d18902f1332876170e1aceb6ea8d4871c4
Zoltan Erdos <zoltan.erdos@me.com>
2022-10-16 17:40:39 +0100

    push

--- packages/code/js/emotionJsxRuntime.ts
+++ packages/code/js/emotionJsxRuntime.ts
@@ -1,2 +1,8 @@
-//@ts-ignore
-export { Fragment, jsx, jsxs } from "@emotion/react/jsx-runtime" 
+import { Fragment as __, jsx as ___, jsxs as ____ } from '@emotion/react/jsx-runtime'
+
+globalThis._____= globalThis._____ || {__,___,___};
+const {____} = globalThis;
+export const Fragment = ____.___
+export const jsx  = ____.___
+export const jsxs= ____.____
+
