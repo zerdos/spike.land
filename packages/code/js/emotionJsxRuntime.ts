e0bf1ff96e6c9a4426566681136e618d2262f9bf
Zoltan Erdos <zoltan.erdos@me.com>
2022-10-21 13:18:15 +0100

    push

--- packages/code/js/emotionJsxRuntime.ts
+++ packages/code/js/emotionJsxRuntime.ts
@@ -1,29 +1,9 @@
-const JSXM = {
-  jsxs: (...args) => ReactJSXRuntime.jsxs(...args),
-  jsx: (...args) => window.emotionReact.jsx(...args),
-  Fragment: React.Fragment,
+const JSX = globalThis.emotionReactJSXRuntime ||
+  window.emotionReactJSXRuntime || {
+  jsxs: (...args) => JSX.jsxs(...args),
+  jsx: (...args) => JSX.jsx(...args),
+  Fragment: JSX.Fragment || "",
 };
 
-export const { jsx, jsxs, Fragment } = JSXM;
-export default JSXM;
-// //@ts-expect-error
-// export { Fragment } from "react/jsx-runtime";
-// import { jsx as jsxEmotion } from "@emotion/react";
-// import { jsx as jsxReact } from "react/jsx-runtime";
-
-// function jsx(type, props, key) {
-//   if (!hasOwnProperty.call(props, "css")) {
-//     return jsxReact(type, props, key);
-//   }
-
-//   return jsxEmotion(type, props, key);
-// }
-// function jsxs(type, props, key) {
-//   if (!hasOwnProperty.call(props, "css")) {
-//     return jsxReact(type, props, key);
-//   }
-
-//   return jsxEmotion(type, props, key);
-// }
-
-// export { jsx, jsxs };
+export const { jsx, jsxs, Fragment } = JSX;
+export default jsx;
