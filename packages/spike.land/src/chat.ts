1655b9777b1828e73626eb30e79b6ec1d387d712
Zoltan Erdos <zoltan.erdos@me.com>
2022-10-22 03:05:32 +0100

    push

--- packages/spike.land/src/chat.ts
+++ packages/spike.land/src/chat.ts
@@ -68,6 +68,7 @@ export default {
     env: CodeEnv,
     ctx: ExecutionContext,
   ) {
+    if (request.cf?.asOrganization?.startsWith("YANDEX") ) return null;
     console.log(JSON.stringify({...request.cf}, null, 2));
     return handleErrors(request, async () => {
       console.log(`handling request: ${request.url}`);
