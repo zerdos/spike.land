d9dd8d9f069b03c76ae0e9c3e1792c4b9b767a0f
Zoltan Erdos <zoltan.erdos@me.com>
2022-10-26 16:03:51 +0100

    push

--- packages/code/dist/sw.js
+++ packages/code/dist/sw.js
@@ -1,1 +1,31 @@
-"use strict";(()=>{var r=new BroadcastChannel(location.origin),o={};r.onmessage=s=>{console.log(s),s.data.type==="set-mock"&&(o[s.data.filePath]=s.data.content)};(void 0).onfetch=s=>new URL(s.request.url).href==="/mocks"?s.respondWith(new Response(JSON.stringify(o),{headers:{"Content-Type":"application/json;charset=UTF-8","Cache-Control":"no-cache"}})):o[s.request.url]?s.respondWith(new Response(o[s.request.url])):s.respondWith(fetch(s.request));})();
+"use strict";
+(() => {
+  // sw.mjs
+  var bc = new BroadcastChannel(location.origin);
+  var mocks = {};
+  bc.onmessage = (event) => {
+    console.log(event);
+    if (event.data.type === "set-mock") {
+      mocks[event.data.filePath] = event.data.content;
+    }
+  };
+  (void 0).onfetch = (event) => {
+    const url = new URL(event.request.url);
+    if (url.href === "/mocks") {
+      return event.respondWith(
+        new Response(JSON.stringify(mocks), {
+          headers: {
+            "Content-Type": "application/json;charset=UTF-8",
+            "Cache-Control": "no-cache"
+          }
+        })
+      );
+    }
+    if (mocks[event.request.url]) {
+      return event.respondWith(
+        new Response(mocks[event.request.url])
+      );
+    }
+    return event.respondWith(fetch(event.request));
+  };
+})();
