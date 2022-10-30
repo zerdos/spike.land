ed321fc071b95ac7f850f4f3aa9d2a44f7210bd4
Zoltan Erdos <zoltan.erdos@me.com>
2022-10-30 20:21:10 +0000

    push

--- packages/code/dist/load.mjs
+++ packages/code/dist/load.mjs
@@ -9,51 +9,53 @@ import {
 
 // js/load.ts
 init_define_process();
-import "./react-jsx-runtime.mjs";
 Object.assign(globalThis, { umdTransform });
-var { hydrateRoot, createRoot } = ReactDOMClient;
-var codeSpace = location.pathname.slice(1).split("/")[1];
-var start = (dry = false) => import(`${location.origin}/live/${codeSpace}/mST.mjs`).then(
-  ({
-    mST,
-    codeSpace: codeSpace2,
-    address
-  }) => import(`${location.origin}/ws.mjs`).then(
-    ({ run }) => run({
+(async () => {
+  await import("./react-jsx-runtime.mjs");
+  const { hydrateRoot, createRoot } = ReactDOMClient;
+  const codeSpace = location.pathname.slice(1).split("/")[1];
+  const start = (dry = false) => import(`${location.origin}/live/${codeSpace}/mST.mjs`).then(
+    ({
       mST,
-      dry,
       codeSpace: codeSpace2,
       address
-    })
-  )
-);
-if (location.pathname.endsWith("/hydrated")) {
-  let i = document.getElementById("root")?.getAttribute("data-i");
-  let unmount = () => {
-  };
-  const bc = new BroadcastChannel(location.origin);
-  bc.onmessage = (event) => {
-    if (event.data.codeSpace === codeSpace) {
-      const rootDiv = document.createElement("div");
-      rootDiv.style.height = "100%";
-      i = event.data.sess.i;
-      const root = createRoot(rootDiv);
-      const current = i;
-      importShim(
-        `${location.origin}/live/${codeSpace}/index.js/${i}`
-      ).then((mod) => i === current && root.render(mod.default())).then(
-        () => document.querySelectorAll("#root>div>div")[0].replaceWith(rootDiv)
-      ).then(unmount).then(
-        () => unmount = () => {
-          root.unmount();
-        }
-      );
-    }
-  };
-  importShim(
-    `${location.origin}/live/${codeSpace}/index.js/${i}`
-  ).then((mod) => hydrateRoot(document.querySelectorAll("#root>div>div")[0], mod.default()));
-} else {
-  const dry = false;
-  start(dry);
-}
+    }) => import(`${location.origin}/ws.mjs`).then(
+      ({ run }) => run({
+        mST,
+        dry,
+        codeSpace: codeSpace2,
+        address
+      })
+    )
+  );
+  if (location.pathname.endsWith("/hydrated")) {
+    let i = document.getElementById("root")?.getAttribute("data-i");
+    let unmount = () => {
+    };
+    const bc = new BroadcastChannel(location.origin);
+    bc.onmessage = (event) => {
+      if (event.data.codeSpace === codeSpace) {
+        const rootDiv = document.createElement("div");
+        rootDiv.style.height = "100%";
+        i = event.data.sess.i;
+        const root = createRoot(rootDiv);
+        const current = i;
+        importShim(
+          `${location.origin}/live/${codeSpace}/index.js/${i}`
+        ).then((mod) => i === current && root.render(mod.default())).then(
+          () => document.querySelectorAll("#root>div>div")[0].replaceWith(rootDiv)
+        ).then(unmount).then(
+          () => unmount = () => {
+            root.unmount();
+          }
+        );
+      }
+    };
+    importShim(
+      `${location.origin}/live/${codeSpace}/index.js/${i}`
+    ).then((mod) => hydrateRoot(document.querySelectorAll("#root>div>div")[0], mod.default()));
+  } else {
+    const dry = false;
+    start(dry);
+  }
+})();
--- packages/code/js/load.ts
+++ packages/code/js/load.ts
@@ -1,70 +1,72 @@
 import { umdTransform } from "runner.js";
-import "./react-jsx-runtime.mjs";
 
 Object.assign(globalThis, { umdTransform });
 // importShim.addImportMap(importmap);
 
-const { hydrateRoot, createRoot } = ReactDOMClient;
+(async () => {
+  await import("./react-jsx-runtime.mjs");
+  const { hydrateRoot, createRoot } = ReactDOMClient;
 
-const codeSpace = location.pathname.slice(1).split("/")[1];
+  const codeSpace = location.pathname.slice(1).split("/")[1];
 
-const start = (dry = false) =>
-  import(
-    `${location.origin}/live/${codeSpace}/mST.mjs`
-  ).then(({
-    mST,
-    codeSpace,
-    address,
-  }) =>
-    import(`${location.origin}/ws.mjs`).then(({ run }) =>
-      run({
-        mST,
-        dry,
-        codeSpace,
-        address,
-      })
-    )
-  );
+  const start = (dry = false) =>
+    import(
+      `${location.origin}/live/${codeSpace}/mST.mjs`
+    ).then(({
+      mST,
+      codeSpace,
+      address,
+    }) =>
+      import(`${location.origin}/ws.mjs`).then(({ run }) =>
+        run({
+          mST,
+          dry,
+          codeSpace,
+          address,
+        })
+      )
+    );
 
-if (location.pathname.endsWith("/hydrated")) {
-  let i = document.getElementById("root")?.getAttribute("data-i");
-  let unmount = () => {};
-  const bc = new BroadcastChannel(location.origin);
-  bc.onmessage = (event) => {
-    if (
-      event.data.codeSpace === codeSpace
-    ) {
-      const rootDiv = document.createElement("div");
-      rootDiv.style.height = "100%";
+  if (location.pathname.endsWith("/hydrated")) {
+    let i = document.getElementById("root")?.getAttribute("data-i");
+    let unmount = () => {};
+    const bc = new BroadcastChannel(location.origin);
+    bc.onmessage = (event) => {
+      if (
+        event.data.codeSpace === codeSpace
+      ) {
+        const rootDiv = document.createElement("div");
+        rootDiv.style.height = "100%";
 
-      i = event.data.sess.i;
+        i = event.data.sess.i;
 
-      const root = createRoot(rootDiv);
+        const root = createRoot(rootDiv);
 
-      const current = i;
-      importShim(
-        `${location.origin}/live/${codeSpace}/index.js/${i}`,
-      ).then((mod) => i === current && root.render(mod.default())).then(
-        () => document.querySelectorAll("#root>div>div")[0].replaceWith(rootDiv),
-      ).then(unmount).then(() =>
-        unmount = () => {
-          root.unmount();
-        }
-      );
-    }
-  };
-  importShim(
-    `${location.origin}/live/${codeSpace}/index.js/${i}`,
-  ).then((mod) => hydrateRoot(document.querySelectorAll("#root>div>div")[0], mod.default()));
-  // .then(() => {
-  //   setTimeout(() => {
-  //     const dry = true;
-  //     start(dry);
-  //     import("./prettierEsm").then((x) => x.prettierJs("dry"));
-  //     import("./esbuildEsm").then((x) => x.transform("dry"));
-  //   }, 1000);
-  // });
-} else {
-  const dry = false;
-  start(dry);
-}
+        const current = i;
+        importShim(
+          `${location.origin}/live/${codeSpace}/index.js/${i}`,
+        ).then((mod) => i === current && root.render(mod.default())).then(
+          () => document.querySelectorAll("#root>div>div")[0].replaceWith(rootDiv),
+        ).then(unmount).then(() =>
+          unmount = () => {
+            root.unmount();
+          }
+        );
+      }
+    };
+    importShim(
+      `${location.origin}/live/${codeSpace}/index.js/${i}`,
+    ).then((mod) => hydrateRoot(document.querySelectorAll("#root>div>div")[0], mod.default()));
+    // .then(() => {
+    //   setTimeout(() => {
+    //     const dry = true;
+    //     start(dry);
+    //     import("./prettierEsm").then((x) => x.prettierJs("dry"));
+    //     import("./esbuildEsm").then((x) => x.transform("dry"));
+    //   }, 1000);
+    // });
+  } else {
+    const dry = false;
+    start(dry);
+  }
+})();
