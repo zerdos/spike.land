98ac4442e5e112e923532fadac5779133cfb4c43
Zoltan Erdos <zoltan.erdos@me.com>
2022-10-31 22:00:15 +0000

    push

--- packages/code/dist/emotionJsxRuntime.mjs
+++ packages/code/dist/emotionJsxRuntime.mjs
@@ -1,7 +1,1 @@
-// js/emotionJsxRuntime.ts
-var { Fragment, jsx, jsxs } = globalThis.emotionReactJsxRuntime;
-export {
-  Fragment,
-  jsx,
-  jsxs
-};
+var{Fragment:o,jsx:n,jsxs:x}=globalThis.emotionReactJsxRuntime;export{o as Fragment,n as jsx,x as jsxs};
