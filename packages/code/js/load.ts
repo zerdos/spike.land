c014e5d191baeb4b56719b67254c20e637d70a01
Zoltan Erdos <zoltan.erdos@me.com>
2022-10-31 21:05:37 +0000

    push

--- packages/code/js/load.ts
+++ packages/code/js/load.ts
@@ -1,72 +1,64 @@
-import { umdTransform } from "runner.js";
+import { createRoot, hydrateRoot } from "./reactMod";
 
-Object.assign(globalThis, { umdTransform });
-// importShim.addImportMap(importmap);
+const codeSpace = location.pathname.slice(1).split("/")[1];
 
-(async () => {
-  const { ReactDOMClient } = await import("./react-jsx-runtime");
-  const { hydrateRoot, createRoot } = ReactDOMClient;
+const start = (dry = false) =>
+  import(
+    `${location.origin}/live/${codeSpace}/mST.mjs`
+  ).then(({
+    mST,
+    codeSpace,
+    address,
+  }) =>
+    import(`${location.origin}/ws.mjs`).then(({ run }) =>
+      run({
+        mST,
+        dry,
+        codeSpace,
+        address,
+      })
+    )
+  );
 
-  const codeSpace = location.pathname.slice(1).split("/")[1];
+if (location.pathname.endsWith("/hydrated")) {
+  let i = document.getElementById("root")?.getAttribute("data-i");
+  let unmount = () => {};
+  const bc = new BroadcastChannel(location.origin);
+  bc.onmessage = (event) => {
+    if (
+      event.data.codeSpace === codeSpace
+    ) {
+      const rootDiv = document.createElement("div");
+      rootDiv.style.height = "100%";
 
-  const start = (dry = false) =>
-    import(
-      `${location.origin}/live/${codeSpace}/mST.mjs`
-    ).then(({
-      mST,
-      codeSpace,
-      address,
-    }) =>
-      import(`${location.origin}/ws.mjs`).then(({ run }) =>
-        run({
-          mST,
-          dry,
-          codeSpace,
-          address,
-        })
-      )
-    );
+      i = event.data.sess.i;
 
-  if (location.pathname.endsWith("/hydrated")) {
-    let i = document.getElementById("root")?.getAttribute("data-i");
-    let unmount = () => {};
-    const bc = new BroadcastChannel(location.origin);
-    bc.onmessage = (event) => {
-      if (
-        event.data.codeSpace === codeSpace
-      ) {
-        const rootDiv = document.createElement("div");
-        rootDiv.style.height = "100%";
+      const root = createRoot(rootDiv);
 
-        i = event.data.sess.i;
-
-        const root = createRoot(rootDiv);
-
-        const current = i;
-        importShim<() => JSX.Element, {}>(
-          `${location.origin}/live/${codeSpace}/index.js/${i}`,
-        ).then((mod) => i === current && root.render(mod.default())).then(
-          () => document.querySelectorAll("#root>div>div")[0].replaceWith(rootDiv),
-        ).then(unmount).then(() =>
-          unmount = () => {
-            root.unmount();
-          }
-        );
-      }
-    };
-    importShim<() => JSX.Element, {}>(
-      `${location.origin}/live/${codeSpace}/index.js/${i}`,
-    ).then(mod => hydrateRoot(document.querySelectorAll("#root>div>div")[0], mod.default()));
-    // .then(() => {
-    //   setTimeout(() => {
-    //     const dry = true;
-    //     start(dry);
-    //     import("./prettierEsm").then((x) => x.prettierJs("dry"));
-    //     import("./esbuildEsm").then((x) => x.transform("dry"));
-    //   }, 1000);
-    // });
-  } else {
-    const dry = false;
-    start(dry);
-  }
-})();
+      const current = i;
+      importShim<() => JSX.Element, {}>(
+        `${location.origin}/live/${codeSpace}/index.js/${i}`,
+      ).then((mod) => i === current && root.render(mod.default())).then(
+        () => document.querySelectorAll("#root>div>div")[0].replaceWith(rootDiv),
+      ).then(unmount).then(() =>
+        unmount = () => {
+          root.unmount();
+        }
+      );
+    }
+  };
+  importShim<() => JSX.Element, {}>(
+    `${location.origin}/live/${codeSpace}/index.js/${i}`,
+  ).then(mod => hydrateRoot(document.querySelectorAll("#root>div>div")[0], mod.default()));
+  // .then(() => {
+  //   setTimeout(() => {
+  //     const dry = true;
+  //     start(dry);
+  //     import("./prettierEsm").then((x) => x.prettierJs("dry"));
+  //     import("./esbuildEsm").then((x) => x.transform("dry"));
+  //   }, 1000);
+  // });
+} else {
+  const dry = false;
+  start(dry);
+}
