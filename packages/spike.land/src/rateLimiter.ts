cc62f513733805e8550b9daed3e2c956d290d354
Zoltan Erdos <zolika84@gmail.com>
2022-11-09 10:30:25 +0000

    push

--- packages/spike.land/src/rateLimiter.ts
+++ packages/spike.land/src/rateLimiter.ts
@@ -1,27 +1,11 @@
-import { CodeEnv } from "./env";
 import { handleErrors } from "./handleErrors";
-// =======================================================================================
-// The RateLimiter Durable Object class.
-
-// RateLimiter implements a Durable Object that tracks the frequency of messages from a particular
-// source and decides when messages should be dropped because the source is sending too many
-// messages.
-//
-// We utilize this in ChatRoom, above, to apply a per-IP-address rate limit. These limits are
-// global, i.e. they apply across all chat rooms, so if a user spams one chat room, they will find
-// themselves rate limited in all other chat rooms simultaneously.
-
 export class CodeRateLimiter {
-  constructor(, env) {
-    // Timestamp at which this IP will next be allowed to send a message. Start in the distant
-    // past, i.e. the IP can send a message now.
-    this.nextAllowedTime = 0;
-  }
+  private nextAllowedTime = 0;
 
   // Our protocol is: POST when the IP performs an action, or GET to simply read the current limit.
   // Either way, the result is the number of seconds to wait before allowing the IP to perform its
   // next action.
-  async fetch(url, request) {
+  async fetch(url: string, request: Request) {
     return await handleErrors(request, async () => {
       const now = Date.now() / 1000;
 
@@ -37,8 +21,8 @@ export class CodeRateLimiter {
       //
       // We provide a "grace" period of 20 seconds, meaning that the client can make 4-5 requests
       // in a quick burst before they start being limited.
-      let cooldown = Math.max(0, this.nextAllowedTime - now - 20);
-      return new Response(cooldown);
+      const cooldown = Math.max(0, this.nextAllowedTime - now - 1);
+      return new Response(`${cooldown}`);
     });
   }
 }
