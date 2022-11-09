e404e238e2cc2def7634f726bf0758ea6300cee7
Zoltan Erdos <zolika84@gmail.com>
2022-11-09 10:13:04 +0000

    push

--- packages/spike.land/src/rateLimiterClient.ts
+++ packages/spike.land/src/rateLimiterClient.ts
@@ -3,13 +3,15 @@
 import { CodeRateLimiter } from ".";
 
 export class RateLimiterClient {
+  private limiter;
+  private inCooldown: boolean;
   // The constructor takes two functions:
   // * getLimiterStub() returns a new Durable Object stub for the RateLimiter object that manages
   //   the limit. This may be called multiple times as needed to reconnect, if the connection is
   //   lost.
   // * reportError(err) is called when something goes wrong and the rate limiter is broken. It
   //   should probably disconnect the client, so that they can reconnect and start over.
-  constructor(getLimiterStub, reportError) {
+  constructor(private getLimiterStub: () => CodeRateLimiter, private reportError: (err: unknown) => void) {
     this.getLimiterStub = getLimiterStub;
     this.reportError = reportError;
 
@@ -40,9 +42,7 @@ export class RateLimiterClient {
         // Currently, fetch() needs a valid URL even though it's not actually going to the
         // internet. We may loosen this in the future to accept an arbitrary string. But for now,
         // we have to provide a dummy URL that will be ignored at the other end anyway.
-        response = await this.limiter.fetch("https://dummy-url", {
-          method: "POST",
-        });
+        response = await this.limiter.fetch("https://dummy-url");
       } catch (err) {
         // `fetch()` threw an exception. This is probably because the limiter has been
         // disconnected. Stubs implement E-order semantics, meaning that calls to the same stub
@@ -54,14 +54,12 @@ export class RateLimiterClient {
         // Anyway, get a new limiter and try again. If it fails again, something else is probably
         // wrong.
         this.limiter = this.getLimiterStub();
-        response = await this.limiter.fetch("https://dummy-url", {
-          method: "POST",
-        });
+        response = await this.limiter.fetch("https://dummy-url");
       }
 
       // The response indicates how long we want to pause before accepting more requests.
-      let cooldown = +(await response.text());
-      await new Promise((resolve) => setTimeout(resolve, cooldown * 1000));
+      const cooldown = +(await response.text());
+      await wait(cooldown * 1000);
 
       // Done waiting.
       this.inCooldown = false;
@@ -69,4 +67,12 @@ export class RateLimiterClient {
       this.reportError(err);
     }
   }
+}
+
+export async function wait(delay: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(delay);
+    }, delay);
+  });
 }
