e34b782e62bb7d18db2b145c8b5dd75f25483523
Zoltan Erdos <zoltan.erdos@me.com>
2022-11-17 14:24:41 +0000

    push

--- packages/code/js/reactMod.ts
+++ packages/code/js/reactMod.ts
@@ -1,81 +1,39 @@
-import * as ReactNameSpace from "react";
 import React from "react";
 
-const {
-  Children,
-  Component,
-  Fragment,
-  PureComponent,
-  StrictMode,
-  Suspense,
-  cloneElement,
-  createContext,
-  createElement,
-  createFactory,
-  createRef,
-  forwardRef,
-  isValidElement,
-  lazy,
-  memo,
-  startTransition,
-  useCallback,
-  useContext,
-  useDebugValue,
-  useDeferredValue,
-  useEffect,
-  useId,
-  useImperativeHandle,
-  useInsertionEffect,
-
-  useLayoutEffect,
-  useMemo,
-  useReducer,
-  useRef,
-  useState,
-  useSyncExternalStore,
-  useTransition,
-  // @ts-ignore
-  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
-  version,
-  Profiler,
-  ...rest
-} = ReactNameSpace;
-
-export {
-  // @ts-ignore
-  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
-  Children,
-  cloneElement,
-  Component,
-  createContext,
-  createElement,
-  createFactory,
-  createRef,
-  forwardRef,
-  Fragment,
-  isValidElement,
-  lazy,
-  memo,
-  PureComponent,
-  rest,
-  startTransition,
-  StrictMode,
-  Suspense,
-  useCallback,
-  useContext,
-  useDebugValue,
-  useDeferredValue,
-  useEffect,
-  useId,
-  useImperativeHandle,
-  useInsertionEffect,
-  useLayoutEffect,
-  useMemo,
-  useReducer,
-  useRef,
-  useState,
-  useSyncExternalStore,
-  useTransition,
-};
+export const Children = React.Children;
+export const Component = React.Component;
+export const Fragment = React.Fragment;
+export const PureComponent = React.PureComponent;
+export const StrictMode = React.StrictMode;
+export const Suspense = React.Suspense;
+export const cloneElement = React.cloneElement;
+export const createContext = React.createContext;
+export const createElement = React.createElement;
+export const createFactory = React.createFactory;
+export const createRef = React.createRef;
+export const forwardRef = React.forwardRef;
+export const isValidElement = React.isValidElement;
+export const lazy = React.lazy;
+export const memo = React.memo;
+export const startTransition = React.startTransition;
+export const useCallback = React.useCallback;
+export const useContext = React.useContext;
+export const useDebugValue = React.useDebugValue;
+export const useDeferredValue = React.useDeferredValue;
+export const useEffect = React.useEffect;
+export const useId = React.useId;
+export const useImperativeHandle = React.useImperativeHandle;
+export const useInsertionEffect = React.useInsertionEffect;
+export const useLayoutEffect = React.useLayoutEffect;
+export const useMemo = React.useMemo;
+export const useReducer = React.useReducer;
+export const useRef = React.useRef;
+export const useState = React.useState;
+export const useSyncExternalStore = React.useSyncExternalStore;
+export const useTransition = React.useTransition;
+export const __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED =
+  React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+export const version = React.version;
+export const Profiler = React.Profiler;
 
 export default React;
