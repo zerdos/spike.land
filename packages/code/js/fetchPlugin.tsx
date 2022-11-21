0f5180a971854a06c13b58382b0b5abcf0b7997f
Zoltan Erdos <zolika84@gmail.com>
2022-11-20 20:47:43 -0500

    push

--- packages/code/js/fetchPlugin.tsx
+++ packages/code/js/fetchPlugin.tsx
@@ -16,16 +16,6 @@ export const fetchPlugin = (inputCode: string) => {
         };
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path,
-        );
-
-        if (cachedResult) {
-          return cachedResult;
-        }
-      });
-
       build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
         const escaped = data
@@ -50,6 +40,14 @@ export const fetchPlugin = (inputCode: string) => {
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path,
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
