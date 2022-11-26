44aced7fdfa6525a75781f0cc3efccfd07be41f2
Zoltan Erdos <zoltan.erdos@me.com>
2022-11-26 12:21:43 +0000

    s

--- packages/spike.land/src/index.ts
+++ packages/spike.land/src/index.ts
@@ -2,5 +2,7 @@ import chat from "./chat";
 export { Code } from "./chatRoom";
 export { CodeRateLimiter } from "./rateLimiter";
 export { Users } from "./users";
+import R2 from './r2bucket'
+export {R2}
 
 export default chat;
---
+++ packages/spike.land/src/r2bucket.ts
@@ -0,0 +1,37 @@
+export default {
+    async fetch(request, env) {
+      const url = new URL(request.url);
+      const key = url.pathname.slice(1);
+  
+      switch (request.method) {
+        case 'PUT':
+          await env.r2_bucket.put(key, request.body);
+          return new Response(`Put ${key} successfully!`);
+        case 'GET':
+          const object = await env.r2_bucket.get(key);
+  
+          if (object === null) {
+            return new Response('Object Not Found', { status: 404 });
+          }
+  
+          const headers = new Headers();
+          object.writeHttpMetadata(headers);
+          headers.set('etag', object.httpEtag);
+  
+          return new Response(object.body, {
+            headers,
+          });
+        case 'DELETE':
+          await env.r2_bucket.delete(key);
+          return new Response('Deleted!');
+  
+        default:
+          return new Response('Method Not Allowed', {
+            status: 405,
+            headers: {
+              Allow: 'PUT, GET, DELETE',
+            },
+          });
+      }
+    },
+  };
