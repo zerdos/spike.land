18b7baf73538640da3ffedd13f7f212f2186ad34
Zoltan Erdos <zoltan.erdos@me.com>
2022-11-28 11:20:45 +0000

    push

--- packages/code/js/unpkg-path-plugin.tsx
+++ packages/code/js/unpkg-path-plugin.tsx
@@ -1,12 +1,12 @@
 import * as esbuild from "esbuild-wasm";
 
-export const unpkgPathPlugin = (inputCode: string) => {
+export const unpkgPathPlugin = (codeSpace: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       // handle root entry file
-      build.onResolve({ filter: /(^live\/index\.js$)/ }, (args: any) => {
-        return { path: "/live/index.js", namespace: "a" };
+      build.onResolve({ filter: /(^index\.js$)/ }, (args: any) => {
+        return { path: `${location.origin}/live/${codeSpace}/index.js`, namespace: "a" };
       });
       // handle rel paths in module :  includes ./ || ../
       build.onResolve({ filter: /^\.+\// }, (args: any) => {
