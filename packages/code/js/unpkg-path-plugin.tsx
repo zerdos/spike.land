399041b257265aa1f7656ffcaff9a0877b237a5a
Zoltan Erdos <zoltan.erdos@me.com>
2022-11-29 10:28:11 +0000

    push

--- packages/code/js/unpkg-path-plugin.tsx
+++ packages/code/js/unpkg-path-plugin.tsx
@@ -5,24 +5,25 @@ export const unpkgPathPlugin = {
   setup(build: esbuild.PluginBuild) {
     // handle rel paths in module :  includes ./ || ../
     build.onResolve({ filter: /^\.+\// }, (args: any) => {
-      const url = new URL(args.path, `${location.origin}/${args.resolveDir}`).toString();
+      const url = new URL(args.path, location.origin).toString();
 
       return {
         path: url,
         namespace: "http-url",
       };
     });
-    // handle main file
-    build.onResolve({ filter: /.*/ }, async (args: any) => {
+
+    build.onResolve({ filter: /^\[a-z]+\// }, (args: any) => {
       if (args.path.indexOf(location.origin) !== -1) {
         return {
           namespace: "http-url",
           path: args.path,
         };
       }
+
       return {
-        namespace: "http-url",
         path: `${location.origin}/npm:/${args.path}`,
+        namespace: "http-url",
       };
     });
   },
