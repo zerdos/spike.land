bfee13406688051edef84ec3d6081b6990b38ec7
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-01 14:11:24 +0000

    push

--- packages/code/js/fetchPlugin.tsx
+++ packages/code/js/fetchPlugin.tsx
@@ -9,30 +9,31 @@ export const fetchPlugin: Plugin = {
     // esbuild doesn't attempt to map them to a file system location.
     // Tag them with the "http-url" namespace to associate them with
     // this plugin.
-    build.onResolve({ filter: /^https?:\/\// }, args => ({
-      path: args.path,
-      namespace: "http-url",
-    }));
+    // build.onResolve({ filter: /^https?:\/\// }, args => ({
+    //   path: args.path,
+    //   namespace: "http-url",
+    // }));
 
     // We also want to intercept all import paths inside downloaded
     // files and resolve them against the original URL. All of these
     // files will be in the "http-url" namespace. Make sure to keep
     // the newly resolved URL in the "http-url" namespace so imports
     // inside it will also be resolved as URLs recursively.
-    build.onResolve({ filter: /^\.*/, namespace: "http-url" }, args => ({
-      path: new URL(args.path, args.importer).toString(),
+    build.onResolve({ filter: /.*/, namespace: "http-url" }, args => ({
+      path: importShim.resolve(args.path, args.importer),
       namespace: "http-url",
     }));
 
-    build.onResolve({ filter: /^.*/, namespace: "http-url" }, args => ({
-      path: new URL(args.path, location.origin + "/npm:/").toString(),
-      namespace: "http-url",
-    }));
+    // build.onResolve({ filter: /^.*/, namespace: "http-url" }, args => ({
+    //   path: new URL(args.path, location.origin + "/npm:/").toString(),
+    //   namespace: "http-url",
+    // }));
     // When a URL is loaded, we want to actually download the content
     // from the internet. This has just enough logic to be able to
     // handle the example import from unpkg.com but in reality this
     // would probably need to be more complex.
     build.onLoad({ filter: /.*/, namespace: "http-url" }, async (args) => {
+      // importShim.resolve(args.path, args.importer)
       let contents = (await esmTransform(await fetch(args.path).then(res => res.text())));
       return { contents };
     });
