a67b16ef08968867dea4f6508a9d1df1427217a7
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-02 16:25:13 +0000

    push

--- packages/code/dist/sw.js
+++ packages/code/dist/sw.js
@@ -5,93 +5,51 @@
 
   // js/sw.ts
   self.addEventListener("activate", () => {
-    const bc = new BroadcastChannel(location.origin);
-    bc.onmessage((ev) => {
-      if (ev.data.type === "bundle") {
-      }
-    });
-  });
-  self.memoryCache = self.memoryCache || localForage.createInstance({
-    name: "memoryCache"
-  });
-  var lastChecked = 0;
-  var npmCache;
-  var chunkCache;
-  var fileCache;
-  var cacheName = "default";
-  var files = {};
-  var assets = {};
-  var getCacheName = /* @__PURE__ */ __name(() => fetch(location.origin + "/files.json").then((resp) => {
-    if (!resp.ok)
-      return;
-    const assetHash = resp.headers.get("asset_hash");
-    if (assetHash === null)
-      return;
-    if (cacheName === assetHash)
-      return;
-    cacheName = assetHash;
-    Object.assign(assets, { [assetHash]: resp.json() });
-    assets[assetHash].then((f) => files = f);
-  }), "getCacheName");
-  self.addEventListener("fetch", function(event) {
-    return event.respondWith((async () => {
-      let url = new URL(event.request.url);
-      let isChunk = url.pathname.includes("chunk-");
-      if (files && files[url.pathname.slice(1)]) {
-        isChunk = true;
-        url = new URL(files[url.pathname.slice(1)], url.origin);
-      }
-      if (url.pathname.indexOf("/live/") !== -1) {
-        const controller = new AbortController();
-        let req = new Request(event.request.url, {
-          ...event.request,
-          signal: controller.signal
-        });
-        let resp = await fetch(req);
-        if (!resp.ok)
-          return resp;
-        resp = new Response(resp.body, resp);
-        const contentHash = resp.headers.get("content_hash");
-        if (contentHash) {
-          const { memoryCache } = self;
-          let body = await memoryCache.getItem(contentHash);
-          if (body === null) {
-            body = await resp.text();
-            await memoryCache.setItem(contentHash, body);
-          } else {
-            controller.abort();
+    let lastChecked = 0;
+    let npmCache;
+    let chunkCache;
+    let fileCache;
+    let cacheName = "default";
+    let files = {};
+    const getCacheName = /* @__PURE__ */ __name(() => fetch(location.origin + "/files.json").then((resp) => {
+      if (!resp.ok)
+        return;
+    }), "getCacheName");
+    self.addEventListener("fetch", function(event) {
+      return event.respondWith(async () => {
+        let url = new URL(event.request.url);
+        let isChunk = url.pathname.includes("chunk-");
+        if (files && files[url.pathname.slice(1)]) {
+          isChunk = true;
+          url = new URL(files[url.pathname.slice(1)], url.origin);
+        }
+        if (url.pathname.indexOf("/live/") !== -1) {
+          const myCache = url.pathname.includes("npm:/v") ? npmCache = npmCache || await caches.open(url.pathname.slice(0, 10)) : url.pathname.includes("chunk-") || isChunk ? chunkCache = chunkCache || await caches.open("chunks") : fileCache = fileCache || await caches.open(`fileCache`);
+          if (Date.now() - lastChecked > 4e4) {
+            lastChecked = Date.now();
+            setTimeout(() => getCacheName());
           }
-          return new Response(body, resp);
-        }
-        return resp;
-      }
-      const myCache = url.pathname.includes("npm:/v") ? npmCache = npmCache || await caches.open(url.pathname.slice(0, 10)) : url.pathname.includes("chunk-") || isChunk ? chunkCache = chunkCache || await caches.open("chunks") : fileCache = fileCache || await caches.open(`fileCache`);
-      if (Date.now() - lastChecked > 4e4) {
-        lastChecked = Date.now();
-        setTimeout(() => getCacheName());
-      }
-      let request = event.request;
-      const cacheKey = new Request(
-        url
-      );
-      let response = await myCache.match(cacheKey);
-      if (response)
-        return response;
-      try {
-        response = await fetch(request);
-        if (!response.ok || !response.body)
+          if (url.origin !== location.origin) {
+            url = new URL(location.origin + "/:z:" + btoa(url.toString()));
+          }
+          let request = event.request;
+          const cacheKey = new Request(
+            url
+          );
+          let response = await myCache.match(cacheKey);
+          if (response && response.ok)
+            return response;
+          response = await fetch(request);
+          if (response === void 0 || !response.ok) {
+            return new Response(response && response.body || "no response", { ...response, status: 500 });
+          }
+          response = new Response(response.body, response);
+          if (response.headers.get("Cache-Control") !== "no-cache" || isChunk) {
+            event.waitUntil(myCache.put(cacheKey, response.clone()));
+          }
           return response;
-        response = new Response(response.body, response);
-        if (response.headers.get("Cache-Control") !== "no-cache" || isChunk) {
-          event.waitUntil(myCache.put(cacheKey, response.clone()));
         }
-        return response;
-      } catch {
-        return new Response("oh no!", {
-          status: 500,
-          statusText: `Could not fetch:  ${request.url}`
-        });
-      }
-    })());
+      });
+    });
   });
 })();
--- packages/code/js/sw.ts
+++ packages/code/js/sw.ts
@@ -1,141 +1,84 @@
 export type {};
 
-interface MyServiceWorkerScope extends ServiceWorkerGlobalScope {
-  memoryCache: LocalForage;
-}
-
-declare const self: MyServiceWorkerScope;
+declare const self: ServiceWorkerGlobalScope;
 
 self.addEventListener("activate", () => {
-  const bc = new BroadcastChannel(location.origin);
+  // const bc = new BroadcastChannel(location.origin);
 
-  bc.onmessage(ev => {
-    if (ev.data.type === "bundle") {
-    }
-  });
-});
-// async function wait(delay) {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve();
-//     }, delay);
-//   });
-// }
+  // bc.onmessage(ev => {
+  //   if (ev.data.type === "bundle") {
+  //   }
+  // });
 
-self.memoryCache = self.memoryCache || localForage.createInstance({
-  name: "memoryCache",
-});
+  // if (assets[content)])
+  let lastChecked = 0;
+  let npmCache: Cache | null;
 
-let lastChecked = 0;
-let npmCache: Cache | null;
+  let chunkCache: Cache | null;
+  let fileCache: Cache | null;
+  let cacheName = "default";
+  let files: { [k: string]: string } = {};
+  // const assets: { [assetHash: string]: Promise<typeof files> } = {};
+  const getCacheName = () =>
+    fetch(location.origin + "/files.json").then((resp) => {
+      if (!resp.ok) return;
+    });
 
-let chunkCache: Cache | null;
-let fileCache: Cache | null;
-let cacheName = "default";
-let files: { [k: string]: string } = {};
-const assets: { [assetHash: string]: Promise<typeof files> } = {};
-const getCacheName = () =>
-  fetch(location.origin + "/files.json").then((resp) => {
-    if (!resp.ok) return;
+  self.addEventListener("fetch", function(event) {
+    return event.respondWith(async () => {
+      let url = new URL(event.request.url);
+      let isChunk = url.pathname.includes("chunk-");
 
-    const assetHash = resp.headers.get("asset_hash");
-    if (assetHash === null) return;
-    if (cacheName === assetHash) return;
-    cacheName = assetHash;
-    Object.assign(assets, { [assetHash]: resp.json() });
-    assets[assetHash].then((f) => files = f);
-  });
-
-self.addEventListener("fetch", function(event) {
-  return event.respondWith((async () => {
-    let url = new URL(event.request.url);
-
-    //  if (event.request.url === "https://example.com/importmap") {
-    //  return new Response(JSON.stringify(importMap), {
-    //     status: 200,
-    //     headers: {
-    //       "Access-Control-Allow-Origin": "*",
-    //       "Cache-Control": "no-cache",
-    //       "Content-Type": "application/json; charset=UTF-8",
-    //     },
-    //   });
-    // }
-    let isChunk = url.pathname.includes("chunk-");
-    if (files && files[url.pathname.slice(1)]) {
-      isChunk = true;
-      url = new URL(files[url.pathname.slice(1)], url.origin);
-    }
-    if (
-      url.pathname.indexOf("/live/") !== -1
-    ) {
-      const controller = new AbortController();
-
-      let req = new Request(event.request.url, {
-        ...event.request,
-        signal: controller.signal,
-      });
-      let resp = await fetch(req);
-      if (!resp.ok) return resp;
-      resp = new Response(resp.body, resp);
-      const contentHash = resp.headers.get("content_hash");
-      if (contentHash) {
-        const { memoryCache } = self;
-
-        let body = await memoryCache.getItem<string>(contentHash);
-        if (body === null) {
-          body = await resp.text();
-
-          await memoryCache.setItem(contentHash, body);
-        } else {
-          controller.abort();
-        }
-        return new Response(body, resp);
+      if (files && files[url.pathname.slice(1)]) {
+        isChunk = true;
+        url = new URL(files[url.pathname.slice(1)], url.origin);
       }
-      return resp;
-    }
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    const myCache = url.pathname.includes("npm:/v")
-      ? (npmCache = npmCache || await caches.open(url.pathname.slice(0, 10)))
-      : (url.pathname.includes("chunk-") || isChunk)
-      ? (chunkCache = chunkCache || await caches.open("chunks"))
-      : (fileCache = fileCache || await caches.open(`fileCache`));
-
-    if (Date.now() - lastChecked > 40_000) {
-      lastChecked = Date.now();
-      setTimeout(() => getCacheName());
-    }
-
-    // if (
-    //   url.origin !== location.origin
-    // ) {
-    //   url = new URL(location.origin + "/:z:" + btoa(url.toString()));
-    // }
-
-    let request = event.request;
-    const cacheKey = new Request(
-      url,
-    );
-
-    let response = await myCache.match(cacheKey);
-
-    if (response) return response;
-
-    try {
-      response = await fetch(request);
-      if (!response.ok || !response.body) return response;
-      response = new Response(response.body, response);
       if (
-        response.headers.get("Cache-Control") !== "no-cache" || isChunk
+        url.pathname.indexOf("/live/") !== -1
       ) {
-        event.waitUntil(myCache.put(cacheKey, response.clone()));
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        const myCache = url.pathname.includes("npm:/v")
+          ? (npmCache = npmCache || await caches.open(url.pathname.slice(0, 10)))
+          : (url.pathname.includes("chunk-") || isChunk)
+          ? (chunkCache = chunkCache || await caches.open("chunks"))
+          : (fileCache = fileCache || await caches.open(`fileCache`));
+
+        if (Date.now() - lastChecked > 40_000) {
+          lastChecked = Date.now();
+          setTimeout(() => getCacheName());
+        }
+
+        if (
+          url.origin !== location.origin
+        ) {
+          url = new URL(location.origin + "/:z:" + btoa(url.toString()));
+        }
+
+        let request = event.request;
+
+        const cacheKey = new Request(
+          url,
+        );
+
+        let response = await myCache.match(cacheKey);
+
+        if (response && response.ok) return response;
+
+        response = await fetch(request);
+
+        if (response === undefined || !response.ok) {
+          return new Response((response && response.body) || "no response", { ...response, status: 500 });
+        }
+
+        response = new Response(response.body, response);
+        if (
+          response.headers.get("Cache-Control") !== "no-cache" || isChunk
+        ) {
+          event.waitUntil(myCache.put(cacheKey, response.clone()));
+        }
+        return response;
       }
-      return response;
-    } catch {
-      return new Response("oh no!", {
-        status: 500,
-        statusText: `Could not fetch:  ${request.url}`,
-      });
-    }
-  })());
+    });
+  });
 });
