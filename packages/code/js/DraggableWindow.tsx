0ebd176867730653bb513176fdcbf3b44149874b
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-03 22:16:44 +0000

    push

--- packages/code/js/DraggableWindow.tsx
+++ packages/code/js/DraggableWindow.tsx
@@ -69,11 +69,10 @@ export const DraggableWindow: FC<DraggableWindowProps> = (
 
     // zBodyRef.current.innerHTML = zBodyRef.current.innerHTML || mST().html;
     const reveal = async () => {
-      await wait(500);
-      // setPositions({
-      //   bottom: window.innerHeight * 0.2,
-      //   right: window.innerWidth * 0.2,
-      // });
+      setPositions({
+        bottom: -25,
+        right: -45,
+      });
 
       if (window.innerWidth / devicePixelRatio < 600) {
         changeScaleRange(50);
@@ -98,7 +97,6 @@ export const DraggableWindow: FC<DraggableWindowProps> = (
       } else if (window.innerWidth / devicePixelRatio > 2500) {
         setWidth(breakPoints[1]);
         setHeight(breakPointHeights[1]);
-
         changeScaleRange(100);
       }
 
