58439f16bba9d14ec7a23f9d3bcf8388b48a5c37
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-04 17:29:57 +0000

    push

--- packages/code/js/worker-dom/src/test/main-thread/test-main-thread.d.ts
+++
@@ -1,8 +0,0 @@
-// Using "@types/jsdom" causes a bunch of conflicting type errors.
-// May be due to https://github.com/DefinitelyTyped/DefinitelyTyped/issues/21517.
-declare module "jsdom" {
-  export class JSDOM {
-    public readonly window: Window;
-    constructor(html: string, options: object);
-  }
-}
