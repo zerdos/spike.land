8112fb8d9e710e3673b981a68098733dff41326c
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-04 18:13:21 +0000

    push

--- packages/code/js/runner.tsx
+++ packages/code/js/runner.tsx
@@ -1,7 +1,7 @@
 // Import type { Dispatch, ReactNode, SetStateAction } from "react";
 import type { TransformOptions } from "esbuild-wasm";
 import debounce from "lodash.debounce";
-import { buildT, transform } from "./esbuildEsm";
+import { build, buildT, transform } from "./esbuildEsm";
 import { render } from "./renderToString";
 import { md5, mST, patchSync } from "./session";
 import { toUmd } from "./toUmd";
@@ -97,7 +97,10 @@ export async function runner({ code, counter, codeSpace }: {
   //   (await import("./esbuildEsm.ts")).transform;
 
   try {
-    const transpiledCode = await esmTransform(await build(code));
+    const ab = new AbortController();
+    const pp = await buildT(codeSpace, counter, ab.signal);
+    if (!pp) return;
+    const transpiledCode = await esmTransform(pp);
 
     const { html, css } = await render(transpiledCode, codeSpace);
 
