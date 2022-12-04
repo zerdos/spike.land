c99067e5591b31b836a113d2ec5327b7036c6f44
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-04 10:49:53 +0000

    push

--- packages/code/dist/react-jsx-runtime.mjs
+++ packages/code/dist/react-jsx-runtime.mjs
@@ -1,4 +1,6 @@
-import "./chunk-chunk-JDIMLRES.mjs";
+import {
+  run
+} from "./chunk-chunk-JDIMLRES.mjs";
 import "./chunk-chunk-ERFM3H56.mjs";
 import "./chunk-chunk-ZZWIKWD4.mjs";
 import "./chunk-chunk-NBK6NTLB.mjs";
@@ -11,7 +13,9 @@ import "./chunk-chunk-FJRKYGWZ.mjs";
 import "./chunk-chunk-OH444ZSQ.mjs";
 import "./chunk-chunk-ZL6L5B7C.mjs";
 import "./chunk-chunk-JQLFMSSW.mjs";
-import "./chunk-chunk-VSR6Z5WK.mjs";
+import {
+  require_react_error_boundary_umd
+} from "./chunk-chunk-VSR6Z5WK.mjs";
 import {
   require_client
 } from "./chunk-chunk-FFMS35Y7.mjs";
@@ -20,6 +24,7 @@ import {
   require_react
 } from "./chunk-chunk-UX3KX3KY.mjs";
 import {
+  __name,
   __toESM,
   init_define_process
 } from "./chunk-chunk-A3E5PINE.mjs";
@@ -31,7 +36,58 @@ init_define_process();
 init_define_process();
 var import_react = __toESM(require_react(), 1);
 var import_client = __toESM(require_client(), 1);
+var import_react_error_boundary = __toESM(require_react_error_boundary_umd(), 1);
 var import_jsx_runtime = __toESM(require_emotion_react_jsx_runtime_cjs(), 1);
+var hydrate = /* @__PURE__ */ __name(async (codeSpace, i) => {
+  const App = (await import(`${location.origin}/live/${codeSpace}/index.js/${i}`)).default;
+  (0, import_client.hydrateRoot)(
+    document.getElementById("root"),
+    /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react.StrictMode, { children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)(
+      import_react_error_boundary.ErrorBoundary,
+      {
+        fallbackRender: ({ error }) => /* @__PURE__ */ (0, import_jsx_runtime.jsxs)("div", { role: "alert", children: [
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)("div", { children: "Oh n o" }),
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)("pre", { children: error.message })
+        ] }),
+        children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)(App, {})
+      }
+    ) })
+  );
+}, "hydrate");
 
 // js/load.ts
 init_define_process();
+var load_default = /* @__PURE__ */ __name(async () => {
+  const codeSpace = location.pathname.slice(1).split("/")[1];
+  const {
+    mST,
+    address
+  } = await import(`${location.origin}/live/${codeSpace}/mST.mjs`);
+  run({
+    mST,
+    dry: false,
+    codeSpace,
+    address
+  });
+}, "default");
+
+// js/react-jsx-runtime.tsx
+(async () => {
+  const paths = location.pathname.split("/");
+  const codeSpace = paths[2];
+  const rootEl = document.getElementById(`root`);
+  const bc = new BroadcastChannel(location.origin);
+  if (location.pathname.includes("hydrated")) {
+    await hydrate(codeSpace, 1);
+    bc.onmessage = async (event) => {
+      if (event.data.codeSpace === codeSpace && location.pathname.includes("dehydrated")) {
+        const { html, css } = event.data.sess;
+        rootEl.innerHTML = `<style>${css}</style>${html}`;
+      } else {
+        await hydrate(codeSpace, event.data.sess.i);
+      }
+    };
+  } else {
+    await load_default();
+  }
+})();
