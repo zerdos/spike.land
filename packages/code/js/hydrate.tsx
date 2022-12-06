92e5449ffbd7cf67bb4eb44d9122abcf8c115a47
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-06 17:30:44 +0000

    push

--- packages/code/js/hydrate.tsx
+++ packages/code/js/hydrate.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
+export { md5 } from "./md5";
 
 export const hydrate = async (codeSpace: string, i: number) => {
   const App = (await import(`${location.origin}/live/${codeSpace}/index.js/${i}`)).default;
