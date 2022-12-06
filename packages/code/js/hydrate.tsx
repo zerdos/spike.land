4e119de4b6b5df5a8a0ca62363575793979ed19d
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-06 19:52:38 +0000

    push

--- packages/code/js/hydrate.tsx
+++ packages/code/js/hydrate.tsx
@@ -12,40 +12,42 @@ export { md5 };
 let r: Root;
 let root: HTMLDivElement;
 
-export const hydrate = async (codeSpace: string, sess?: ICodeSession) => {
+export const hydrate = (codeSpace: string, sess?: ICodeSession) => {
   if (r) r.unmount();
-  let rootEl: HTMLDivElement;
-  let App;
-  if (sess) {
-    rootEl = document.createElement("div");
-    const { i, css, html, transpiled } = sess;
-    rootEl.innerHTML = `<style>${css}</style>${html}`.split(md5(transpiled)).join(`css`);
-    document.body.appendChild(rootEl);
-    App = (await import(`${location.origin}/live/${codeSpace}/index.js/${i}`)).default;
-  } else {
-    rootEl = document.getElementById(codeSpace + "-css") as unknown as HTMLDivElement;
-    const rt = document.getElementById("root");
-    const i = rt?.getAttribute("data-i") || 1;
-    App = (await import(`${location.origin}/live/${codeSpace}/index.js/${i}`)).default;
-  }
+  requestAnimationFrame(async () => {
+    let rootEl: HTMLDivElement;
+    let App;
+    if (sess) {
+      rootEl = document.createElement("div");
+      const { i, css, html, transpiled } = sess;
+      rootEl.innerHTML = `<style>${css}</style>${html}`.split(md5(transpiled)).join(`css`);
+      document.body.appendChild(rootEl);
+      App = (await import(`${location.origin}/live/${codeSpace}/index.js/${i}`)).default;
+    } else {
+      rootEl = document.getElementById(codeSpace + "-css") as unknown as HTMLDivElement;
+      const rt = document.getElementById("root");
+      const i = rt?.getAttribute("data-i") || 1;
+      App = (await import(`${location.origin}/live/${codeSpace}/index.js/${i}`)).default;
+    }
 
-  root = rootEl;
+    root = rootEl;
 
-  r = createRoot(rootEl);
-  r.render(
-    <StrictMode>
-      <ErrorBoundary
-        fallbackRender={({ error }) => (
-          <div role="alert">
-            <div>Oh, no!!!</div>
-            <pre>{error.message}</pre>
-          </div>
-        )}
-      >
-        <App />
-      </ErrorBoundary>
-    </StrictMode>,
-  );
+    r = createRoot(rootEl);
+    r.render(
+      <StrictMode>
+        <ErrorBoundary
+          fallbackRender={({ error }) => (
+            <div role="alert">
+              <div>Oh, no!!!</div>
+              <pre>{error.message}</pre>
+            </div>
+          )}
+        >
+          <App />
+        </ErrorBoundary>
+      </StrictMode>,
+    );
+  });
 
   // root = newRoot;
 };
