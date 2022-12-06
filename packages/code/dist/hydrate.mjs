cc28ca040a597baad3001716beda66213e974d0c
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-06 19:52:38 +0000

    push

--- packages/code/dist/hydrate.mjs
+++ packages/code/dist/hydrate.mjs
@@ -31,37 +31,39 @@ var import_react_error_boundary = __toESM(require_react_error_boundary_umd(), 1)
 var import_jsx_runtime = __toESM(require_emotion_react_jsx_runtime_cjs(), 1);
 var r;
 var root;
-var hydrate = /* @__PURE__ */ __name(async (codeSpace, sess) => {
+var hydrate = /* @__PURE__ */ __name((codeSpace, sess) => {
   if (r)
     r.unmount();
-  let rootEl;
-  let App;
-  if (sess) {
-    rootEl = document.createElement("div");
-    const { i, css, html, transpiled } = sess;
-    rootEl.innerHTML = `<style>${css}</style>${html}`.split(md5(transpiled)).join(`css`);
-    document.body.appendChild(rootEl);
-    App = (await import(`${location.origin}/live/${codeSpace}/index.js/${i}`)).default;
-  } else {
-    rootEl = document.getElementById(codeSpace + "-css");
-    const rt = document.getElementById("root");
-    const i = rt?.getAttribute("data-i") || 1;
-    App = (await import(`${location.origin}/live/${codeSpace}/index.js/${i}`)).default;
-  }
-  root = rootEl;
-  r = (0, import_client.createRoot)(rootEl);
-  r.render(
-    /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react.StrictMode, { children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)(
-      import_react_error_boundary.ErrorBoundary,
-      {
-        fallbackRender: ({ error }) => /* @__PURE__ */ (0, import_jsx_runtime.jsxs)("div", { role: "alert", children: [
-          /* @__PURE__ */ (0, import_jsx_runtime.jsx)("div", { children: "Oh, no!!!" }),
-          /* @__PURE__ */ (0, import_jsx_runtime.jsx)("pre", { children: error.message })
-        ] }),
-        children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)(App, {})
-      }
-    ) })
-  );
+  requestAnimationFrame(async () => {
+    let rootEl;
+    let App;
+    if (sess) {
+      rootEl = document.createElement("div");
+      const { i, css, html, transpiled } = sess;
+      rootEl.innerHTML = `<style>${css}</style>${html}`.split(md5(transpiled)).join(`css`);
+      document.body.appendChild(rootEl);
+      App = (await import(`${location.origin}/live/${codeSpace}/index.js/${i}`)).default;
+    } else {
+      rootEl = document.getElementById(codeSpace + "-css");
+      const rt = document.getElementById("root");
+      const i = rt?.getAttribute("data-i") || 1;
+      App = (await import(`${location.origin}/live/${codeSpace}/index.js/${i}`)).default;
+    }
+    root = rootEl;
+    r = (0, import_client.createRoot)(rootEl);
+    r.render(
+      /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react.StrictMode, { children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)(
+        import_react_error_boundary.ErrorBoundary,
+        {
+          fallbackRender: ({ error }) => /* @__PURE__ */ (0, import_jsx_runtime.jsxs)("div", { role: "alert", children: [
+            /* @__PURE__ */ (0, import_jsx_runtime.jsx)("div", { children: "Oh, no!!!" }),
+            /* @__PURE__ */ (0, import_jsx_runtime.jsx)("pre", { children: error.message })
+          ] }),
+          children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)(App, {})
+        }
+      ) })
+    );
+  });
 }, "hydrate");
 export {
   hydrate,
