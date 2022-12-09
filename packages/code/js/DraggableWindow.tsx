5db6283ae2eeb94c1574940e0611dd97b178c7ff
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-09 14:12:22 +0000

    push

--- packages/code/js/DraggableWindow.tsx
+++ packages/code/js/DraggableWindow.tsx
@@ -48,9 +48,7 @@ export const DraggableWindow: FC<DraggableWindowProps> = (
   const scale = Math.sqrt(scaleRange / 100);
 
   const setWidth = (width: number) => {
-    changeScaleRange(100 * window.innerWidth / width);
-    changeMaxScaleRange(100 * window.innerWidth / width);
-
+    changeScaleRange(Math.floor(100 * window.innerWidth / width));
     changeMaxScaleRange(Math.floor(100 * 1 / Math.sqrt(Math.abs(1 - (innerWidth / (width + 40))))));
 
     // changeMaxScaleRange(Math.floor(100 * Math.sqrt(1 - (innerWidth / (width + 40)))));
