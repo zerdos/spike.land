87b8f33a6d58326a14184fafd14c29be50331745
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-09 08:32:27 +0000

    push

--- packages/spike.land/src/staticContent.mjs
+++ packages/spike.land/src/staticContent.mjs
@@ -3,9 +3,9 @@ import ASSET_MANIFEST from "__STATIC_CONTENT_MANIFEST";
 
 const assets = JSON.parse(ASSET_MANIFEST);
 
-let files = {};
-Object.keys(assets).forEach((file) => files = { ...files, [`/${file}`]: `/` + assets[file] });
-export { files };
+export const files = assets;
+// Object.keys(assets).forEach((file) => files = { ...files, [`/${file}`]: `/` + assets[file] });
+// export { files };
 let reverseMap = {};
 
 Object.keys(files).forEach((file) => reverseMap = { ...reverseMap, [files[file]]: file });
