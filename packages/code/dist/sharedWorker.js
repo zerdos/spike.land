21b106a26ef5926f293a8dbd6ca9737d2179cdb7
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-10 22:30:31 +0000

    push

--- packages/code/dist/sharedWorker.js
+++ packages/code/dist/sharedWorker.js
@@ -4,31 +4,11 @@
   var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
   // js/sharedWorker.ts
-  var mod = {};
-  var counters = {};
-  var reconnect = /* @__PURE__ */ __name((codeSpace, name, hashCode) => new Promise(async (resolve) => {
-    if (isPromise(mod[codeSpace])) {
-      return resolve(await mod[codeSpace]);
-    }
-    if (mod[codeSpace] && mod[codeSpace].readyState !== 1)
-      delete mod[codeSpace];
-    const ws = new WebSocket(
-      `wss://${location.host}/live/` + codeSpace + "/websocket"
-    );
-    ws.addEventListener("open", () => {
-      ws.send(JSON.stringify({ name, hashCode }));
-      ws.addEventListener(
-        "message",
-        (ev) => {
-          const mess = { codeSpace, ...JSON.parse(ev.data) };
-          console.log({ mess });
-          bc.postMessage(mess);
-        }
-      );
-      mod[codeSpace] = ws;
-      resolve(ws);
-    });
-  }), "reconnect");
+  self.mod = self.mod || {};
+  self.counters = self.counters || {};
+  self.idToPortMap = self.idToPortMap || {};
+  self.bc = self.bc || new BroadcastChannel(location.origin);
+  var { counters, mod, idToPortMap, bc } = self;
   var onMessage = /* @__PURE__ */ __name(async ({ name, codeSpace, target, type, patch, users, i, address, hashCode, newHash, oldHash, candidate, offer, answer }) => {
     if (!counters[codeSpace])
       counters[codeSpace] = i;
@@ -57,17 +37,11 @@
       mod[codeSpace].send(JSON.stringify(obj));
     }
   }, "onMessage");
-  var idToPortMap = {};
-  var bc;
-  self.addEventListener("connect", (e) => {
+  self.addEventListener("connect", ({ ports }) => {
     console.log("connected");
-    if (!bc) {
-      bc = new BroadcastChannel(location.origin);
-      bc.addEventListener("message", (ev) => onMessage(ev.data));
-    }
-    const port = e.ports[0];
-    port.onmessage = ({ data }) => {
-      idToPortMap[data.name] = port;
+    bc.onmessage = ({ data }) => onMessage(data);
+    ports[0].onmessage = ({ data }) => {
+      idToPortMap[data.name] = ports[0];
       onMessage(data);
     };
   });
@@ -78,4 +52,30 @@
     return false;
   }
   __name(isPromise, "isPromise");
+  function reconnect(codeSpace, name, hashCode) {
+    return new Promise(async (resolve) => {
+      if (isPromise(mod[codeSpace])) {
+        return resolve(await mod[codeSpace]);
+      }
+      if (mod[codeSpace] && mod[codeSpace].readyState !== 1)
+        delete mod[codeSpace];
+      const ws = new WebSocket(
+        `wss://${location.host}/live/` + codeSpace + "/websocket"
+      );
+      ws.addEventListener("open", () => {
+        ws.send(JSON.stringify({ name, hashCode }));
+        ws.addEventListener(
+          "message",
+          (ev) => {
+            const mess = { codeSpace, ...JSON.parse(ev.data) };
+            console.log({ mess });
+            bc.postMessage(mess);
+          }
+        );
+        mod[codeSpace] = ws;
+        resolve(ws);
+      });
+    });
+  }
+  __name(reconnect, "reconnect");
 })();
