a3c4f830920ddb9f75cce0ccc918b5e94b0447e3
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-12 14:30:36 +0000

    push

--- packages/code/js/ws.ts
+++ packages/code/js/ws.ts
@@ -208,12 +208,13 @@ globalThis.broadcast = (msg: string) => {
 // p2pcf.send(peer, new ArrayBuffer(...))
 bc = new BroadcastChannel(location.origin);
 let messagePort: MessagePort;
+const sharedWorker = new SharedWorker("/sharedWorker.js?" + globalThis.assetHash);
+sharedWorker.port.start();
+messagePort = sharedWorker.port;
+messagePort.postMessage({ codeSpace, name: user });
 const ws = {
   send: (message: object) => {
     if (!messagePort) {
-      const sharedWorker = new SharedWorker("/sharedWorker.js?" + globalThis.assetHash);
-      sharedWorker.port.start();
-      messagePort = sharedWorker.port;
       sharedWorker.port.onmessage = ({ data }) => {
         console.log("ONMESSAGE", { data });
 
