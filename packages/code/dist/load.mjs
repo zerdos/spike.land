113a4592e35cb51aebe5c047c0e570178984c086
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-13 14:25:05 +0000

    push

--- packages/code/dist/load.mjs
+++ packages/code/dist/load.mjs
@@ -1,6 +1,6 @@
 import {
   run
-} from "./chunk-chunk-LSXDZJVD.mjs";
+} from "./chunk-chunk-CPZAFDNI.mjs";
 import "./chunk-chunk-S7IISJSO.mjs";
 import "./chunk-chunk-AIF5T3IG.mjs";
 import "./chunk-chunk-KJNWZ244.mjs";
--- packages/code/dist/ws.mjs
+++ packages/code/dist/ws.mjs
@@ -2,7 +2,7 @@ import {
   run,
   sendChannel,
   syncWS
-} from "./chunk-chunk-LSXDZJVD.mjs";
+} from "./chunk-chunk-CPZAFDNI.mjs";
 import "./chunk-chunk-S7IISJSO.mjs";
 import "./chunk-chunk-AIF5T3IG.mjs";
 import "./chunk-chunk-KJNWZ244.mjs";
