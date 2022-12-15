3e0ef915f138e002c4c454c1021a44fc08338834
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-15 22:30:48 +0000

    push

--- packages/code/dist/reactMod.mjs
+++ packages/code/dist/reactMod.mjs
@@ -1,140 +1,14 @@
 import {
-  require_react_error_boundary_umd
-} from "./chunk-chunk-SZ4QZZPG.mjs";
-import {
   require_react
 } from "./chunk-chunk-WOINJVTR.mjs";
 import {
-  __commonJS,
-  __name,
   __toESM,
   init_define_process
 } from "./chunk-chunk-A3E5PINE.mjs";
 
-// node_modules/is-callable/index.js
-var require_is_callable = __commonJS({
-  "node_modules/is-callable/index.js"(exports, module) {
-    "use strict";
-    init_define_process();
-    var fnToStr = Function.prototype.toString;
-    var reflectApply = typeof Reflect === "object" && Reflect !== null && Reflect.apply;
-    var badArrayLike;
-    var isCallableMarker;
-    if (typeof reflectApply === "function" && typeof Object.defineProperty === "function") {
-      try {
-        badArrayLike = Object.defineProperty({}, "length", {
-          get: function() {
-            throw isCallableMarker;
-          }
-        });
-        isCallableMarker = {};
-        reflectApply(function() {
-          throw 42;
-        }, null, badArrayLike);
-      } catch (_) {
-        if (_ !== isCallableMarker) {
-          reflectApply = null;
-        }
-      }
-    } else {
-      reflectApply = null;
-    }
-    var constructorRegex = /^\s*class\b/;
-    var isES6ClassFn = /* @__PURE__ */ __name(function isES6ClassFunction(value) {
-      try {
-        var fnStr = fnToStr.call(value);
-        return constructorRegex.test(fnStr);
-      } catch (e) {
-        return false;
-      }
-    }, "isES6ClassFunction");
-    var tryFunctionObject = /* @__PURE__ */ __name(function tryFunctionToStr(value) {
-      try {
-        if (isES6ClassFn(value)) {
-          return false;
-        }
-        fnToStr.call(value);
-        return true;
-      } catch (e) {
-        return false;
-      }
-    }, "tryFunctionToStr");
-    var toStr = Object.prototype.toString;
-    var objectClass = "[object Object]";
-    var fnClass = "[object Function]";
-    var genClass = "[object GeneratorFunction]";
-    var ddaClass = "[object HTMLAllCollection]";
-    var ddaClass2 = "[object HTML document.all class]";
-    var ddaClass3 = "[object HTMLCollection]";
-    var hasToStringTag = typeof Symbol === "function" && !!Symbol.toStringTag;
-    var isIE68 = !(0 in [,]);
-    var isDDA = /* @__PURE__ */ __name(function isDocumentDotAll() {
-      return false;
-    }, "isDocumentDotAll");
-    if (typeof document === "object") {
-      all = document.all;
-      if (toStr.call(all) === toStr.call(document.all)) {
-        isDDA = /* @__PURE__ */ __name(function isDocumentDotAll(value) {
-          if ((isIE68 || !value) && (typeof value === "undefined" || typeof value === "object")) {
-            try {
-              var str = toStr.call(value);
-              return (str === ddaClass || str === ddaClass2 || str === ddaClass3 || str === objectClass) && value("") == null;
-            } catch (e) {
-            }
-          }
-          return false;
-        }, "isDocumentDotAll");
-      }
-    }
-    var all;
-    module.exports = reflectApply ? /* @__PURE__ */ __name(function isCallable2(value) {
-      if (isDDA(value)) {
-        return true;
-      }
-      if (!value) {
-        return false;
-      }
-      if (typeof value !== "function" && typeof value !== "object") {
-        return false;
-      }
-      try {
-        reflectApply(value, null, badArrayLike);
-      } catch (e) {
-        if (e !== isCallableMarker) {
-          return false;
-        }
-      }
-      return !isES6ClassFn(value) && tryFunctionObject(value);
-    }, "isCallable") : /* @__PURE__ */ __name(function isCallable2(value) {
-      if (isDDA(value)) {
-        return true;
-      }
-      if (!value) {
-        return false;
-      }
-      if (typeof value !== "function" && typeof value !== "object") {
-        return false;
-      }
-      if (hasToStringTag) {
-        return tryFunctionObject(value);
-      }
-      if (isES6ClassFn(value)) {
-        return false;
-      }
-      var strClass = toStr.call(value);
-      if (strClass !== fnClass && strClass !== genClass && !/^\[object HTML/.test(strClass)) {
-        return false;
-      }
-      return tryFunctionObject(value);
-    }, "isCallable");
-  }
-});
-
 // js/reactMod.ts
 init_define_process();
-var import_is_callable = __toESM(require_is_callable(), 1);
-var import_react = __toESM(require_react(), 1);
-var import_react_error_boundary = __toESM(require_react_error_boundary_umd(), 1);
+var React = __toESM(require_react(), 1);
 var {
   Children,
   Component,
@@ -143,6 +17,7 @@ var {
   PureComponent,
   StrictMode,
   Suspense,
+  useState,
   cloneElement,
   createContext,
   createElement,
@@ -168,24 +43,11 @@ var {
   useSyncExternalStore,
   useTransition,
   version
-} = import_react.default;
-var originalUseState = import_react.default.useState;
-function useState(initialState) {
-  if (globalThis.workerDom) {
-    const [state, setState] = originalUseState(initialState);
-    const delayedSetState = /* @__PURE__ */ __name((updates) => setTimeout(() => (0, import_is_callable.default)(updates) ? setState(() => updates()) : setState(updates)), "delayedSetState");
-    return [state, delayedSetState];
-  }
-  return originalUseState(initialState);
-}
-__name(useState, "useState");
-Object.assign(import_react.default, { useState });
-var reactMod_default = import_react.default;
-var export_ErrorBoundary = import_react_error_boundary.ErrorBoundary;
+} = React;
+var reactMod_default = React;
 export {
   Children,
   Component,
-  export_ErrorBoundary as ErrorBoundary,
   Fragment,
   Profiler,
   PureComponent,
