b8c4c182785531942fa1aade8cf1027cab28f388
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-15 17:30:24 +0000

    push

--- packages/spike.land/src/chat.ts
+++ packages/spike.land/src/chat.ts
@@ -197,7 +197,7 @@ const api: ExportedHandler<CodeEnv> = {
           const regex4 = /from"\//gm;
           const regex5 = /import"\//gm;
 
-          if (isText) {
+          if (isText && response.url.endsWith(".d.ts")) {
             const url = response.url;
             const baSe = (new URL(".", url)).toString();
             const parent = (new URL("..", url)).toString();
