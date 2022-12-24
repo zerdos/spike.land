5c1cadb9a59ec7c6447366f47c46c64bb71c244a
z <zolika84@gmail.com>
2022-12-24 18:05:27 +0000

    push

--- packages/code/js/importMapReplace.ts
+++ packages/code/js/importMapReplace.ts
@@ -67,7 +67,7 @@ export function importMapReplace(
       if (x.length === 0 || x.indexOf("import") === -1) return x;
       if (
         x.startsWith("import") && x.indexOf(`"`) !== -1
-        && x.indexOf(`"https://`) === -1
+        && x.indexOf(`https://`) === -1
       ) {
         const slices = x.split(`"`);
         slices[1] = origin + "/npm:/*" + slices[1];
