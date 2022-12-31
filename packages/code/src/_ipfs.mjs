7cff016526ef15d675f04e7846d4484cb1f02672
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-31 04:58:12 +0000

    push

--- packages/code/src/_ipfs.mjs
+++ packages/code/src/_ipfs.mjs
@@ -188,13 +188,6 @@
 // //   }
 // // }
 
-// // const loadApp = async () => {
-// //   console.log("es-module-shims import and start the app!");
-// //   if (window.startedWithNativeEsmModules) return;
-
-// //   await import("es-module-shims");
-// // };
-
 // // setTimeout(loadApp, 200);
 
 // // async function syncCachesLater() {
