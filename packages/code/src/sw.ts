56de01245bfae6b90faa0d2ce7acb8a6221e964c
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-31 05:11:32 +0000

    push

--- packages/code/src/sw.ts
+++ packages/code/src/sw.ts
@@ -48,149 +48,149 @@ let fileCache: Cache | null;
 //     assets[assetHash].then((f) => files = f);
 //   });
 
-self.addEventListener("fetch", function(event) {
-  return event.respondWith(
-    (async () => {
-      let url = new URL(event.request.url);
-      if (url.origin.indexOf("spike.land") === -1) return fetch(event.request);
+const createResponse = async (request: Request) => {
+  let url = new URL(request.url);
+  if (url.origin.indexOf("spike.land") === -1) return fetch(request);
 
-      // const fs = globalThis.fs;
+  // const fs = globalThis.fs;
 
-      if (url.pathname.startsWith("/live")) {
-        try {
-          const file = await readFile(url.pathname);
-          if (file) {
-            return new Response(file, {
-              headers: {
-                "content-type": "application/javascript; charset=utf-8",
-                "Cache-Control": "no-cache",
-              },
-            });
-          }
-        } catch {
-        }
-      }
-
-      let isChunk = url.pathname.includes("chunk-");
-      // if (files && files[url.pathname.slice(1)]) {
-      //   isChunk = true;
-      //   url = new URL(files[url.pathname.slice(1)], url.origin);
-      // }
-      if (
-        url.pathname.indexOf("/live/") !== -1
-      ) {
-        const controller = new AbortController();
-
-        let req = new Request(event.request.url, {
-          ...event.request,
-          signal: controller.signal,
+  if (url.pathname.startsWith("/live")) {
+    try {
+      const file = await readFile(url.pathname);
+      if (file) {
+        return new Response(file, {
+          headers: {
+            "content-type": "application/javascript; charset=utf-8",
+            "Cache-Control": "no-cache",
+          },
         });
-        let resp = await fetch(req);
-        if (!resp.ok) return resp;
-        resp = new Response(resp.body, resp);
-        const contentHash = resp.headers.has("content_hash")
-          ? resp.headers.get("content_hash")
-          : null;
-        if (contentHash) {
-          chunkCache = chunkCache || await caches.open("chunks");
-
-          let cacheKey = new Request(
-            new URL("/" + contentHash, url).toString(),
-          );
-
-          let cachedResp = await chunkCache.match(cacheKey);
-          if (cachedResp) {
-            controller.abort();
-            return cachedResp;
-          }
-
-          cachedResp = new Response(await resp.blob(), resp);
-
-          event.waitUntil(chunkCache.put(cacheKey, cachedResp.clone()));
-          return cachedResp;
-        }
-
-        resp = new Response(resp.body, resp);
-
-        return resp;
       }
+    } catch {
+    }
+  }
 
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      const myCache = url.pathname.includes("npm:/v")
-        ? (npmCache = npmCache || await caches.open(url.pathname.slice(0, 10)))
-        : (url.pathname.includes("chunk-") || isChunk)
-        ? (chunkCache = chunkCache || await caches.open("chunks"))
-        : (fileCache = fileCache || await caches.open(`fileCache`));
+  let isChunk = url.pathname.includes("chunk-");
+  // if (files && files[url.pathname.slice(1)]) {
+  //   isChunk = true;
+  //   url = new URL(files[url.pathname.slice(1)], url.origin);
+  // }
+  if (
+    url.pathname.indexOf("/live/") !== -1
+  ) {
+    const controller = new AbortController();
 
-      if (url.pathname.length < 2) return fetch(event.request);
-      // if (Date.now() - lastChecked > 40_000) {
-      // lastChecked = Date./now();
-      // setTimeout(() => getCacheName());
-      // }
+    let req = new Request(request.url, {
+      ...request,
+      signal: controller.signal,
+    });
+    let resp = await fetch(req);
+    if (!resp.ok) return resp;
+    resp = new Response(resp.body, resp);
+    const contentHash = resp.headers.has("content_hash")
+      ? resp.headers.get("content_hash")
+      : null;
+    if (contentHash) {
+      chunkCache = chunkCache || await caches.open("chunks");
 
-      // if (
-      //   url.origin !== location.origin
-      // ) {
-      //   url = new URL(location.origin + "/:z:" + btoa(url.toString()));
-      // }
-
-      let request = event.request;
-      const cacheKey = new Request(
-        url,
+      let cacheKey = new Request(
+        new URL("/" + contentHash, url).toString(),
       );
 
-      let response = await myCache.match(cacheKey);
-
-      if (response) return response;
-
-      try {
-        request = new Request(request.url, { ...request, redirect: "follow" });
-        response = await fetch(request);
-        if (!response.ok) return response;
-
-        // response = new Response(response.body, response);
-        if (
-          response.status === 307 || response.status === 302 || !response.ok
-          || !response.body
-        ) {
-          return response;
-        }
-
-        if (
-          (url.pathname.indexOf(".ts") !== -1 || url.pathname.indexOf(".tsx") !== -1)
-          && url.pathname.indexOf(".d.ts") === -1
-        ) {
-          const transformed = (await transform(await response.text(), {
-            format: "esm",
-            loader: "tsx",
-            target: "es2022",
-          })).code;
-          if (typeof transformed !== "string") {
-            return new Response("500 transpile error", { status: 500 });
-          }
-
-          response = new Response(transformed, {
-            ...response,
-            status: 200,
-            headers: {
-              "Access-Control-Allow-Origin": "*",
-              "Cache-Control": "no-cache",
-              "Content-Type": "application/json; charset=UTF-8",
-            },
-          });
-        }
-        if (
-          response.headers.get("Cache-Control") !== "no-cache" || isChunk
-        ) {
-          event.waitUntil(myCache.put(cacheKey, response.clone()));
-        }
-        return response;
-      } catch (err) {
-        return new Response("oh no! " + JSON.stringify({ err }), {
-          status: 500,
-          statusText: `Could not fetch:  ${request.url}`,
-        });
+      let cachedResp = await chunkCache.match(cacheKey);
+      if (cachedResp) {
+        controller.abort();
+        return cachedResp;
       }
-    })(),
+
+      cachedResp = new Response(await resp.blob(), resp);
+
+      await chunkCache.put(cacheKey, cachedResp.clone());
+      return cachedResp;
+    }
+
+    resp = new Response(resp.body, resp);
+
+    return resp;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  const myCache = url.pathname.includes("npm:/v")
+    ? (npmCache = npmCache || await caches.open(url.pathname.slice(0, 10)))
+    : (url.pathname.includes("chunk-") || isChunk)
+    ? (chunkCache = chunkCache || await caches.open("chunks"))
+    : (fileCache = fileCache || await caches.open(`fileCache`));
+
+  if (url.pathname.length < 2) return fetch(request);
+  // if (Date.now() - lastChecked > 40_000) {
+  // lastChecked = Date./now();
+  // setTimeout(() => getCacheName());
+  // }
+
+  // if (
+  //   url.origin !== location.origin
+  // ) {
+  //   url = new URL(location.origin + "/:z:" + btoa(url.toString()));
+  // }
+
+  const cacheKey = new Request(
+    url,
+  );
+
+  let response = await myCache.match(cacheKey);
+
+  if (response) return response;
+
+  try {
+    request = new Request(request.url, { ...request, redirect: "follow" });
+    response = await fetch(request);
+    if (!response.ok) return response;
+
+    // response = new Response(response.body, response);
+    if (
+      !response.ok
+    ) {
+      return response;
+    }
+
+    if (
+      (url.pathname.indexOf(".ts") !== -1 || url.pathname.indexOf(".tsx") !== -1)
+      && url.pathname.indexOf(".d.ts") === -1
+    ) {
+      const transformed = (await transform(await response.text(), {
+        format: "esm",
+        loader: "tsx",
+        target: "es2022",
+      })).code;
+      if (typeof transformed !== "string") {
+        return new Response("500 transpile error", { status: 500 });
+      }
+
+      response = new Response(transformed, {
+        ...response,
+        status: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Cache-Control": "no-cache",
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+      });
+    }
+    if (
+      response.headers.get("Cache-Control") !== "no-cache" || isChunk
+    ) {
+      await myCache.put(cacheKey, response.clone());
+    }
+    return response;
+  } catch (err) {
+    return new Response("oh no! " + JSON.stringify({ err }), {
+      status: 500,
+      statusText: `Could not fetch:  ${request.url}`,
+    });
+  }
+};
+
+self.addEventListener("fetch", function(event) {
+  return event.respondWith(
+    createResponse(event.request),
   );
 });
