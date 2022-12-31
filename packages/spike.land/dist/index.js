6bbebb58bbf4aeab6026d79aa0c826d753c16012
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-31 17:31:25 +0000

    push

--- packages/spike.land/dist/index.js
+++ packages/spike.land/dist/index.js
@@ -556,7 +556,7 @@ var init_define_process = __esm({
   }
 });
 
-// ../code/dist/chunk-chunk-M4LX6WM3.mjs
+// ../code/dist/chunk-chunk-3UNVCTI6.mjs
 var require_diff = __commonJS2({
   "../../node_modules/fast-diff/diff.js"(exports, module) {
     init_define_process();
@@ -8746,9 +8746,18 @@ var syncStorage = /* @__PURE__ */ __name(async (_setItem, _getItem, oldSession, 
     message.oldHash
   );
   await setItem(message.oldHash, {
+    i: oldSession.i,
     newHash: message.newHash,
     patch: message.patch,
-    ...oldNode ? { oldHash: oldNode.oldHash, reversePatch: oldNode.reversePatch } : { ...oldSession }
+    ...oldNode ? {
+      oldHash: oldNode.oldHash,
+      reversePatch: oldNode.reversePatch
+    } : {
+      code: oldSession.code,
+      transpiled: oldSession.transpiled,
+      html: oldSession.html,
+      css: oldSession.css
+    }
   });
   await setItem("head", message.newHash);
 }, "syncStorage");
