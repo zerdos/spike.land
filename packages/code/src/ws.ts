f06e0a47dcadc28b735757c1afd85d90a3ea6963
Zoltan Erdos <zoltan.erdos@me.com>
2022-12-31 10:14:49 +0000

    push

--- packages/code/src/ws.ts
+++ packages/code/src/ws.ts
@@ -310,8 +310,8 @@ export const run = async () => {
           messageData.oldHash && messageData.oldHash === messageData.newHash
         ) return;
         messagePort.postMessage(messageData);
-      }
-       messagePort.postMessage({ codeSpace, type: "handshake", name: user, hashCode: hashCode(codeSpace) });
+      };
+      messagePort.postMessage({ codeSpace, type: "handshake", name: user, hashCode: hashCode(codeSpace) });
     } else {
       try {
         const data = JSON.parse(ab2str(ev.data));
