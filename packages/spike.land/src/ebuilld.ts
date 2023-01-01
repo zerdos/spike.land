039ee33dee70dc844ffcef035d64bcef628429be
Zoltan Erdos <zoltan.erdos@me.com>
2023-01-01 11:33:19 +0000

    ebuild

---
+++ packages/spike.land/src/ebuilld.ts
@@ -0,0 +1,71 @@
+import was from "./esbuild.wasm";
+// const wasmModule = new WebAssembly.Instance(mod).exports.Module;
+
+globalThis.performance = {
+    now: ()=>Date.now()
+}
+import {
+    initialize,
+    transform,
+    type TransformOptions,
+  } from "esbuild-wasm";
+
+import { importMapReplace, md5 } from "@spike.land/code/src/session";
+  
+  // import impMap from "./importmap.json";
+  //
+  // import { imports as importMapImports } from "./importmap.json";
+  
+//   import { fetchPlugin } from "./fetchPlugin";
+//   import { importMapReplace } from "./importMapReplace";
+//   import { md5 } from "./md5";
+//   import { unpkgPathPlugin } from "./unpkg-path-plugin";
+  
+  const mod = {
+    init: false as (boolean | Promise<void>),
+    initialize: () => mod.init ||  initialize({
+            wasmModule: was,
+            worker: false
+          }).then(() => mod.init = true) as Promise<void>
+  };
+  
+  export const initAndTransform = async (
+    code: string,
+    opts: TransformOptions,
+    origin: string,
+  ) => {
+    // const code = prettierJs(c)!;
+    const initFinished = mod.initialize();
+    
+
+    if (initFinished !== true) await (initFinished);
+  
+    return await esmTransform(code, origin);
+    
+  };
+
+  export async function esmTransform(code: string, origin: string) {
+    // transform = transform || (await import(`./esbuildEsm`)).transform;
+    const transpiled = await transform(code, {
+      loader: "tsx",
+      format: "esm",
+      treeShaking: true,
+      platform: "browser",
+      minify: false,
+      //   globalName: md5(code),
+      keepNames: true,
+      tsconfigRaw: {
+        compilerOptions: {
+          jsx: "react-jsx",
+          useDefineForClassFields: false,
+          jsxFragmentFactory: "Fragment",
+          jsxImportSource: "@emotion/react",
+        },
+      },
+      target: "es2022",
+    } as unknown as TransformOptions, origin);
+  
+    // apps[md5(transpiled.code)] = require(md5(code));
+    return transpiled.code;
+  }
+  
