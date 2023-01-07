d3ed61d7c2014d9d5aa27720e2960a3e2a4a945c
Zoltan Erdos <zoltan.erdos@me.com>
2023-01-07 18:36:12 +0000

    push

--- packages/code/esbuild-dev.ts
+++ packages/code/esbuild-dev.ts
@@ -18,7 +18,7 @@ import * as esbuild from "https://deno.land/x/esbuild@v0.16.14/mod.js";
 // import { wait } from "./src/wait.mjs";
 
 // await esbuild.initialize();
-const environment = "production";
+const environment = "development";
 
 // = "production" === "production"
 //   ? "production"
