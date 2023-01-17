f5122e7dba6645b65489cade6ddb6d8531c07f38
Zoltan Erdos <zoltan.erdos@me.com>
2023-01-17 19:24:13 +0000

    push

--- packages/code/src/ata.ts
+++ packages/code/src/ata.ts
@@ -143,7 +143,7 @@ export async function ata(
       }
       impRes[new URL(r.indexOf("d.ts") !== -1 ? r : r + "/index.ts", baseUrl).toString()] = {
         url: new URL(r.indexOf("d.ts") !== -1 ? r : r + "/index.ts", baseUrl).toString(),
-        content: `export * from "${newBase}" `,
+        content: `import type {} from "${newBase}" `,
         ref: r,
       };
       // console.log("processing: " + r);
