c222c3a0b6367ca00ba9ed3fe9b9ae1173a52184
Zoltan Erdos <zoltan.erdos@me.com>
2023-01-17 18:37:55 +0000

    push

--- packages/code/src/ata.ts
+++ packages/code/src/ata.ts
@@ -111,8 +111,7 @@ export async function ata(
       const [, ...refs] = refParts;
       res = [
         ...res,
-        ...refs,
-        // ???.map((r) => r.split(`"`)[0]).map((r) =>
+        ...refs.map((r) => r.split(`"`)[0]), // .map((r) =>
         //   (r.startsWith(".") || r.startsWith("https:/"))
         //     ? r
         //     : new URL(`./` + r, baseUrl).toString()
