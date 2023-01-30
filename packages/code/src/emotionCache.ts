0ccf00caaf53cbdc3421028a1f2bed123f8b303d
z <zolika84@gmail.com>
2023-01-30 10:25:38 +0000

    push

--- packages/code/src/emotionCache.ts
+++ packages/code/src/emotionCache.ts
@@ -2,7 +2,5 @@ import createEmotionCache from "@emotion/cache";
 
 const emotionCacheWithExtraDefault = (createEmotionCache as unknown as { default: typeof createEmotionCache })
   .default;
-const cache = (emotionCacheWithExtraDefault)
-  ? emotionCacheWithExtraDefault
-  : createEmotionCache;
+const cache = typeof emotionCacheWithExtraDefault === "function" ? emotionCacheWithExtraDefault : createEmotionCache;
 export default cache;
