487d87ec36714a59b7a4678043362989c72c1f65
Zoltan Erdos <zoltan.erdos@me.com>
2023-03-27 17:48:32 +0100

    push

--- packages/code/src/emotion.ts
+++ packages/code/src/emotion.ts
@@ -1,22 +1,39 @@
 import * as EmotionReact from "@emotion/react";
-export * from "@emotion/react/jsx-runtime";
-
-// import * as EA from "@emotion/react";
+import { Fragment, jsx as emotionJsx, jsxs } from "@emotion/react/jsx-runtime";
 
 export const {
-  // @ts-ignore
   __unsafe_useEmotionCache,
   CacheProvider,
   ClassNames,
-  // @ts-ignore
+
   createElement,
-  css,
   Global,
+  css,
   keyframes,
   ThemeContext,
   ThemeProvider,
   useTheme,
   withEmotionCache,
   withTheme,
-  // @ts-ignore
 } = EmotionReact;
+
+export function jsx(type, props, ...children) {
+  if (props && props.style) {
+    // Convert the style prop to a CSS class using Emotion's css function
+    const classNameFromStyle = css(props.style);
+
+    // Combine the generated class with any existing className prop
+    const existingClassName = props.className || "";
+    props.className = `${existingClassName} ${classNameFromStyle}`.trim();
+
+    // Remove the style prop from the props object
+    delete props.style;
+  }
+
+  // Call the original Emotion jsx runtime with the modified props
+  return emotionJsx(type, props, ...children);
+}
+
+export { Fragment, jsxs };
+
+// import * as EA from "@emotion/react";
