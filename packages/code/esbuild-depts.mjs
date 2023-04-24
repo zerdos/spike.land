e1f145e1bd3d87772b3e5f855d447b420bfdaf66
Zoltan Erdos <zoltan.erdos@me.com>
2023-04-24 13:26:52 +0100

    push

--- packages/code/esbuild-depts.mjs
+++ packages/code/esbuild-depts.mjs
@@ -1,3 +1,3 @@
-import * as esbuild from "https://deno.land/x/esbuild@v0.17.17/mod.js";
+import * as esbuild from "https://deno.land/x/esbuild@v0.17.18/mod.js";
 export const { env, readDir, mkdir, copyFile, symlink } = Deno;
 export { esbuild };
