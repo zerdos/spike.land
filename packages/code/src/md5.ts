ad52b704eae8b484b1209177ceb244e80752f0c5
Zoltan Erdos <zoltan.erdos@me.com>
2023-05-23 14:53:29 +0100

    push

--- packages/code/src/md5.ts
+++ packages/code/src/md5.ts
@@ -1,22 +1,29 @@
 import { Md5 } from "ts-md5";
-// const cache = globalThis.md5cac
-export const md5 = (code: string | object) =>
-  // cache.get(code) || cache.set(
-  // code,
-  md5FULL(typeof code === "string" ? code : JSON.stringify(code)).split("0")
-    .join("k")
-    .split("1").join("g")
-    .split("2").join("j")
-    .split("3").join("k")
-    .split("4").join("b")
-    .split("5").join("n")
-    .split("6").join("o")
-    .split("7").join("x")
-    .split("8").join("q")
-    .split("9").join("z")
-    .slice(0, 8);
-// ).get(code);
 
-function md5FULL(inputString: string) {
-  return Md5.hashStr(inputString);
-}
+const characterMap: { [key: string]: string } = {
+  "0": "k",
+  "1": "g",
+  "2": "j",
+  "3": "k",
+  "4": "b",
+  "5": "n",
+  "6": "o",
+  "7": "x",
+  "8": "q",
+  "9": "z",
+};
+
+export const md5 = (input: string | object): string => {
+  const inputString = typeof input === "string" ? input : JSON.stringify(input);
+  const fullHash = Md5.hashStr(inputString).toString();
+  const hashWithoutZeros = fullHash.replace(/0/g, "");
+  const modifiedHash = hashWithoutZeros.split("").map(char => {
+    if (characterMap[char]) return characterMap[char];
+    return char;
+  }).join("");
+  return modifiedHash.slice(0, 8);
+};
+
+// Now using a object to map characters that need to be replaced,
+// and refactored the code to remove the commented out cache functionality
+// and added some variable names to improve readability.
