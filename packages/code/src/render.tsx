bc36b8faa60aed51d9d764e050ad550c0bc2acab
z <z@z.local>
2023-06-12 16:16:38 +0100

    feat: Check data.transpiled before appFactory; handle AppBundled import error

--- packages/code/src/render.tsx
+++ packages/code/src/render.tsx
@@ -49,15 +49,22 @@ async function rerender(data: ICodeSession & { transpiled: string }) {
     const indexMjs = await stat(`/live/${codeSpace}/index.mjs`);
 
     let AppBundled: typeof AppTranspiled;
-    const AppTranspiled = (await import(
+    
+
+
+    const AppTranspiled =  (await (data.transpiled? appFactory(data.transpiled) :import(
       `${location.origin}/live/${codeSpace}/index.js?i=${data.i}`
-    )).default;
-    // ? await appFactory(data.transpiled)
+    ))).default;
+
 
     if (indexMjs) {
+      try{
       AppBundled = (await import(
         `${location.origin}/live/${codeSpace}/index.mjs`
       )).default;
+      } catch{
+        AppBundled = null;
+      }
     }
 
     const App = AppBundled || AppTranspiled;
