41214ea63eb1715c58a257a37d7f65d20eddc636
Zoltan Erdos <z@zoltans-mbp.lan>
2023-07-14 15:34:15 +0100

    save

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,35 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2022": true,
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  "overrides": [],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-  },
-  "plugins": [
-    "react",
-    "@typescript-eslint",
-  ],
-  "rules": {
-    "@typescript-eslint/no-non-null-assertion": "off",
-  },
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+    }
+}
--- .github/.fixtures/test/esbuild.ts
+++
@@ -1,9 +0,0 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-import * as esbuild from "https://deno.land/x/esbuild@v0.11.16/mod.js";
-const ts = "let hasProcess: boolean = typeof process != \"null\"";
-const result = await esbuild.build(ts, {
-  loader: "ts",
-  logLevel: "warning",
-});
-console.log("result:", result);
-esbuild.stop();
