79189adacfe960a78102019d7c2a982c9784a59e
Zoltan Erdos <zoltan.erdos@me.com>
2024-05-14 11:50:38 +0100

    up

--- .pnp.cjs
+++ .pnp.cjs
@@ -2649,7 +2649,7 @@ const RAW_RUNTIME_STATE =
           ["fast-diff", "npm:1.3.0"],\
           ["fetch-retry", "npm:6.0.0"],\
           ["file-type", "npm:19.0.0"],\
-          ["framer-motion", "virtual:0cccbb210d7ec9866a375ee6aac5ae681292837121ca6cd216111fc5654785c02e2c1d848ea5f25a9bffd881268f2dc1275f829abd835491864f40a96950a34a#npm:11.1.9"],\
+          ["framer-motion", "virtual:0cccbb210d7ec9866a375ee6aac5ae681292837121ca6cd216111fc5654785c02e2c1d848ea5f25a9bffd881268f2dc1275f829abd835491864f40a96950a34a#npm:11.2.0"],\
           ["immutable", "npm:4.3.6"],\
           ["isomorphic-fetch", "npm:3.0.0"],\
           ["jest", "virtual:0cccbb210d7ec9866a375ee6aac5ae681292837121ca6cd216111fc5654785c02e2c1d848ea5f25a9bffd881268f2dc1275f829abd835491864f40a96950a34a#npm:29.7.0"],\
@@ -2704,6 +2704,7 @@ const RAW_RUNTIME_STATE =
           ["esbuild-wasm", "npm:0.21.2"],\
           ["esm-worker", "npm:0.135.16"],\
           ["immutable", "npm:4.3.6"],\
+          ["lib0", "npm:0.2.93"],\
           ["openai-edge", "npm:1.2.2"],\
           ["p-map", "npm:7.0.2"],\
           ["p-queue", "npm:8.0.1"],\
@@ -7464,17 +7465,17 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["framer-motion", [\
-      ["npm:11.1.9", {\
-        "packageLocation": "./.yarn/global/cache/framer-motion-npm-11.1.9-dd53bf76d6-10.zip/node_modules/framer-motion/",\
+      ["npm:11.2.0", {\
+        "packageLocation": "./.yarn/global/cache/framer-motion-npm-11.2.0-7fa18cfcb7-10.zip/node_modules/framer-motion/",\
         "packageDependencies": [\
-          ["framer-motion", "npm:11.1.9"]\
+          ["framer-motion", "npm:11.2.0"]\
         ],\
         "linkType": "SOFT"\
       }],\
-      ["virtual:0cccbb210d7ec9866a375ee6aac5ae681292837121ca6cd216111fc5654785c02e2c1d848ea5f25a9bffd881268f2dc1275f829abd835491864f40a96950a34a#npm:11.1.9", {\
-        "packageLocation": "./.yarn/__virtual__/framer-motion-virtual-39dfe0f37f/0/global/cache/framer-motion-npm-11.1.9-dd53bf76d6-10.zip/node_modules/framer-motion/",\
+      ["virtual:0cccbb210d7ec9866a375ee6aac5ae681292837121ca6cd216111fc5654785c02e2c1d848ea5f25a9bffd881268f2dc1275f829abd835491864f40a96950a34a#npm:11.2.0", {\
+        "packageLocation": "./.yarn/__virtual__/framer-motion-virtual-818cc2f1bc/0/global/cache/framer-motion-npm-11.2.0-7fa18cfcb7-10.zip/node_modules/framer-motion/",\
         "packageDependencies": [\
-          ["framer-motion", "virtual:0cccbb210d7ec9866a375ee6aac5ae681292837121ca6cd216111fc5654785c02e2c1d848ea5f25a9bffd881268f2dc1275f829abd835491864f40a96950a34a#npm:11.1.9"],\
+          ["framer-motion", "virtual:0cccbb210d7ec9866a375ee6aac5ae681292837121ca6cd216111fc5654785c02e2c1d848ea5f25a9bffd881268f2dc1275f829abd835491864f40a96950a34a#npm:11.2.0"],\
           ["@emotion/is-prop-valid", null],\
           ["@types/emotion__is-prop-valid", null],\
           ["@types/react", "npm:18.3.2"],\
@@ -9075,6 +9076,15 @@ const RAW_RUNTIME_STATE =
         "linkType": "HARD"\
       }]\
     ]],\
+    ["isomorphic.js", [\
+      ["npm:0.2.5", {\
+        "packageLocation": "./.yarn/global/cache/isomorphic.js-npm-0.2.5-5d4ede8887-10.zip/node_modules/isomorphic.js/",\
+        "packageDependencies": [\
+          ["isomorphic.js", "npm:0.2.5"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
     ["isstream", [\
       ["npm:0.1.2", {\
         "packageLocation": "./.yarn/global/cache/isstream-npm-0.1.2-8581c75385-10.zip/node_modules/isstream/",\
@@ -9997,6 +10007,16 @@ const RAW_RUNTIME_STATE =
           ["levn", "npm:0.4.1"],\
           ["prelude-ls", "npm:1.2.1"],\
           ["type-check", "npm:0.4.0"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
+    ["lib0", [\
+      ["npm:0.2.93", {\
+        "packageLocation": "./.yarn/global/cache/lib0-npm-0.2.93-279e125304-10.zip/node_modules/lib0/",\
+        "packageDependencies": [\
+          ["lib0", "npm:0.2.93"],\
+          ["isomorphic.js", "npm:0.2.5"]\
         ],\
         "linkType": "HARD"\
       }]\
