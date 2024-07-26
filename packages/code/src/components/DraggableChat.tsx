26091e596594a551debcf4651a899896388f7b61
Zoltan Erdos <zoltan.erdos@me.com>
2024-07-26 15:39:37 +0100

    chore: Add i18next dependency and update package.json

--- packages/code/src/components/DraggableChat.tsx
+++ packages/code/src/components/DraggableChat.tsx
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 import { Resizable } from "re-resizable";
 import { FC, useState } from "react";
 import { IoClose } from "react-icons/io5";
-import ChatInterface from "../ChatInterface";
 
 interface DraggableChatProps {
   onClose: () => void;
