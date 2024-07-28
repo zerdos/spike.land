0505c31d63acbc033aee894644b8220cf3e802e1
Zoltan Erdos <zoltan.erdos@me.com>
2024-07-28 15:22:25 +0100

    Consolidated the import statements in the AutoSaveHistory.test.tsx file.

--- packages/code/src/components/AutoSaveHistory.test.tsx
+++ packages/code/src/components/AutoSaveHistory.test.tsx
@@ -1,5 +1,4 @@
-import { render } from '@testing-library/react';
-import { screen, fireEvent, waitFor } from '@testing-library/dom';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import AutoSaveHistory from './AutoSaveHistory';
 import * as monaco from 'monaco-editor';
