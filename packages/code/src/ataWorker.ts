a1dae102638f7481dae4569453a467a52291b01f
Zoltan Erdos <zoltan.erdos@me.com>
2024-08-13 20:24:57 +0100

    deploy

--- packages/code/src/ataWorker.ts
+++ packages/code/src/ataWorker.ts
@@ -1,294 +1,200 @@
 import { rpcFactory } from "./workerRpc";
-// import register from "./BroadcastLogger";
 import type { ata as Ata } from "./ata";
 import { applyCodePatch, createPatch, ICodeSession, makeHash, makeSession } from "./makeSess";
 import type { prettierCss as PrettierCSS, prettierJs as Prettier } from "./prettierEsm";
 import type { build as Build, transpile as Transpile } from "./transpile";
-
 import { Mutex } from "async-mutex";
-
 import { BufferedSocket, Socket, StableSocket } from "@github/stable-socket";
 
-// register();
-const policy = {
+declare var self: SharedWorkerGlobalScope & {
+  ata: typeof Ata;
+  prettierCss: typeof PrettierCSS;
+  prettierJs: typeof Prettier;
+  transpile: typeof Transpile;
+  build: typeof Build;
+  tsx: (code: string) => Promise<string[]>;
+};
+
+importScripts("/workerScripts/dts.js", "/workerScripts/ata.js", "/workerScripts/prettierEsm.js", "/workerScripts/transpile.js");
+
+const { ata, prettierJs, transpile, build, tsx, prettierCss } = self;
+
+const SOCKET_POLICY = {
   timeout: 4000,
   attempts: Infinity,
   maxDelay: 60000,
 };
 
-declare var self:
-  & SharedWorkerGlobalScope
-  & { ata: typeof Ata }
-  & { prettierCss: typeof PrettierCSS }
-  & {
-    prettierJs: typeof Prettier;
-  }
-  & { transpile: typeof Transpile }
-  & { build: typeof Build }
-  & { tsx: (code: string) => Promise<string[]> };
+const connections: Record<string, Connection> = {};
+const mutex = new Mutex();
 
-importScripts("/workerScripts/dts.js");
-importScripts("/workerScripts/ata.js");
-importScripts("/workerScripts/prettierEsm.js");
-importScripts("/workerScripts/transpile.js");
+interface Connection {
+  BC: BroadcastChannel;
+  ws: Socket;
+  controller: AbortController;
+  user: string;
+  oldSession: ICodeSession;
+}
 
-const { ata, prettierJs, transpile, build, tsx, prettierCss } = self;
-const start = (port: MessagePort) => {
-  // All your normal Worker and SharedWorker stuff can be placed here and should just work, with no extra setup required
-
+function start(port: MessagePort) {
   const rpcProvider = rpcFactory(port);
-
   port.onmessage = ({ data }) => rpcProvider.dispatch(data);
 
-  rpcProvider.registerRpcHandler(
-    "prettierJs",
-    ({ code, toThrow }: { code: string; toThrow: boolean }) => prettierJs(code, toThrow),
-  );
+  registerRpcHandlers(rpcProvider);
+}
 
-  rpcProvider.registerRpcHandler(
-    "prettierCss",
-    (code: string) => prettierCss(code),
-  );
+function registerRpcHandlers(rpcProvider: ReturnType<typeof rpcFactory>) {
+  rpcProvider.registerRpcHandler("prettierJs", ({ code, toThrow }: { code: string; toThrow: boolean }) => prettierJs(code, toThrow));
+  rpcProvider.registerRpcHandler("prettierCss", (code: string) => prettierCss(code));
+  rpcProvider.registerRpcHandler("ata", ({ code, originToUse }: { code: string; originToUse: string }) => ata({ code, originToUse, prettierJs, tsx }));
+  rpcProvider.registerRpcHandler("transpile", ({ code, originToUse }: { code: string; originToUse: string }) => transpile(code, originToUse));
+  rpcProvider.registerRpcHandler("build", (params: { codeSpace: string; splitting?: boolean; entryPoint?: string; origin: string; format: "esm" | "iife" }) => build(params));
+  rpcProvider.registerSignalHandler("connect", (signal: string) => setConnections(signal));
+}
 
-  rpcProvider.registerRpcHandler(
-    "ata",
-    ({ code, originToUse }: { code: string; originToUse: string }) => {
-      return ata({ code, originToUse, prettierJs, tsx });
-    },
-  );
+self.onconnect = (e) => start(e.ports[0]);
 
-  rpcProvider.registerRpcHandler(
-    "transpile",
-    ({ code, originToUse }: { code: string; originToUse: string }) => transpile(code, originToUse),
-  );
-
-  rpcProvider.registerRpcHandler(
-    "build",
-    (
-      { codeSpace, origin, format, splitting, entryPoint }: {
-        codeSpace: string;
-        splitting?: boolean;
-        entryPoint?: string;
-        origin: string;
-        format: "esm" | "iife";
-      },
-    ) => build({ codeSpace, origin, splitting, entryPoint, format }),
-  );
-
-  rpcProvider.registerSignalHandler(
-    "connect",
-    (signal: string) => setConnections(signal),
-  );
-};
-
-self.onconnect = (e) => {
-  let [port] = e.ports;
-  start(port);
-};
-
-// This is the fallback, just in case the browser doesn't support SharedWorkers natively
-self.onconnect = ({ ports }) => start(ports[0]);
-
-// If the script is running in a normal webworker thr don't worry about the Shared Worker message ports
 if (!("SharedWorkerGlobalScope" in self)) {
   start(self as typeof self & MessagePort);
 }
 
-const connections: {
-  [key: string]: {
-    BC: BroadcastChannel;
-    ws: Socket;
-    controller: AbortController;
-    user: string;
-    oldSession: ICodeSession;
-  };
-} = {};
+async function setConnections(signal: string) {
+  const [codeSpace, user] = signal.split(" ");
 
-const mutex = new Mutex();
-
-function setConnections(signal: string) {
-  const parts = signal.split(" ");
-
-  const codeSpace = parts[0];
-
-  const user = parts[1];
-
-  const c = connections[codeSpace] = connections[codeSpace] || {
+  const connection = connections[codeSpace] || {
     user,
     controller: new AbortController(),
     oldSession: makeSession({ i: 0, html: "", css: "", code: "" }),
   };
 
-  if (!c.ws) {
-    fetch(`/live/${codeSpace}/session`).then((s) => s.json().then((ss) => c.oldSession = makeSession(ss)));
+  if (!connection.ws) {
+    connection.oldSession = await fetchInitialSession(codeSpace);
+    connection.ws = createWebSocket(codeSpace, connection);
+    connection.BC = createBroadcastChannel(codeSpace, connection);
+  }
 
-    const delegate = {
-      socketDidOpen(_socket: Socket) {
-        // Socket is ready to write.
-      },
-      socketDidClose(_socket: Socket, _code?: number, _reason?: string) {
-        // Socket closed and will retry the connection.
-      },
-      socketDidFinish(_socket: Socket) {
-        // Socket closed for good and will not retry.
-      },
-      socketDidReceiveMessage(ws: Socket, message: string) {
-        const data = JSON.parse(message);
-        (async () => {
-          if (data.changes) {
-            BC.postMessage({ ...data, sender: "ATA WORKER1" });
-          }
-          if (data.strSess) {
-            const sess = makeSession(data.strSess);
-            const pp = createPatch(sess, c.oldSession);
-            ws.send(JSON.stringify({ ...pp, name: c.user, i: c.oldSession.i }));
-            return;
-          }
-          if (data.i && data.i < c.oldSession.i) return;
-          if (data.type === "handShake") {
-            ws.send(JSON.stringify({ name: c.user }));
+  connections[codeSpace] = connection;
+}
 
-            if (makeHash(c.oldSession) !== String(data.hashCode)) {
-              c.oldSession = await (await fetch(`/live/${codeSpace}/session`))
-                .json();
-              const transpiled = data.transpiled
-                || await transpile(c.oldSession.code, location.origin);
+async function fetchInitialSession(codeSpace: string): Promise<ICodeSession> {
+  const response = await fetch(`/live/${codeSpace}/session`);
+  return makeSession(await response.json());
+}
 
-              BC.postMessage({
-                ...c.oldSession,
-                transpiled,
-                sender: "ATA WORKER2",
-              });
-              return;
-            }
-            return;
-          }
-          // if (data.type === "transpile") {
-          // ws.send(JSON.stringify({ name: c.user }));
+function createWebSocket(codeSpace: string, connection: Connection): Socket {
+  const host = location.host === "localhost" ? "testing.spike.land" : location.host;
+  const url = `wss://${host}/live/${codeSpace}/websocket`;
+  const delegate = createSocketDelegate(connection, codeSpace);
+  const ws = new BufferedSocket(new StableSocket(url, delegate, SOCKET_POLICY));
+  ws.open();
+  return ws;
+}
 
-          // if (makeHash(c.oldSession) !== String(data.hashCode)) {
-          // c.oldSession = await (await fetch(`/live/${codeSpace}/session`)).json();
-          //  const transpiled = await transpile(c.oldSession.code);
-          //
-          // BC.postMessage({});
-          // return;
-          // }
-          // return;
-          // }
-          // if (data.type === "transpile") {
-          //   transpile(data.code).then(transpiled => ws.send(JSON.stringify({ ...data, transpiled })));
-          // }
+function createSocketDelegate(connection: Connection, codeSpace: string) {
+  return {
+    socketDidOpen: () => {},
+    socketDidClose: () => {},
+    socketDidFinish: () => {},
+    socketDidReceiveMessage: (ws: Socket, message: string) => handleSocketMessage(ws, message, connection, codeSpace),
+    socketShouldRetry: (_socket: Socket, code: number) => code !== 1008,
+  };
+}
 
-          // ^? a
-          if (data.newHash && data.oldHash) {
-            c.controller.abort();
-            const signal = c.controller.signal;
-            await mutex.runExclusive(async () => {
-              const oldSession = makeSession(c.oldSession);
-              const oldHash = makeHash(oldSession);
+function createBroadcastChannel(codeSpace: string, connection: Connection): BroadcastChannel {
+  const BC = new BroadcastChannel(`${location.origin}/live/${codeSpace}/`);
+  BC.onmessage = ({ data }) => handleBroadcastMessage(data, connection);
+  return BC;
+}
 
-              if (oldHash !== String(data.oldHash)) {
-                c.oldSession = makeSession(
-                  await (await fetch(`/live/${codeSpace}/session`)).json(),
-                );
+async function handleSocketMessage(ws: Socket, message: string, connection: Connection, codeSpace: string) {
+  const data = JSON.parse(message);
+  const { BC, oldSession, user } = connection;
 
-                console.log(c.oldSession);
-                if (signal.aborted) return;
-                const transpiled = await transpile(c.oldSession.code, location.origin);
-                if (signal.aborted) return;
+  if (data.changes) {
+    BC.postMessage({ ...data, sender: "ATA WORKER1" });
+  } else if (data.strSess) {
+    const sess = makeSession(data.strSess);
+    const pp = createPatch(sess, oldSession);
+    ws.send(JSON.stringify({ ...pp, name: user, i: oldSession.i }));
+  } else if (data.i && data.i < oldSession.i) {
+    return;
+  } else if (data.type === "handShake") {
+    await handleHandshake(ws, data, connection, codeSpace);
+  } else if (data.newHash && data.oldHash) {
+    await handleHashUpdate(data, connection, codeSpace);
+  }
+}
 
-                BC.postMessage({
-                  ...c.oldSession,
-                  transpiled,
-                  sender: "ATA WORKER3",
-                });
-                return;
-              }
+async function handleHandshake(ws: Socket, data: any, connection: Connection, codeSpace: string) {
+  ws.send(JSON.stringify({ name: connection.user }));
 
-              const newSession = applyCodePatch(oldSession, data);
-              const newHash = makeHash(newSession);
-              if (signal.aborted) return;
-              const transpiled = await transpile(newSession.code, location.origin);
-              if (signal.aborted) return;
-              if (data.newHash === newHash) {
-                c.oldSession = newSession;
-                console.log(newSession);
-                BC.postMessage({
-                  ...newSession,
-                  transpiled,
-                  sender: "ATA WORKER4",
-                });
-                return;
-              }
+  if (makeHash(connection.oldSession) !== String(data.hashCode)) {
+    connection.oldSession = await fetchInitialSession(codeSpace);
+    const transpiled = data.transpiled || await transpile(connection.oldSession.code, location.origin);
+    connection.BC.postMessage({ ...connection.oldSession, transpiled, sender: "ATA WORKER2" });
+  }
+}
 
-              if (signal.aborted) return;
+async function handleHashUpdate(data: any, connection: Connection, codeSpace: string) {
+  connection.controller.abort();
+  connection.controller = new AbortController();
+  const signal = connection.controller.signal;
+  if (signal.aborted) return;
 
-              c.oldSession = makeSession(
-                await (await fetch(`/live/${codeSpace}/session`)).json(),
-              );
-              if (signal.aborted) return;
+  await mutex.runExclusive(async () => {
+    const oldSession = makeSession(connection.oldSession);
+    const oldHash = makeHash(oldSession);
 
-              console.log(c.oldSession);
-              BC.postMessage({ ...c.oldSession, sender: "ATA WORKER5" });
-            });
-          }
-        })();
-      },
-      socketShouldRetry(_socket: Socket, code: number): boolean {
-        // Socket reconnects unless server returns the policy violation code.
-        return code !== 1008;
-      },
-    };
-    const host = location.host === "localhost" ? "testing.spike.land" : location.host;
-    const url = `wss://${host}/live/${codeSpace}/websocket`;
-    const ws = new BufferedSocket(new StableSocket(url, delegate, policy));
-    ws.open();
+    if (oldHash !== String(data.oldHash)) {
+      await handleHashMismatch(connection, codeSpace, signal);
+    } else {
+      await handleHashMatch(data, connection, oldSession, signal);
+    }
+  });
+}
 
-    c.ws = ws;
-    const BC = new BroadcastChannel(`${location.origin}/live/${codeSpace}/`);
-    c.BC = BC;
+async function handleHashMismatch(connection: Connection, codeSpace: string, signal: AbortSignal) {
+  if (signal.aborted) return;
+  connection.oldSession = await fetchInitialSession(codeSpace);
+  if (signal.aborted) return;
+  const transpiled = await transpile(connection.oldSession.code, location.origin);
+  if (signal.aborted) return;
+  connection.BC.postMessage({ ...connection.oldSession, transpiled, sender: "ATA WORKER3" });
+}
 
-    const mod = { controller: new AbortController(), i: 0 };
-    BC.onmessage = async ({ data }) => {
-      if (data.changes) {
-        ws.send(JSON.stringify({ ...data, name: c.user }));
+async function handleHashMatch(data: any, connection: Connection, oldSession: ICodeSession, signal: AbortSignal) {
+  const newSession = applyCodePatch(oldSession, data);
+  const newHash = makeHash(newSession);
+  if (signal.aborted) return;
+  const transpiled = await transpile(newSession.code, location.origin);
+  if (signal.aborted) return;
+  if (data.newHash === newHash) {
+    connection.oldSession = newSession;
+    connection.BC.postMessage({ ...newSession, transpiled, sender: "ATA WORKER4" });
+  } else {
+    if (signal.aborted) return;
+    connection.oldSession = await fetchInitialSession(codeSpace);
+    if (signal.aborted) return;
+    connection.BC.postMessage({ ...connection.oldSession, sender: "ATA WORKER5" });
+  }
+}
+
+async function handleBroadcastMessage(data: any, connection: Connection) {
+  if (data.changes) {
+    connection.ws.send(JSON.stringify({ ...data, name: connection.user }));
+  } else if (data.i > connection.oldSession.i && data.html && data.code) {
+    const oldSession = makeSession(connection.oldSession);
+    const newSession = makeSession(data);
+    const newHash = makeHash(newSession);
+    const oldHash = makeHash(oldSession);
+
+    if (newHash !== oldHash) {
+      const patchMessage = createPatch(oldSession, newSession);
+      if (patchMessage.oldHash === oldHash) {
+        connection.oldSession = newSession;
+        connection.ws.send(JSON.stringify({ ...patchMessage, i: newSession.i, name: connection.user }));
       }
-
-      if (data.i > c.oldSession.i && data.i > mod.i && data.html && data.code) {
-        mod.i = data.i;
-        mod.controller.abort();
-        mod.controller = new AbortController();
-        const signal = mod.controller.signal;
-        if (signal.aborted) return;
-
-        const oldSession = makeSession(c.oldSession);
-
-        const newSession = makeSession(data);
-        const newHash = makeHash(newSession);
-        const oldHash = makeHash(oldSession);
-
-        if (newHash !== oldHash) {
-          const patchMessage = createPatch(oldSession, newSession);
-          // const  transpiled = await transpile(c.oldSession.code);
-
-          // BC.postMessage({ ...patchMessage, name: c.user });
-          if (patchMessage.oldHash === oldHash) {
-            c.oldSession = newSession;
-            ws.send(
-              JSON.stringify({
-                ...patchMessage,
-                i: newSession.i,
-                name: c.user,
-              }),
-            );
-
-            // BC.postMessage(
-            //   { ...newSession, transpiled },
-            // );
-          }
-        }
-      }
-    };
+    }
   }
 }
 
