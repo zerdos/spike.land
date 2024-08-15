b996fc3d58e16dc2edf30362bca1d1137756524d
Zoltan Erdos <zoltan.erdos@me.com>
2024-08-15 18:09:27 +0100

    deploy

--- packages/code/src/reveal.tsx
+++ packages/code/src/reveal.tsx
@@ -1,8 +1,15 @@
 export const reveal = () => {
   const re = document.getElementById("root");
-  const rootEl = re!.lastElementChild as HTMLElement;
+  const rootEl = re!.lastElementChild as HTMLDivElement;
+  const firstEl = re!.lastElementChild as HTMLDivElement;
+
+  if (!rootEl) return;
+
   rootEl.style.height = "100%";
-  re?.removeChild(re.firstElementChild!);
+
+  if (firstEl !== rootEl) {
+    re?.removeChild(re.firstElementChild!);
+  }
 
   rootEl.style.opacity = "1";
 };
