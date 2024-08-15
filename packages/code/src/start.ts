e8e1bdadf87714f3823f5f6f635f7f45bf2fdad8
Zoltan Erdos <zoltan.erdos@virginmediao2.co.uk>
2024-08-15 12:53:23 +0100

    deploy

--- packages/code/src/start.ts
+++ packages/code/src/start.ts
@@ -1,13 +1,19 @@
 import { useCodeSpace } from "./hooks/useCodeSpace";
 
-if (location.pathname.endsWith("iframe") === false) import("./hydrate");
+if (location.pathname.endsWith("iframe") === false) {
+  setTimeout(() => {
+    import("./hydrate");
+  }, 0);
+}
 
 const codeSpace = useCodeSpace();
 
 if (location.pathname !== `/live/${codeSpace}` && location.pathname.endsWith("dehydrated") === false) {
-  (async () => {
-    const rootElement = document.getElementById("root")! as HTMLDivElement;
-    const { renderApp } = await import("./Wrapper");
-    renderApp({ codeSpace, rootElement });
-  })();
+  setTimeout(() => {
+    (async () => {
+      const rootElement = document.getElementById("root")! as HTMLDivElement;
+      const { renderApp } = await import("./Wrapper");
+      renderApp({ codeSpace, rootElement });
+    })();
+  }, 0);
 }
