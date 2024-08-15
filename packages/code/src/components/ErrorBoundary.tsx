6d029c4fae9444889a5f68b99145f65f98842e6c
Zoltan Erdos <zoltan.erdos@me.com>
2024-08-15 14:50:38 +0100

    deploy

--- packages/code/src/components/ErrorBoundary.tsx
+++ packages/code/src/components/ErrorBoundary.tsx
@@ -11,7 +11,7 @@ export class ErrorBoundary extends React.Component<{ children: React.ReactNode }
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error: any) {
+  static getDerivedStateFromError(_error: any) {
     // Update state so the next render will show the fallback UI.
 
     return { hasError: true };
