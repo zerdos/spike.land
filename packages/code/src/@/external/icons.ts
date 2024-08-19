bb2a8cf4dadfaf02fd8d7ae2fcf11328fc4e986f
Zoltan Erdos <zoltan.erdos@me.com>
2024-08-19 12:53:35 +0100

    deploy

--- packages/code/src/@/external/icons.ts
+++ packages/code/src/@/external/icons.ts
@@ -1,6 +1,5 @@
-import { FaDownload } from "react-icons/fa";
+import { FaDownload } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
-import { MdFullscreen } from "react-icons/md";
-import { MdPhoneAndroid, MdQrCode, MdShare, MdTabletAndroid, MdTv } from "react-icons/md";
+import { MdFullscreen, MdPhoneAndroid, MdQrCode, MdShare, MdTabletAndroid, MdTv } from "react-icons/md";
 
 export { FaDownload, IoClose, MdFullscreen, MdPhoneAndroid, MdQrCode, MdShare, MdTabletAndroid, MdTv };
