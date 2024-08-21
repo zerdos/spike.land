2c7d72f98255485116175dd2eab5b23f5660e6d0
Zoltan Erdos <zoltan.erdos@me.com>
2024-08-21 22:47:20 +0100

    deploy

--- packages/code/src/reactDom.ts
+++ packages/code/src/reactDom.ts
@@ -1,15 +1,16 @@
 export * as ReactDOM from "react-dom";
-export { default } from "react-dom";
-// export const {
-//   createPortal,
-//   findDOMNode,
-//   flushSync,
-//   hydrate,
-//   render,
-//   unmountComponentAtNode,
-//   unstable_batchedUpdates,
-//   unstable_renderSubtreeIntoContainer,
-//   version,
-// } = ReactDOM;
+import reactDom from "react-dom";
+export default reactDom;
+
+export const createPortal: typeof reactDom.createPortal = ReactDOM.createPortal;
+export const findDOMNode: typeof reactDom.findDOMNode = ReactDOM.findDOMNode;
+export const flushSync: typeof reactDom.flushSync = ReactDOM.flushSync;
+export const hydrate: reactDom.Renderer = ReactDOM.hydrate;
+export const render: reactDom.Renderer = ReactDOM.render;
+export const unmountComponentAtNode: typeof reactDom.unmountComponentAtNode = ReactDOM.unmountComponentAtNode;
+export const unstable_batchedUpdates: typeof reactDom.unstable_batchedUpdates = ReactDOM.unstable_batchedUpdates;
+export const unstable_renderSubtreeIntoContainer: typeof reactDom.unstable_renderSubtreeIntoContainer =
+  ReactDOM.unstable_renderSubtreeIntoContainer;
+export const version: string = ReactDOM.version;
 
 // export default ReactDOM;
