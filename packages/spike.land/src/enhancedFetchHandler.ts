39f6c20ee75b03eb0b3338200bac2e4ec0a21be7
Zoltan Erdos <zoltan.erdos@me.com>
2024-08-22 09:36:02 +0100

    deploy

--- packages/spike.land/src/enhancedFetchHandler.ts
+++ packages/spike.land/src/enhancedFetchHandler.ts
@@ -3,7 +3,7 @@ export const handleEnhancedFetch = async (request: Request) => {
     const optionsParam = await request.json() as RequestInit & { url: string };
 
     // Perform the fetch
-    const response = await fetch(optionsParam.url, optionsParam);
+    return fetch(optionsParam.url, optionsParam);
 
     // if (response.body instanceof ReadableStream) {
     //   // convert it to arrayBuffer
@@ -49,24 +49,24 @@ export const handleEnhancedFetch = async (request: Request) => {
     // }
 
     // Create a TransformStream to modify headers
-    const { readable, writable } = new TransformStream();
+    // const { readable, writable } = new TransformStream();
 
-    // Start pumping the response body into the TransformStream
-    response.body?.pipeTo(writable);
+    // // Start pumping the response body into the TransformStream
+    // response.body?.pipeTo(writable);
 
-    // Create a new response with the TransformStream as the body
-    const newResponse = new Response(readable, {
-      status: response.status,
-      statusText: response.statusText,
-      headers: response.headers,
-    });
+    // // Create a new response with the TransformStream as the body
+    // const newResponse = new Response(readable, {
+    //   status: response.status,
+    //   statusText: response.statusText,
+    //   headers: response.headers,
+    // });
 
-    // Add CORS headers if needed
-    newResponse.headers.set("Access-Control-Allow-Origin", "*");
-    newResponse.headers.set("Access-Control-Allow-Methods", "*");
-    newResponse.headers.set("Access-Control-Allow-Headers", "*");
+    // // Add CORS headers if needed
+    // newResponse.headers.set("Access-Control-Allow-Origin", "*");
+    // newResponse.headers.set("Access-Control-Allow-Methods", "*");
+    // newResponse.headers.set("Access-Control-Allow-Headers", "*");
 
-    return newResponse;
+    // return newResponse;
   } catch (error) {
     console.error("Server-side fetch error:", error);
     return new Response("Server-side fetch failed", { status: 500 });
