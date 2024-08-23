5c6a2cd888421f3952ebfb78a4727521cd658405
Zoltan Erdos <zoltan.erdos@me.com>
2024-08-23 23:12:08 +0100

    deploy

--- packages/code/src/hooks/useAutoSave.ts
+++ packages/code/src/hooks/useAutoSave.ts
@@ -1,1 +1,1 @@
-export const useAutoSave = async (codeSpace: string) => fetch(`/live/${codeSpace}/autosave`);
+export const useAutoSave = async (codeSpace: string) => fetch(`/live/${codeSpace}/auto-save`);
