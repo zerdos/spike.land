e8748c5c5baa633c0f4efcd1867a27b782560c6f
Zoltan Erdos <zoltan.erdos@me.com>
2024-08-26 16:03:05 +0100

    deploy

--- packages/code/src/replacePreservingWhitespace.tsx
+++ packages/code/src/replacePreservingWhitespace.tsx
@@ -6,6 +6,26 @@ export function replacePreservingWhitespace(
   search: string,
   replace: string,
 ) {
+  if (replace.startsWith("//")) {
+    const firstLine = replace.split("\n")[0];
+    if (firstLine.includes(".tsx")) {
+      const codeSpace = firstLine.split(".tsx")[0].split(" ")[1];
+      const session = {
+        code: replace,
+        transpiled: "",
+        html: "",
+        css: "",
+      };
+      fetch(`/live/${codeSpace}/session`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(session),
+      });
+      replace = "";
+    }
+  }
   const escapedSearch = search.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
   const regex = new RegExp(
     `(\\s*)(${escapedSearch.replace(/\s+/g, "\\s+")})([\\s]*)`,
