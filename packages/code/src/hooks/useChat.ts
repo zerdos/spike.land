e075b50cef708a36ee5c399e4cf6fb4bb2fb0070
z <z@zs-macbook-pro.lan>
2024-09-01 17:55:28 +0100

    deploy

--- packages/code/src/hooks/useChat.ts
+++ packages/code/src/hooks/useChat.ts
@@ -5,7 +5,7 @@ import { useSyncedStorage } from "./useSyncedStorage";
 export const useChat = (
   codeSpace: string,
 ) => {
-  const [messages, setMessages] = useSyncedStorage(`chatMessages-${codeSpace}`, [] as Message[]);
+  const [messagesRaw, setMessages] = useSyncedStorage(`chatMessages-${codeSpace}`, [] as Message[]);
   const [isStreaming, setIsStreaming] = useSyncedStorage(`streaming-${codeSpace}`, false);
 
   const [input, setInput] = useState("");
@@ -14,6 +14,23 @@ export const useChat = (
   const [editInput, setEditInput] = useState("");
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLTextAreaElement>(null);
+
+  // if the role of the prev message is the same as the current message, then the current message will be displayed in the same bubble as the previous message, so we merge them in the array them in
+  const messages = messagesRaw.reduce((acc, message, index) => {
+    if (index === 0) {
+      return [message];
+    }
+    const prevMessage = acc[acc.length - 1];
+    if (prevMessage.role === message.role) {
+      acc[acc.length - 1] = {
+        ...prevMessage,
+        content: `${prevMessage.content}\n${message.content}`,
+      };
+    } else {
+      acc.push(message);
+    }
+    return acc;
+  }, [] as Message[]).slice(-5);
 
   const resetChat = useCallback(() => {
     setMessages([]);
