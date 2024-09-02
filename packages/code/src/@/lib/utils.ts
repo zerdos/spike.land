8979b05f85398d782481d0e6a9dac5777c324039
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-03 00:49:33 +0100

    deploy

--- packages/code/src/@/lib/utils.ts
+++ packages/code/src/@/lib/utils.ts
@@ -1,1 +1,6 @@
-export { cn } from "../../jsx";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
