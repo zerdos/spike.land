924345ffa53e90d0231d151e39b07bbddc43767a
Zoltan Erdos <zoltan.erdos@virginmediao2.co.uk>
2024-09-05 20:13:40 +0100

    deploy

--- packages/code/src/@/lib/md5.ts
+++ packages/code/src/@/lib/md5.ts
@@ -1,16 +1,10 @@
 import { Md5 } from "ts-md5";
 
-export const md5 = (input: string | object): string => {
-  let key = generateDeterministicKey(JSON.stringify(input));
-
-  while (Number(key[0]).toString() === key[0]) key = key.slice(1);
-
-  return key.slice(0, 8);
-};
+export const md5 = (input: string): string => generateDeterministicKey(input).slice(0, 8);
 
 const generateDeterministicKey = (input: string): string => {
   const hash = Md5.hashStr(input).toString();
-  const validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let result = "";
   for (let i = 0; i < hash.length; i++) {
     const charCode = hash.charCodeAt(i);
