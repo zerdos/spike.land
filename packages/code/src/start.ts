8ebb79cd4724a072dad95d8dfa9acc3a20ec1c76
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-05 08:52:50 +0100

    deploy

--- packages/code/src/start.ts
+++ packages/code/src/start.ts
@@ -1,19 +1,20 @@
-import { renderApp } from "@/components/app/wrapper";
-import { useCodeSpace } from "./hooks/useCodeSpace";
+import "./ws";
+// import { renderApp } from "@/components/app/wrapper";
+// import { useCodeSpace } from "./hooks/useCodeSpace";
 
-if (location.pathname.endsWith("embed") === false) {
-  setTimeout(() => {
-    import("./ws");
-  }, 0);
-}
+// if (location.pathname.endsWith("embed") === false) {
+// setTimeout(() => {
+// import("./ws");
+// }, 0);
+// }
 
-const codeSpace = useCodeSpace();
+// const codeSpace = useCodeSpace();
 
-if (
-  location.pathname !== `/live/${codeSpace}`
-  && location.pathname.endsWith("dehydrated") === false
-) {
-  const rootElement = (document.getElementById("root") || document.getElementById("embed")) as HTMLDivElement;
+// if (
+//   location.pathname !== `/live/${codeSpace}`
+//   && location.pathname.endsWith("dehydrated") === false
+// ) {
+//   const rootElement = (document.getElementById("root") || document.getElementById("embed")) as HTMLDivElement;
 
-  renderApp({ codeSpace, rootElement });
-}
+//   renderApp({ codeSpace, rootElement });
+// }
