3e19ab05748ff8f3ca9e1c313fa0c95a175196ef
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-09 06:14:56 +0100

    deploy

--- packages/code/src/sw.ts
+++ packages/code/src/sw.ts
@@ -7,7 +7,7 @@ sw.__WB_DISABLE_DEV_LOGS = true;
 
 import { CacheableResponsePlugin } from "workbox-cacheable-response";
 import { registerRoute } from "workbox-routing";
-import { CacheFirst, StaleWhileRevalidate } from "workbox-strategies";
+import { CacheFirst } from "workbox-strategies";
 
 importScripts("/swVersion.js");
 
@@ -108,7 +108,7 @@ registerRoute(
   cacheFirstStrategy,
 );
 
-const esmCacheStrategy = new StaleWhileRevalidate({
+const esmCacheStrategy = new CacheFirst({
   cacheName: ESM_CACHE_NAME,
   plugins: [
     new CacheableResponsePlugin({
