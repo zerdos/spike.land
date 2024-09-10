cae66bbbb0880ed3d06c3f5f22b3935d32189bb0
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-10 02:57:44 +0100

    deploy

--- packages/spike.land/src/chat.ts
+++ packages/spike.land/src/chat.ts
@@ -133,12 +133,12 @@ export default {
     const cacheKey = new Request(new URL(files[mightAsset] || mightAsset, url.origin).toString(), request.clone());
     const cachedResponse = await cache.match(cacheKey);
     if (cachedResponse) {
-      return cachedResponse;
+      return makeResponse(cachedResponse as any, mightAsset);
     }
    
     const resp =await handleMainFetch(request, env, ctx);
     if (resp && resp.status === 200 && resp.headers.get('cache-control')?.includes('public')) {
-      ctx.waitUntil(cache.put(cacheKey,  new Response(resp.clone().body, resp)));
+      ctx.waitUntil(cache.put(cacheKey,  new Response(resp.clone().body, resp)))
     }
 
     return resp;
@@ -166,70 +166,77 @@ export async function handleCMSIndexRequest(request: Request, env: Env) {
         // 404
         return new Response("File not found", { status: 404 });
       }
-      const headers = new Headers();
-      object.writeHttpMetadata(headers);
-      headers.set("etag", object.httpEtag);
-      headers.set("Cache-Control", "public, max-age=31536000");
-      headers.set("Access-Control-Allow-Origin", "*");
-      headers.set("Cross-Origin-Embedder-Policy", "require-corp");
-      headers.set(
-        "Content-Type",
-        key.endsWith("js")
-          ? "application/javascript; charset=UTF-8"
-          : key.endsWith("css")
-          ? "text/css; charset=UTF-8"
-          : key.endsWith("html")
-          ? "text/html; charset=UTF-8"
-          : key.endsWith("json")
-          ? "application/json; charset=UTF-8"
-          : key.endsWith("ttf")
-          ? "font/ttf"
-          : key.endsWith("woff")
-          ? "font/woff"
-          : key.endsWith("woff2")
-          ? "font/woff2"
-          : key.endsWith("eot")
-          ? "font/eot"
-          : key.endsWith("otf")
-          ? "font/otf"
-          : key.endsWith("png")
-          ? "image/png"
-          : key.endsWith("pdf")
-          ? "application/pdf"
-          : key.endsWith("gif")
-          ? "image/gif"
-          : key.endsWith("webp")
-          ? "image/webp"
-          : key.endsWith("jpg")
-          ? "image/jpeg"
-          : key.endsWith("jpeg")
-          ? "image/jpeg"
-          : key.endsWith("svg")
-          ? "image/svg+xml"
-          : key.endsWith("mp4")
-          ? "video/mp4"
-          : key.endsWith("webm")
-          ? "video/webm"
-          : key.endsWith("mov")
-          ? "video/quicktime"
-          : key.endsWith("avi")
-          ? "video/x-msvideo"
-          : key.endsWith("wmv")
-          ? "video/x-ms-wmv"
-          : key.endsWith("mp3")
-          ? "audio/mpeg"
-          : key.endsWith("wav")
-          ? "audio/wav"
-          : key.endsWith("ogg")
-          ? "audio/ogg"
-          : key.endsWith("flac")
-          ? "audio/flac"
-          : key.endsWith("aac")
-          ? "audio/aac"
-          : "text/html; charset=UTF-8",
-      );
-      return new Response(object.body, { headers });
+      return makeResponse(object, key); 
     default:
       return new Response("Method not allowed", { status: 405 });
   }
+}
+
+
+function makeResponse(object: R2ObjectBody, key: string) {
+
+  const headers = new Headers();
+  if (object.writeHttpMetadata) object.writeHttpMetadata(headers);
+
+  headers.set("etag", object.httpEtag);
+  headers.set("Cache-Control", "public, max-age=31536000");
+  headers.set("Access-Control-Allow-Origin", "*");
+  headers.set("Cross-Origin-Embedder-Policy", "require-corp");
+  headers.set(
+    "Content-Type",
+    key.endsWith("js")
+      ? "application/javascript; charset=UTF-8"
+      : key.endsWith("css")
+      ? "text/css; charset=UTF-8"
+      : key.endsWith("html")
+      ? "text/html; charset=UTF-8"
+      : key.endsWith("json")
+      ? "application/json; charset=UTF-8"
+      : key.endsWith("ttf")
+      ? "font/ttf"
+      : key.endsWith("woff")
+      ? "font/woff"
+      : key.endsWith("woff2")
+      ? "font/woff2"
+      : key.endsWith("eot")
+      ? "font/eot"
+      : key.endsWith("otf")
+      ? "font/otf"
+      : key.endsWith("png")
+      ? "image/png"
+      : key.endsWith("pdf")
+      ? "application/pdf"
+      : key.endsWith("gif")
+      ? "image/gif"
+      : key.endsWith("webp")
+      ? "image/webp"
+      : key.endsWith("jpg")
+      ? "image/jpeg"
+      : key.endsWith("jpeg")
+      ? "image/jpeg"
+      : key.endsWith("svg")
+      ? "image/svg+xml"
+      : key.endsWith("mp4")
+      ? "video/mp4"
+      : key.endsWith("webm")
+      ? "video/webm"
+      : key.endsWith("mov")
+      ? "video/quicktime"
+      : key.endsWith("avi")
+      ? "video/x-msvideo"
+      : key.endsWith("wmv")
+      ? "video/x-ms-wmv"
+      : key.endsWith("mp3")
+      ? "audio/mpeg"
+      : key.endsWith("wav")
+      ? "audio/wav"
+      : key.endsWith("ogg")
+      ? "audio/ogg"
+      : key.endsWith("flac")
+      ? "audio/flac"
+      : key.endsWith("aac")
+      ? "audio/aac"
+      : "text/html; charset=UTF-8",
+  );
+  return new Response(object.body, { headers });
 }
