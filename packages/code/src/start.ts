1018cb0d681dd7fb382868deb8aaf3c91e2a77f7
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-14 22:53:11 +0100

    deploy

--- packages/code/src/start.ts
+++ packages/code/src/start.ts
@@ -1,6 +1,6 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-Object.assign(globalThis, { require });
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+// Object.assign(globalThis, { require });
 
 import { useCodeSpace } from "@/hooks/use-code-space";
 import { renderApp } from "@/lib/render-app";
