e0918aa502ad12e511171a6dc587c6bb902800ec
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-14 23:54:44 +0100

    deploy

--- packages/code/src/@/lib/importmap-utils.ts
+++ packages/code/src/@/lib/importmap-utils.ts
@@ -1,7 +1,6 @@
 // src/importMapUtils.ts
 
 export const oo = {
-  "@/": "/@/",
   "@emotion/react/jsx-runtime": "/emotionJsxRuntime.mjs",
   "react/jsx-runtime": "/jsx.mjs",
   "react-dom/server": "/reactDomServer.mjs",
@@ -100,7 +99,7 @@ export function importMapReplace(code: string, origin: string): string {
     }
 
     if (packageName?.startsWith("@/")) {
-      return p1 + `"${packageName}.mjs"` + p3;
+      return p1 + `"/${packageName}.mjs"` + p3;
     }
 
     if (packageName.includes(".")) {
