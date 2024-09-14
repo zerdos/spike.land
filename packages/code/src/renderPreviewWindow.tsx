af2f44ad53d4c4c67b10c1c5e1117c1fc9d6562e
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-14 10:34:12 +0100

    deploy

--- packages/code/src/renderPreviewWindow.tsx
+++ packages/code/src/renderPreviewWindow.tsx
@@ -1,25 +1,17 @@
 import { ClerkProvider } from "@clerk/clerk-react";
 import { renderApp } from "@/lib/render-app";
 import type { ICode } from "@/lib/interfaces";
-import { Suspense, lazy } from "react";
-
+import { AppToRender } from "./AppToRender";
 
 export const renderPreviewWindow = async (
   { codeSpace, cSess }: { codeSpace: string; cSess: ICode },
 ) => {
 
 
-  const LazyAppToRender = lazy(() => import("./AppToRender").then((module) => ({ default: module.AppToRender })));
 
-
-
-  
 const App = ()=> 
 <ClerkProvider publishableKey="pk_live_Y2xlcmsuc3Bpa2UubGFuZCQ" afterSignOutUrl="/">
-<Suspense fallback={
- <iframe src={`/live/${codeSpace}/iframe`} style={{width: "100%", height: "100dvh"}}></iframe> 
-}><LazyAppToRender codeSpace={codeSpace} cSess={cSess} />
-</Suspense>
+<AppToRender codeSpace={codeSpace} cSess={cSess} />
 </ClerkProvider>
 
   return renderApp({ App });
