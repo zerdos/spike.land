8e7851048d7c1a3b3ccaa53f191d7b81e4be7cd5
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-19 03:01:38 +0100

    deploy

--- packages/code/src/@/lib/importmap-utils.ts
+++ packages/code/src/@/lib/importmap-utils.ts
@@ -73,10 +73,10 @@ export function importMapReplace(code: string, origin: string): string {
       packageName?.startsWith(`/live`)
       && !packageName.includes("index.js")
     ) {
-      return p1 + `"${packageName}/index.js/${Date.now()}"` + p3;
+      return p1 + `"${packageName}/index.js"` + p3;
     }
     if (packageName?.startsWith("./") && !packageName.slice(1).includes(".")) {
-      return p1 + `"/live/${packageName.slice(2)}/index.js/${Date.now()}"` + p3;
+      return p1 + `"/live/${packageName.slice(2)}/index.js"` + p3;
     }
 
     if (packageName?.startsWith("/")) {
@@ -96,7 +96,7 @@ export function importMapReplace(code: string, origin: string): string {
     }
 
     if (packageName?.startsWith("/live")) {
-      return p1 + `"${packageName}/index.js/${Date.now()}"` + p3;
+      return p1 + `"${packageName}/index.js"` + p3;
     }
 
     if (packageName?.startsWith("@/")) {
