b6d635c8280df60179bfccdd981487b72b48267d
Zoltan Erdos <zoltan.erdos@virginmediao2.co.uk>
2024-09-20 12:24:59 +0100

    deploy

--- packages/code/src/@/external/monaco-editor.ts
+++ packages/code/src/@/external/monaco-editor.ts
@@ -1,25 +1,36 @@
 export { editor, languages, Uri } from "monaco-editor";
 
-const workerPool: Record<string, Worker> = {};
-
 const baseUrl = document.head.baseURI;
+const workerPool: Record<string, null | Worker> = {};
+const getWorker = (_workerId: string, label: string, justRefill = false) => {
+  if (label === "json") {
+    workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/json.worker.js`);
+  }
+  if (label === "css") {
+    workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/css.worker.js`);
+  }
+  if (label === "html") {
+    workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/html.worker.js`);
+  }
+  if (label === "typescript" || label === "javascript") {
+    workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/ts.worker.js`);
+  }
+  workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/editor.worker.js`);
+
+  if (justRefill) {
+    return workerPool[label];
+  }
+
+  const returnWorker = workerPool[label];
+  workerPool[label] = null;
+  workerPool[label] = getWorker(label, label, true);
+  // ref
+
+  return returnWorker;
+};
+
 window.MonacoEnvironment = {
   baseUrl,
 
-  getWorker: (_workerId: string, label: string) => {
-    if (label === "json") {
-      workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/json.worker.js`);
-    }
-    if (label === "css") {
-      workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/css.worker.js`);
-    }
-    if (label === "html") {
-      workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/html.worker.js`);
-    }
-    if (label === "typescript" || label === "javascript") {
-      workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/ts.worker.js`);
-    }
-    workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/editor.worker.js`);
-    return workerPool[label];
-  },
+  getWorker,
 };
