73c33143dba811b32ffdc07b388d232059f18c99
Zoltan Erdos <zoltan.erdos@virginmediao2.co.uk>
2024-09-20 12:07:59 +0100

    deploy

--- packages/code/src/@/external/monaco-editor.ts
+++ packages/code/src/@/external/monaco-editor.ts
@@ -2,23 +2,24 @@ export { editor, languages, Uri } from "monaco-editor";
 
 const workerPool: Record<string, Worker> = {};
 
+const baseUrl = document.head.baseURI;
 window.MonacoEnvironment = {
-  baseUrl: location.origin,
+  baseUrl,
 
   getWorker: (_workerId: string, label: string) => {
     if (label === "json") {
-      workerPool[label] = workerPool[label] || new Worker(`/@/monaco-editor/esm/json.worker.js`);
+      workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/json.worker.js`);
     }
     if (label === "css") {
-      workerPool[label] = workerPool[label] || new Worker(`/@/monaco-editor/esm/css.worker.js`);
+      workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/css.worker.js`);
     }
     if (label === "html") {
-      workerPool[label] = workerPool[label] || new Worker(`/@/monaco-editor/esm/html.worker.js`);
+      workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/html.worker.js`);
     }
     if (label === "typescript" || label === "javascript") {
-      workerPool[label] = workerPool[label] || new Worker(`/@/monaco-editor/esm/ts.worker.js`);
+      workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/ts.worker.js`);
     }
-    workerPool[label] = workerPool[label] || new Worker(`/@/monaco-editor/esm/editor.worker.js`);
+    workerPool[label] = workerPool[label] || new Worker(baseUrl + `@/monaco-editor/esm/editor.worker.js`);
     return workerPool[label];
   },
 };
