3e3b1a359cf1834821caba5233510a0b8feff0be
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-23 00:06:39 +0100

    save

--- packages/code/src/@/lib/esbuild-build-tasks.ts
+++ packages/code/src/@/lib/esbuild-build-tasks.ts
@@ -2,10 +2,10 @@ import { getCommonBuildOptions } from "@/lib/esbuild-build-config";
 import { environment } from "@/lib/esbuild-make-env";
 import { build } from "@/lib/esbuild-operations";
 import { copy } from "esbuild-plugin-copy";
-import { readdir, readFile, stat, writeFile } from "fs/promises";
+import { readdir } from "fs/promises";
 export type Environment = "development" | "production";
-import { importMapReplace } from "@/lib/importmap-utils";
-import path from "path";
+import { importMap } from "@/lib/importmap-utils";
+// import path from "path";
 
 const isProduction = environment === "production";
 
@@ -216,7 +216,17 @@ export async function buildMainBundle(wasmFile: string): Promise<void> {
     ignoreAnnotations: false,
     legalComments: "none",
     target: "es2024",
-    external: undefined,
+    external: [
+      ...Object.keys(importMap.imports),
+      "worker_threads",
+      "fs",
+      "path",
+      "os",
+      "tty",
+      "crypto",
+      "child_process",
+      "./fsevents.node",
+    ],
     alias: undefined,
     outdir: "dist/@/",
     platform: "browser",
@@ -292,30 +302,30 @@ export async function buildMainBundle(wasmFile: string): Promise<void> {
     ],
   });
 
-  async function runImportMapReplaceOnAllFilesRecursive(dir: string): Promise<void> {
-    try {
-      const files = await readdir(dir);
+  // async function runImportMapReplaceOnAllFilesRecursive(dir: string): Promise<void> {
+  //   try {
+  //     const files = await readdir(dir);
 
-      for (const file of files) {
-        const filePath = path.join(dir, file);
-        const fileStat = await stat(filePath);
+  //     for (const file of files) {
+  //       const filePath = path.join(dir, file);
+  //       const fileStat = await stat(filePath);
 
-        if (fileStat.isDirectory()) {
-          // If it's a directory, recursively process its contents
+  //       if (fileStat.isDirectory()) {
+  //         // If it's a directory, recursively process its contents
 
-          await runImportMapReplaceOnAllFilesRecursive(filePath);
-        } else {
-          // If it's a file, process it
-          if (filePath.includes("worker")) continue;
-          const content = await readFile(filePath, "utf8");
-          const newContent = importMapReplace(content, "");
-          await writeFile(filePath, newContent);
-        }
-      }
-    } catch (error) {
-      console.error(`Error processing directory ${dir}:`, error);
-    }
-  }
+  //         await runImportMapReplaceOnAllFilesRecursive(filePath);
+  //       } else {
+  //         // If it's a file, process it
+  //         if (filePath.includes("worker")) continue;
+  //         const content = await readFile(filePath, "utf8");
+  //         const newContent = importMapReplace(content, "");
+  //         await writeFile(filePath, newContent);
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.error(`Error processing directory ${dir}:`, error);
+  //   }
+  // }
 
-  runImportMapReplaceOnAllFilesRecursive("./dist/@");
+  // runImportMapReplaceOnAllFilesRecursive("./dist/@");
 }
