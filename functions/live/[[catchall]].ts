df99a11a6c448312249673e9aefddfd3aa1a915a
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-22 15:32:47 +0100

    fix

--- functions/live/[[catchall]].ts
+++ functions/live/[[catchall]].ts
@@ -10,6 +10,15 @@ export const onRequest: PagesFunction<Env> = async (ctx) => {
 
     const path = allParams.join('/');
     const url = new URL("/"+path,ctx.request.url).toString( );
+
+    const ctxFor= {
+        ...ctx,
+        request: {
+            ...ctx.request,
+            url: url
+        }
+       
+    } as typeof ctx;
     
-    return ctx.env.CODESPACE.fetch!(new Request(url), ctx.env.CODESPACE, ctx);
+    return ctx.env.CODESPACE.fetch!(ctxFor.request, ctx.env.CODESPACE, ctx);
 }   
