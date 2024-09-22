b2298df49c2bf8b547bf47111deef86a9aab2b30
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-22 13:23:44 +0100

    up

--- functions/live/[codeSpace].ts
+++ functions/live/[codeSpace].ts
@@ -1,14 +1,14 @@
 import { DurableObjectNamespace, PagesFunction } from '@cloudflare/workers-types';
 
 interface Env {
-    CODE_SPACE: DurableObjectNamespace;
+    CODESPACE: DurableObjectNamespace;
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
     const codeSpace =  context.params.codeSpace as string;
 
-    const id = context.env.CODE_SPACE.idFromString(codeSpace);
-    const stub = context.env.CODE_SPACE.get(id);
+    const id = context.env.CODESPACE.idFromString(codeSpace);
+    const stub = context.env.CODESPACE.get(id);
 
     
     return stub.fetch(context.request);
