e4bfa66700bd85befa28699a4b5f8b9711e0f68d
Zoltan Erdos <zolika84@gmail.com>
2024-09-22 05:06:30 +0100

    deploy

--- packages/code/src/hooks/useArchive.ts
+++ packages/code/src/hooks/useArchive.ts
@@ -29,7 +29,7 @@ export const useSpeedy2 = async () => {
     splitting: true,
 
     origin: location.origin,
-    format: "esm",
+    format: "iife",
   }) as unknown as { text: string; path: string; contents: ArrayBuffer }[];
 
   // res.map(async (f) => {
