4ff62b7ee5b743c3645ba74a024bb0dae6f402d1
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-29 01:25:32 +0100

    push

--- packages/code/src/@/workers/generate-css.worker.ts
+++ packages/code/src/@/workers/generate-css.worker.ts
@@ -1,6 +1,6 @@
 import autoprefixer from "autoprefixer";
 import postcss from "postcss";
-import postcssJs, { CssInJs } from "postcss-js";
+
 import tailwindcss from "tailwindcss";
 
 async function generateCSS(classNames: string[]) {
@@ -9,11 +9,9 @@ async function generateCSS(classNames: string[]) {
   const result = await postcss([
     tailwindcss(),
     autoprefixer(),
-  ]).process(cssString as unknown as CssInJs, {
-    parser: postcssJs.parse,
-  });
+  ]);
 
-  return result.css;
+  return result.process(cssString).css;
 }
 
 Object.assign(globalThis, { generateCSS });
