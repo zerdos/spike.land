6ea10af43e99e7d734fac9e35268d76be021968f
Zoltan Erdos <zoltan.erdos@me.com>
2024-09-29 04:01:32 +0100

    push

--- packages/code/tailwind.config.ts
+++ packages/code/tailwind.config.ts
@@ -12,34 +12,23 @@ export default {
 				ring: 'hsl(var(--ring))',
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
-				primary: {
-					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))'
-				},
-				secondary: {
-					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))'
-				},
-				destructive: {
-					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))'
-				},
-				muted: {
-					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))'
-				},
-				accent: {
-					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))'
-				},
-				popover: {
-					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))'
-				},
-				card: {
-					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))'
-				}
+				primary: 'hsl(var(--primary))',
+				'primary-foreground': 'hsl(var(--primary-foreground))',
+				
+				secondary: 'hsl(var(--secondary))',
+				'secondary-foreground': 'hsl(var(--secondary-foreground))',
+				
+				destructive: 'hsl(var(--destructive))',
+				"destructive-foreground": 'hsl(var(--destructive-foreground))',
+			
+				muted: 'hsl(var(--muted))',
+				"muted-foreground": 'hsl(var(--muted-foreground))',
+				accent:  'hsl(var(--accent))',
+				"accent-foreground": 'hsl(var(--accent-foreground))',
+				popover:  'hsl(var(--popover))',
+				"popover-foreground": 'hsl(var(--popover-foreground))',
+					card: 'hsl(var(--card))',
+				"card-foreground": 'hsl(var(--card-foreground))',
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
