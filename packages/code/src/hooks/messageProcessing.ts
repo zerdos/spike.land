3143f14bff2e245497bb76380dd6b23b417662c5
Zoltan Erdos <zolika84@gmail.com>
2024-10-03 17:26:02 +0100

    push

--- packages/code/src/hooks/messageProcessing.ts
+++ packages/code/src/hooks/messageProcessing.ts
@@ -205,34 +205,34 @@ function createOnUpdateFunction({
               if (lastCode === mod.lastCode) {
                 if (len === 0) {
                   finished = true;
-                  mod.actions.push({
-                    TRIED: TRIED + 1,
-                    SKIP: SKIP + 1,
-                    DIFFs,
-                    chars: instructions.length,
-                    type: "skip",
-                    startPos,
-                    chunLength: chunk.length,
-                    chunk,
-                    lastSuccessCut: startPos,
-                    hash: md5(lastCode),
-                  });
-                  console.log("Skipped chunk", { startPos, chunkLength: chunk.length });
-                } else {
-                  mod.actions.push({
-                    TRIED: TRIED + 1,
-                    SKIP,
-                    DIFFs: DIFFs + 1,
-                    chars: instructions.length,
-                    type: "updated",
-                    startPos,
-                    chunLength: len,
-                    chunk: chunk.slice(0, len),
-                    lastSuccessCut: len + startPos,
-                    hash: md5(mod.lastCode),
-                  });
-                  console.log("Updated chunk", { startPos, chunkLength: len });
                 }
+                mod.actions.push({
+                  TRIED: TRIED + 1,
+                  SKIP: SKIP + 1,
+                  DIFFs,
+                  chars: instructions.length,
+                  type: "skip",
+                  startPos,
+                  c: chunk.length,
+                  chunk,
+                  lastSuccessCut: startPos,
+                  hash: md5(lastCode),
+                });
+                console.log("Skipped chunk", { startPos, chunkLength: chunk.length });
+              } else {
+                mod.actions.push({
+                  TRIED: TRIED + 1,
+                  SKIP,
+                  DIFFs: DIFFs + 1,
+                  chars: instructions.length,
+                  type: "updated",
+                  startPos,
+                  chunLength: len,
+                  chunk: chunk.slice(0, len),
+                  lastSuccessCut: len + startPos,
+                  hash: md5(mod.lastCode),
+                });
+                console.log("Updated chunk", { startPos, chunkLength: len });
               }
             } else {
               finished = true;
