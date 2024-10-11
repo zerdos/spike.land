747233414da00630039f99b2165430a20c6c4c0c
Zoltan Erdos <zoltan.erdos@me.com>
2024-10-11 22:18:36 +0100

    deploy

--- packages/code/src/sw-local-fake-server.ts
+++ packages/code/src/sw-local-fake-server.ts
@@ -5,6 +5,7 @@ import { transpile } from "@/lib/shared";
 import { CodeSessionBC } from "./services/CodeSessionBc";
 
 import HTML from "./index.html";
+import type { ICodeSession } from "./modules";
 
 const cSessions: {
   [key: string]: CodeSessionBC;
@@ -22,63 +23,19 @@ export async function fakeServer(request: Request) {
   if (
     request.url.includes("/session.json")
   ) {
-    console.log("Session request:", request.url);
-
-    return new Response(JSON.stringify(session), {
-      headers: {
-        "Content-Type": "application/json",
-        ...request.headers,
-      },
-    });
+    return handleSessionJson(request, session);
   } else if (
     request.url.includes("/index.tsx")
   ) {
-    console.log("Index request:", request.url);
-
-    return new Response(session.code, {
-      headers: {
-        "Content-Type": "application/javascript; charset=UTF-8",
-        ...request.headers,
-      },
-    });
+    return handleIndexTsx(request, session);
   } else if (
     request.url.includes("/index.js")
   ) {
-    console.log("Transpiled request:", request.url);
-
-    if (typeof session.transpiled !== "string" || session.transpiled === "") {
-      const transpiled = await transpile({
-        code: session.code,
-        originToUse: location.origin,
-      }) as unknown as string;
-      session.transpiled = transpiled;
-      await cSessions[codeSpace].postMessage({
-        ...session,
-        transpiled,
-        i: session.i + 1,
-      });
-    }
-
-    return new Response(
-      importMapReplace(session.transpiled, location.origin),
-      {
-        headers: {
-          "Content-Type": "application/javascript; charset=UTF-8",
-          ...request.headers,
-        },
-      },
-    );
+    return await handleIndexJs(request, session, codeSpace);
   } else if (
     request.url.includes("/index.css")
   ) {
-    console.log("css request:", request.url);
-
-    return new Response(session.css, {
-      headers: {
-        "Content-Type": "text/css; charset=UTF-8",
-        ...request.headers,
-      },
-    });
+    return handleIndexCss(request, session);
   } else if (
     pathname in Object.keys(routes)
     || request.url.includes("/hydrated")
@@ -90,55 +47,126 @@ export async function fakeServer(request: Request) {
     || request.url.endsWith(`/live/${codeSpace}/xxx`)
     || request.url.endsWith(`/live/${codeSpace}/`)
   ) {
-    const respText = HTML.replace(
-      `<script type="importmap"></script>`,
-      `<script type="importmap">${JSON.stringify(importMap)}</script>`,
-    ).replace(
-      `<!-- Inline LINK for initial theme -->`,
-      `<link rel="preload" href="/live/${session.codeSpace}/index.css" as="style" />
-    <link rel="stylesheet" href="/live/${session.codeSpace}/index.css" />`,
-    ).replace(
-      "<div id=\"embed\"></div>",
-      `<div id="embed">${session.html}</div>`,
-    );
-
-    const headers = new Headers({
-      "Access-Control-Allow-Origin": "*",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Resource-Policy": "cross-origin",
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cache-Control": "no-cache",
-      "Content-Encoding": "gzip",
-      "Content-Type": "text/html; charset=UTF-8",
-    });
-
-    return new Response(respText, { status: 200, headers });
+    return handleHtmlResponse(session);
   } else if (
     request.url.endsWith(`/live/${codeSpace}`)
     || request.url.endsWith(`/live/${codeSpace}/`)
   ) {
-    const respText = HTML.replace(
-      `<script type="importmap"></script>`,
-      `<script type="importmap">${JSON.stringify(importMap)}</script>`,
-    ).replace(
-      `<div id="embed"></div>`,
-      `<div id="embed"><iframe title="Live preview" src="/live/${codeSpace}/iframe"></iframe></div>`,
-    );
-
-    const headers = new Headers({
-      "Access-Control-Allow-Origin": "*",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Resource-Policy": "cross-origin",
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cache-Control": "no-cache",
-      "Content-Encoding": "gzip",
-      "Content-Type": "text/html; charset=UTF-8",
-    });
-
-    return new Response(respText, { status: 200, headers });
+    return buildHTMLResponse(codeSpace);
   } else {
     console.log("Default request:", request.url);
 
     return fetch(request);
   }
+}
+function buildHTMLResponse(codeSpace: string) {
+  const respText = HTML.replace(
+    `<script type="importmap"></script>`,
+    `<script type="importmap">${JSON.stringify(importMap)}</script>`,
+  ).replace(
+    `<div id="embed"></div>`,
+    `<div id="embed"><iframe title="Live preview" src="/live/${codeSpace}/iframe"></iframe></div>`,
+  );
+
+  const headers = new Headers({
+    "Access-Control-Allow-Origin": "*",
+    "Cross-Origin-Embedder-Policy": "require-corp",
+    "Cross-Origin-Resource-Policy": "cross-origin",
+    "Cross-Origin-Opener-Policy": "same-origin",
+    "Cache-Control": "no-cache",
+    "Content-Encoding": "gzip",
+    "Content-Type": "text/html; charset=UTF-8",
+  });
+
+  return new Response(respText, { status: 200, headers });
+}
+
+function handleHtmlResponse(session: ICodeSession) {
+  const respText = HTML.replace(
+    `<script type="importmap"></script>`,
+    `<script type="importmap">${JSON.stringify(importMap)}</script>`,
+  ).replace(
+    `<!-- Inline LINK for initial theme -->`,
+    `<link rel="preload" href="/live/${session.codeSpace}/index.css" as="style" />
+    <link rel="stylesheet" href="/live/${session.codeSpace}/index.css" />`,
+  ).replace(
+    "<div id=\"embed\"></div>",
+    `<div id="embed">${session.html}</div>`,
+  );
+
+  const headers = new Headers({
+    "Access-Control-Allow-Origin": "*",
+    "Cross-Origin-Embedder-Policy": "require-corp",
+    "Cross-Origin-Resource-Policy": "cross-origin",
+    "Cross-Origin-Opener-Policy": "same-origin",
+    "Cache-Control": "no-cache",
+    "Content-Encoding": "gzip",
+    "Content-Type": "text/html; charset=UTF-8",
+  });
+
+  return new Response(respText, { status: 200, headers });
+}
+
+function handleIndexCss(request: Request<unknown, CfProperties<unknown>>, session: ICodeSession) {
+  console.log("css request:", request.url);
+
+  return new Response(session.css, {
+    headers: {
+      "Content-Type": "text/css; charset=UTF-8",
+      ...request.headers,
+    },
+  });
+}
+
+async function handleIndexJs(
+  request: Request<unknown, CfProperties<unknown>>,
+  session: ICodeSession,
+  codeSpace: string,
+) {
+  console.log("Transpiled request:", request.url);
+
+  if (typeof session.transpiled !== "string" || session.transpiled === "") {
+    const transpiled = await transpile({
+      code: session.code,
+      originToUse: location.origin,
+    }) as unknown as string;
+    session.transpiled = transpiled;
+    await cSessions[codeSpace].postMessage({
+      ...session,
+      transpiled,
+      i: session.i + 1,
+    });
+  }
+
+  return new Response(
+    importMapReplace(session.transpiled, location.origin),
+    {
+      headers: {
+        "Content-Type": "application/javascript; charset=UTF-8",
+        ...request.headers,
+      },
+    },
+  );
+}
+
+function handleIndexTsx(request: Request<unknown, CfProperties<unknown>>, session: ICodeSession) {
+  console.log("Index request:", request.url);
+
+  return new Response(session.code, {
+    headers: {
+      "Content-Type": "application/javascript; charset=UTF-8",
+      ...request.headers,
+    },
+  });
+}
+
+function handleSessionJson(request: Request<unknown, CfProperties<unknown>>, session: ICodeSession) {
+  console.log("Session request:", request.url);
+
+  return new Response(JSON.stringify(session), {
+    headers: {
+      "Content-Type": "application/json",
+      ...request.headers,
+    },
+  });
 }
