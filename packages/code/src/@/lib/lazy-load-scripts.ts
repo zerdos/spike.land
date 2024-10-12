a5a2182e96dd94a26f7c2013dc0c3a22af49e70f
Your Name <you@example.com>
2024-10-12 22:28:17 +0100

    deploy

--- packages/code/src/@/lib/lazy-load-scripts.ts
+++ packages/code/src/@/lib/lazy-load-scripts.ts
@@ -8,16 +8,8 @@ const loadedScripts: Set<string> = globalWithLoadedScripts.loadedScripts
   || new Set<string>();
 globalWithLoadedScripts.loadedScripts = loadedScripts;
 
-export const lazyLoadScript = (scriptName: string, v = "0"): void => {
-  if (!loadedScripts.has(scriptName)) {
-    try {
-      importScripts(`/@/workers/${scriptName}.worker.js?v=${v}`);
-      loadedScripts.add(scriptName);
-    } catch (error) {
-      console.error(`Error loading script ${scriptName}:`, error);
-      throw error;
-    }
-  }
+export const lazyLoadScript = (scriptName: string): void => {
+  importScripts(`/@/workers/${scriptName}.worker.js`);
 };
 
 // If you need to add other global properties, you can do it like this:
