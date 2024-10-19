019108a9f7f8a167cd37b216ba615630f0a2d419
Zoltan Erdos <zoltan.erdos@me.com>
2024-10-19 01:03:01 +0100

    deploy

--- packages/code/vite.config.ts
+++ packages/code/vite.config.ts
@@ -21,6 +21,9 @@ export default defineConfig({
   plugins: [react({
     jsxImportSource: "@emotion/react",
   })],
+  build: {
+    outDir: "dist-vite",
+  },
 
   appType: "spa",
   assetsInclude: [
