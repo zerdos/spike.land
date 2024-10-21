322784825dc666afd3dbfd3ce0a6757a0ea719d2
Zoltan Erdos <zoltan.erdos@me.com>
2024-10-21 22:12:54 +0100

    deploy

--- packages/code/src/@/components/app/diff-editor.tsx
+++ packages/code/src/@/components/app/diff-editor.tsx
@@ -1,3 +1,4 @@
+// diff-editor.tsx
 import type { FC } from "react";
 import { diffLines as d } from "diff";
 
@@ -7,20 +8,18 @@ interface DiffViewerProps {
 }
 
 export const DiffViewer: FC<DiffViewerProps> = ({ original, modified }) => (
-  <pre className="font-mono whitespace-pre-wrap break-words bg-gray-700 p-4 rounded-md">
-    {d(original + "\n" || "", modified + "\n" || original).map(
-      (part, index) => (
-        <span
-          key={index}
-          className={`text-gray-200
+  <pre className="font-mono text-xs whitespace-pre-wrap break-words bg-gray-700 p-4 rounded-md">
+    {d(original ?? "", modified ?? original).map((part, index) => (
+      <span
+        key={index}
+        className={`text-gray-200
           ${part.added ? "bg-green-200 text-green-800" : ""}
           ${part.removed ? "bg-red-200 text-red-800" : ""}
         `}
-        >
-          {part.value}
-        </span>
-      )
-    )}
+      >
+        {part.value}
+      </span>
+    ))}
   </pre>
 );
 const original = `
@@ -37,7 +36,7 @@ const modified = `
 <DiffContainer>
   {diff.map((part, index) => (
     <DiffPart key={index} added={part.added} removed={part.removed}>
-      {part.value}
+
     </DiffPart>
   ))}
 </DiffContainer>
