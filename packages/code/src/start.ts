054a0715c8ee9347feb9096dd82f06fcb68076a0
Zoltan Erdos <zoltan.erdos@me.com>
2024-10-28 10:26:01 +0000

    deploy

--- packages/code/src/start.ts
+++ packages/code/src/start.ts
@@ -6,6 +6,9 @@ import { getCodeSpace } from "@/hooks/use-code-space";
 import { renderApp } from "@/lib/render-app";
 import { main } from "./ws";
 
+// import "./assets/app.css";
+// import "./index.css";
+
 // if ("serviceWorker" in navigator) {
 //   navigator.serviceWorker.getRegistrations().then((registrations) => {
 //     for (const registration of registrations) {
