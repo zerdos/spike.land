761158695b7e2c56456ebaa3b17595a02141adab
Zoltan Erdos <zoltan.erdos@virginmediao2.co.uk>
2024-10-29 11:53:32 +0000

    deploy

--- packages/code/tailwind.config.js
+++ packages/code/tailwind.config.js
@@ -48,8 +48,8 @@ const config = {
         },
       },
       borderRadius: {
-        lg: "`var(--radius)`",
-        md: "`calc(var(--radius) - 2px)`",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
