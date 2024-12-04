02ce5b8ae69763b5ddff4709f891a189f428e345
Zoltan Erdos <zoltan.erdos@virginmediao2.co.uk>
2024-12-04 09:56:15 +0000

    deploy

--- packages/code/src/start.ts
+++ packages/code/src/start.ts
@@ -6,8 +6,8 @@ import { getCodeSpace } from "@/hooks/use-code-space";
 import { renderApp } from "@/lib/render-app";
 import { main } from "./ws";
 
-// import "./assets/app.css";
-// import "./index.css";
+import "./assets/app.css";
+import "./index.css";
 
 // if ("serviceWorker" in navigator) {
 //   navigator.serviceWorker.getRegistrations().then((registrations) => {
