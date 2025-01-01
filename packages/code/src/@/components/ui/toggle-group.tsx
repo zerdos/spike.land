ee9e2c8f5fe3d243eec43b9a2b5ec24725945529
Zoltan Erdos <zoltan.erdos@me.com>
2025-01-01 11:20:35 +0000

    deploy

--- packages/code/src/@/components/ui/toggle-group.tsx
+++ packages/code/src/@/components/ui/toggle-group.tsx
@@ -3,7 +3,7 @@ import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
 import { type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
-import { toggleVariants } from "@/components/ui/toggle";
+import { toggleVariants } from "@/components/ui/toggle-variants";
 
 const ToggleGroupContext = React.createContext<
   VariantProps<typeof toggleVariants>
@@ -13,14 +13,14 @@ const ToggleGroupContext = React.createContext<
 });
 
 const ToggleGroup = React.forwardRef<
-  React.ElementRef<typeof ToggleGroupPrimitive.Root>,
+  React.ComponentRef<typeof ToggleGroupPrimitive.Root>,
   & React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root>
   & VariantProps<typeof toggleVariants>
->(({ className, variant, size, children, ...props }, ref) => (
+>(({ className, variant, size, children, type}, ref) => (
   <ToggleGroupPrimitive.Root
     ref={ref}
+    type={type}
     className={cn("flex items-center justify-center gap-1", className)}
-    {...props}
   >
     <ToggleGroupContext.Provider value={{ variant, size }}>
       {children}
@@ -31,10 +31,10 @@ const ToggleGroup = React.forwardRef<
 ToggleGroup.displayName = ToggleGroupPrimitive.Root.displayName;
 
 const ToggleGroupItem = React.forwardRef<
-  React.ElementRef<typeof ToggleGroupPrimitive.Item>,
+  React.ComponentRef<typeof ToggleGroupPrimitive.Item>,
   & React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item>
   & VariantProps<typeof toggleVariants>
->(({ className, children, variant, size, ...props }, ref) => {
+>(({ className, children, variant, size, value, ...props }, ref) => {
   const context = React.useContext(ToggleGroupContext);
 
   return (
@@ -44,9 +44,9 @@ const ToggleGroupItem = React.forwardRef<
         toggleVariants({
           variant: context.variant || variant,
           size: context.size || size,
-        }),
         className,
-      )}
+      }))}
+      value={value}
       {...props}
     >
       {children}
