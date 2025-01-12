f04b0afcb923efb56fcfbe86382546d8cb2d976e
Zoltan Erdos <zoltan.erdos@me.com>
2025-01-12 11:56:05 +0000

    deploy

--- packages/code/src/tw-dev-setup.ts
+++ packages/code/src/tw-dev-setup.ts
@@ -57,3 +57,4 @@ class ResourceLoaderImpl implements ResourceLoader {
 // Singleton instance
 export const resourceLoader = new ResourceLoaderImpl();
 export const init = () => resourceLoader.init();
+init();
