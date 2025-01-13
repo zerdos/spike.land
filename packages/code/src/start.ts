01b54cf5c2e6071c5b5f95ea79d114224aea6fa1
Zoltan Erdos <zoltan.erdos@virginmediao2.co.uk>
2025-01-13 10:07:36 +0000

    deploy

--- packages/code/src/start.ts
+++ packages/code/src/start.ts
@@ -1,6 +1,7 @@
 // import { createRequire } from "module";
 // const require = createRequire(import.meta.url);
 // Object.assign(globalThis, { require });
+// import "./tw-dev-setup";
 
 import { getCodeSpace } from "@/hooks/use-code-space";
 import { renderApp } from "@/lib/render-app";
