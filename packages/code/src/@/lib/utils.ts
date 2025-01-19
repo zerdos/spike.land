746b3b9ed4fd68ca65f57a999f9525f12243ce19
Zoltan Erdos <zoltan.erdos@me.com>
2025-01-19 10:53:01 +0000

    deploy

--- packages/code/src/@/lib/utils.ts
+++ packages/code/src/@/lib/utils.ts
@@ -1,11 +1,6 @@
-import type { ClassNameMerger } from "@/workers/tw-merge.worker";
-import * as tw from "@/workers/tw-merge.worker";
-import type { ClassValue } from "class-variance-authority/types";
-interface GlobalThisWithClassNameMerger {
-  cn: ClassNameMerger;
-}
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return (globalThis as unknown as GlobalThisWithClassNameMerger).cn(inputs);
+  return twMerge(clsx(inputs));
 }
-export default tw;
