c5325643d3de799e3987a076a80c823975653b0b
Zoltan Erdos <zoltan.erdos@me.com>
2025-01-19 10:36:30 +0000

    deploy

--- packages/code/src/@/lib/utils.ts
+++ packages/code/src/@/lib/utils.ts
@@ -1,7 +1,10 @@
 import type { ClassNameMerger } from "@/workers/tw-merge.worker";
-
+import "@/workers/tw-merge.worker";
+import type { ClassValue } from "class-variance-authority/types";
 interface GlobalThisWithClassNameMerger {
   cn: ClassNameMerger;
 }
 
-export const { cn } = globalThis as unknown as GlobalThisWithClassNameMerger;
+export function cn(...inputs: ClassValue[]) {
+  return (globalThis as unknown as GlobalThisWithClassNameMerger).cn(inputs);
+}
