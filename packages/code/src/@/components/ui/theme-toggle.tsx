efba0e482e99e2f148b0d58f013de664eace5c27
Zoltan Erdos <zoltan.erdos@virginmediao2.co.uk>
2025-01-23 12:54:58 +0000

    deploy

--- packages/code/src/@/components/ui/theme-toggle.tsx
+++ packages/code/src/@/components/ui/theme-toggle.tsx
@@ -6,7 +6,10 @@ import { motion } from "framer-motion";
 export const ThemeToggle = () => {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
   return (
-    <motion.div whileTap={{ scale: 0.9 }} whileHover={{ scale: 1.1, rotate: isDarkMode ? 180 : 0 }}>
+    <motion.div
+      whileTap={{ scale: 0.9 }}
+      whileHover={{ scale: 1.1, rotate: isDarkMode ? 360 + 180 : 360 }}
+    >
       <Button
         onClick={toggleDarkMode}
         variant="outline"
