6acd8cd611cb038bd6f769bac31e1b2fce3f380e
Zoltan Erdos <zoltan.erdos@virginmediao2.co.uk>
2025-01-23 11:41:27 +0000

    deploy

--- packages/code/src/renderPreviewWindow.tsx
+++ packages/code/src/renderPreviewWindow.tsx
@@ -1,11 +1,22 @@
 import type { ICode } from "@/lib/interfaces";
-import { renderApp } from "@/lib/render-app";
-import { ClerkProvider } from "@clerk/clerk-react";
-import { AppToRender } from "./AppToRender";
 
 export const renderPreviewWindow = async (
   { codeSpace, cSess }: { codeSpace: string; cSess: ICode; },
 ) => {
+  //   import { renderApp } from "@/lib/render-app";
+  // import { ClerkProvider } from "@clerk/clerk-react";
+  // import { AppToRender } from "./AppToRender";
+
+  const renderAppPromise = import("@/lib/render-app");
+  const ClerkProviderPromise = import("@clerk/clerk-react");
+  const AppToRenderPromise = import("./AppToRender");
+
+  const [
+    { renderApp },
+    { ClerkProvider },
+    { AppToRender },
+  ] = await Promise.all([renderAppPromise, ClerkProviderPromise, AppToRenderPromise]);
+
   const App = () => (
     <ClerkProvider
       publishableKey="pk_live_Y2xlcmsuc3Bpa2UubGFuZCQ"
