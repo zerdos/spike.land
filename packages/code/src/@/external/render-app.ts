34c5d4407789c3adcf6faffb700ef95dadbdcdaf
Zoltan Erdos <zoltan.erdos@virginmediao2.co.uk>
2025-01-29 14:35:10 +0000

    deploy

--- packages/code/src/@/external/render-app.ts
+++ packages/code/src/@/external/render-app.ts
@@ -1,6 +1,1 @@
-import type { renderApp as RenderApp } from "@/lib/render-app";
-import "/@/workers/render-app.worker.js";
-
-export const { renderApp } = globalThis as unknown as {
-  renderApp: typeof RenderApp;
-};
+export { renderApp } from "@/lib/render-app";
